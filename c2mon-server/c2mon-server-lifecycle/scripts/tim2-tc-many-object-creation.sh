839693640c780dff5ef24d01a7ae5d70dc23308b
Mark Brightwell <markbrightwell@gmail.com>
2011-05-19 13:57:55 +0000

    moved to new c2mon naming

---
+++ c2mon-server/c2mon-server-lifecycle/scripts/tim2-tc-many-object-creation.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# TIM2 server
+# script for starting up the test server with Terracotta cache distribution
+# change SERVER home to switch between dev and dist
+
+########
+# JAVA #
+########
+
+export JAVA_HOME=/usr/java/jdk
+export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH/
+JAVA_BIN=$JAVA_HOME/jre/bin
+
+#############
+# arguments #
+#############
+
+#first argument is process name (used for logging file also)
+PROCESS_NAME=$1
+
+###############
+# DIRECTORIES #
+###############
+
+#server home
+SERVER_HOME=$HOME/dev/tim2-prototype
+
+#config home
+CONF_HOME=$SERVER_HOME/config
+
+#application home
+APP_HOME=$SERVER_HOME
+
+#Terracotta installation directory
+TERRACOTTA_HOME=~/opt/terracotta
+
+#directory of shared libraries
+SHARED_LIB_HOME=$HOME/dist/libs
+
+
+
+#################
+# CONFIGURATION #
+#################
+
+#Terracotta server host
+TC_HOST=cs-ccr-tim4
+
+#terracotta server name (one of them)
+#TC_NAME=server1
+
+#terracotta DSO port
+TC_PORT=9510
+
+#Terracotta configuration location (either file or host:port)
+TERRACOTTA_CONFIG=$TC_HOST:$TC_PORT
+
+#.tim.properties location
+TIM_PROPERTIES=~/.tim2.properties
+
+#log4j configuration file
+LOG4J_CONF_FILE=$CONF_HOME/log4j.tim.xml
+
+#log directory
+LOG_DIR=$SERVER_HOME/log
+
+##############
+# LIBRAIRIES #
+##############
+
+#librairies (links to required libs)
+APP_LIBS=$APP_HOME/libs/*
+
+#Terracotta module libraries
+TC_MOD_HOME=$TERRACOTTA_HOME/platform/modules/org/terracotta/modules
+TERRACOTTA_MODULES=$TC_MOD_HOME/tim-distributed-cache/1.3.2/tim-distributed-cache-1.3.2.jar:$TC_MOD_HOME/tim-ehcache-2.0/1.5.2/tim-ehcache-2.0-1.5.2.jar:$TC_MOD_HOME/tim-concurrent-collections/1.3.2/tim-concurrent-collections-1.3.2.jar:$TC_MOD_HOME/tim-async-processing/1.3.2/tim-async-processing-1.3.2.jar:$TC_MOD_HOME/tim-annotations/1.5.1/tim-annotations-1.5.1.jar
+
+#Spring modules
+SPRING_MODULES=$SHARED_LIB_HOME/spring-modules/spring-modules-cache
+
+#libs for Ehcache
+#EHCACHE_LIBS=$SHARED_LIB_HOME/ehcache/ehcache-core-2.0.1.jar:$SHARED_LIB_HOME/slf4j/slf4j-api-1.5.8.jar:$SHARED_LIB_HOME/slf4j/slf4j-jdk14-1.5.8.jar
+#$SHARED_LIB_HOME/ehcache/ehcache-terracotta-2.0.1.jar:
+
+#other libs
+#OTHER_LIBS=$SHARED_LIB_HOME/jta/jta-1.1.jar:$SHARED_LIB_HOME/fuse/jencks-amqpool-2.2.jar:$SHARED_LIB_HOME/jencks/jencks-2.1.jar:$SHARED_LIB_HOME/apache-activemq/activemq-ra-5.3.0.jar
+
+#apache commons libs
+#APACHE_COMMONS_LIBS=$SHARED_LIB_HOME/apache-commons/commons-cli-1.1.jar:$SHARED_LIB_HOME/apache-commons/commons-io-1.4.jar
+
+#application jar location (now in lib location)
+APPLICATION_JAR=$APP_HOME/dist/multi-server-prototype.jar
+
+#all needed librairies (including cachetest)
+REQUIRED_LIBS=$APPLICATION_JAR:$APP_LIBS:$TERRACOTTA_MODULES:$SPRING_MODULES
+#:$EHCACHE_LIBS
+echo required libs: $REQUIRED_LIBS
+
+#run the java package
+COMMAND_OPTIONS="-Xms1024m -Xmx1024m -XX:+PrintGCDetails -XX:InitialSurvivorRatio=4 -XX:MinSurvivorRatio=4 -XX:-UseAdaptiveSizePolicy -XX:+UseParallelGC -XX:NewRatio=1 -Dcom.tc.l1.cachemanager.percentageToEvict=10 -Dcom.tc.l1.cachemanager.threshold=70 -Dcom.tc.l1.cachemanager.monitorOldGenOnly=false -Dserver.process.name=$PROCESS_NAME -Dtc.config=$TERRACOTTA_CONFIG -Dlog4j.configuration=$LOG4J_CONF_FILE -Dtim.log.dir=$LOG_DIR -Dtim.properties.location=$TIM_PROPERTIES -classpath ${REQUIRED_LIBS}"
+#-Dcom.tc.l1.cachemanager.criticalObjectThreshold=100000 -XX:+UseConcMarkSweepGC
+
+echo run with options: $COMMAND_OPTIONS
+
+dso-java.sh $COMMAND_OPTIONS cern.tim.server.startup.ServerStartup
---
+++ c2mon-server/c2mon-server-lifecycle/src/java/cern/tim/server/lifecycle/ServerStartup.java
@@ -0,0 +1,178 @@
+/******************************************************************************
+ * This file is part of the Technical Infrastructure Monitoring (TIM) project.
+ * See http://ts-project-tim.web.cern.ch
+ * 
+ * Copyright (C) 2005-2010 CERN.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * 
+ * Author: TIM team, tim.support@cern.ch
+ *****************************************************************************/
+package cern.tim.server.lifecycle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+/**
+ * The class containing the main method for starting a server.
+ * It loads the Spring XML files (one for each module). 
+ * 
+ * Distributed configuration details are kept in the DistributedParams
+ * Spring bean (in server-common).
+ * 
+ * The main method can be called with the following arguments:
+ * 
+ *  -Dlog4j.configuration - location of the log4j configuration file (compulsory)
+ *  -Dtim.properties      - location of the tim.properties file 
+ *                          (optional - default is .tim2.properties in the user home directory) 
+ *  
+ * @author Mark Brightwell
+ *
+ */
+public final class ServerStartup {
+  
+  /**
+   * Class logger.
+   */
+  private static Logger logger = Logger.getLogger(ServerStartup.class);  
+  
+  /**
+   * Override public constructor.
+   */
+  private ServerStartup() { 
+  }
+  
+  /**
+   * Terracotta root with distributed configuration data.
+   */
+  //private static HashMap<String,Object> serverConfig;
+  //private static DistributedParams distributedParams;
+  
+  /**
+   * Main server start-up method
+   * @param args start-up args
+   */
+  public static void main(final String[] args) {
+    
+    //load the Spring context from the XML file
+    //(must be done after the DistributedParams have been initialized, as some of the Spring
+    // beans will need these at startup - Spring should manage this dependency on the DistributedParams bean)
+    
+    //initialize log4j
+    if (System.getProperty("log4j.configuration") == null) {
+      System.err.println("Please specify log4j location using Java VM argument -Dlog4j.configuration.");
+      System.exit(-1);
+    }
+    
+    try {          
+      DOMConfigurator.configureAndWatch(System.getProperty("log4j.configuration"));      
+    }
+    catch (Exception ex) {       
+      ex.printStackTrace();            
+      System.exit(-1);
+    }
+     
+    //set default tim.properties location if not specified as Dtim.properties.location     
+    if (System.getProperty("tim.properties.location") == null) {
+      System.setProperty("tim.properties.location", System.getProperty("user.home") + "/.tim2.properties");
+    }
+    logger.info("Using tim.properties file at: " + System.getProperty("tim.properties.location"));
+    
+    String confLocation = "file:" + System.getProperty("tim.home") + "/config";
+    
+    String[] coreProperties = new String[]{"file:" + System.getProperty("tim.properties.location"),
+                                           confLocation + "/tim-jms.properties",
+                                           confLocation + "/tim-datasource.properties",
+                                           confLocation + "/tim-cache.properties"};
+    
+    GenericBeanDefinition propertiesFactoryBean = new GenericBeanDefinition();
+    propertiesFactoryBean.setBeanClass(PropertiesFactoryBean.class);
+    MutablePropertyValues propertyValues = new MutablePropertyValues();
+    propertyValues.addPropertyValue("locations", coreProperties);
+    propertiesFactoryBean.setPropertyValues(propertyValues);    
+    
+    //start an initial Spring application context and register properties bean
+    GenericApplicationContext ctx = new GenericApplicationContext();    
+    ctx.registerBeanDefinition("serverProperties", propertiesFactoryBean);        
+    ctx.refresh();
+        
+    //by default run in single-server mode
+    List<String> cacheModeModules;
+    if (System.getProperty("cern.c2mon.cache.mode") != null && System.getProperty("cern.c2mon.cache.mode").equals("multi")) {
+      logger.info("C2MON server running in distributed cache mode");
+      cacheModeModules = new ArrayList<String>(Arrays.asList("cern/tim/server/lifecycle/config/server-lifecycle-multi.xml",         
+                                         "cern/tim/server/cache/config/server-cache-multi-server.xml")); 
+    } else {
+      logger.info("C2MON server running in local cache mode (not distributed)");
+      cacheModeModules = new ArrayList<String>(Arrays.asList("cern/tim/server/lifecycle/config/server-lifecycle-single.xml",         
+      "cern/tim/server/cache/config/server-cache-single-server.xml"));
+    }
+    
+    //core modules (in classpath); optional modules are imported in server-startup.xml
+    List<String> coreModules = new ArrayList<String>(Arrays.asList(
+                                         "cern/tim/server/common/config/server-common.xml",                                         
+                                         "cern/tim/server/cache/dbaccess/config/server-cachedbaccess.xml",
+                                         "cern/tim/server/cache/loading/config/server-cacheloading.xml",
+                                         "cern/tim/server/supervision/config/server-supervision.xml",
+                                         "cern/tim/server/daqcommunication/in/config/server-daqcommunication-in.xml",
+                                         "cern/tim/server/daqcommunication/out/config/server-daqcommunication-out.xml",
+                                         "cern/c2mon/server/configuration/config/server-configuration.xml"
+                                         )); 
+    
+    coreModules.addAll(cacheModeModules);
+    
+    final ClassPathXmlApplicationContext xmlContext = new ClassPathXmlApplicationContext(coreModules.toArray(new String[0]), ctx);
+       
+    logger.info("Starting the beans in application context.");
+    //start all components that need manually starting
+    xmlContext.start();
+    
+    //"cern/tim/server/command/config/server-command.xml"
+//                                                                                                  Diamon module
+    //"resources/application-context-with-tim-server.xml"
+    //"cern/tim/server/benchmark/config/server-benchmark.xml",                                                                                          
+    
+    //add shutdown hook to context (all @PreDestroy methods will then be called)
+    xmlContext.registerShutdownHook();
+//    Runtime.getRuntime().addShutdownHook(new Thread() {
+//      public void run() {
+//        xmlContext.stop();
+//        xmlContext.close();
+//      }
+//    });
+    
+  }
+
+  
+  
+  //****************
+  // HELPER METHODS 
+  //****************
+  
+  //not used so far
+  /**
+   * Converts the Java properties object into the configuration
+   * HashMap, mapping Strings to the objects as needed.
+   */
+//  private static HashMap<String,Object> propertiesToMap(Properties properties) {
+//    return null;
+//  }
+
+}
