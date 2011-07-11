12a7516f1eeb40ba941d956ac8ed536443e00d0d
Matthias Braeger <matthias.braeger@cern.ch>
2011-07-11 09:16:57 +0000

    TIMS-106: Added fake test for Bamboo. Proper JUnit tests have to be implemented once the logic is completely defined.

---
+++ c2mon-client/c2mon-client-auth/src/test/cern/c2mon/client/auth/LoginToolBarTest.java
@@ -0,0 +1,14 @@
+package cern.c2mon.client.auth;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class LoginToolBarTest {
+
+  @Test
+  public void testNothing() {
+    // nothing to test, yet
+    Assert.assertTrue(true);
+  }
+}
