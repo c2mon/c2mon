76fc4d6a4b41aa8c4fdcc75a80969190cd9ecb03
Wojciech Buczak <wojciech.buczak@gmail.com>
2011-07-20 10:33:01 +0000

    importing module

---
+++ c2mon-daq/c2mon-daq-deploy/modules/dmn2/bin/dmn2-cleanpids2.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#
+# This script can be used to remove all DAQ pid files
+# from processes which are not anymore running on the
+# host from which the script is being executed
+#
+# Points to the DAQ2 directory.
+#
+# Author: Matthias Braeger
+#
+
+# sets the home directory
+#
+HOME=`dirname $0`
+[[ $HOME == "." ]] && HOME=$PWD
+HOME=$HOME/..
+
+C2MON_DAQ_HOME=$HOME
+
+
+# Temp directory of the DAQ processes
+DAQ_TMP=${C2MON_DAQ_HOME}/tmp
+
+
+# Call the runs function to find out if a process with
+# a certain pid runs. The "runs" function will return
+# 0 if the process is running, 1 if it is not.
+# #Example: runs 23049
+runs() {
+  pid=${1##*/}
+  tmp=`ps -p $pid -o pid=`
+  if [ -z "$tmp" ] ; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+################### Main routine ##########################
+
+if [ -d ${DAQ_TMP}/${HOSTNAME} ] ; then
+  for file in `ls ${DAQ_TMP}/${HOSTNAME}/*.pid` ; do
+    pid=`cat $file | awk '{print $1}'` 
+    runs $pid
+    if [ $? -eq 1 ] ; then
+      # It is not running --> remove PID file
+      echo "Cleaned $file"
+      rm $file
+    fi
+  done
+else 
+  echo "This machine is not a DAQ host!"
+fi
+
+
---
+++ c2mon-daq/c2mon-daq-deploy/modules/tim2/bin/tim2-stop-all-tim6_2.sh
@@ -0,0 +1,26 @@
+DAQ_HOME=~/dist/daqprocess2
+DAQ_CONTROL=$DAQ_HOME/bin/daqprocess2.sh
+
+pushd $DAQ_HOME/tmp/ >/dev/null 2>/dev/null
+LS=`ls -1 *.pid`
+popd >/dev/null 2>/dev/null
+
+for pidfile in $LS; do
+  proc=${pidfile:11}
+  namelength=`expr index "$proc" .pid`-1
+  proc=${proc:0:$namelength}
+  $DAQ_CONTROL stop $proc
+  sleep 1
+done
+
+pushd $DAQ_HOME/tmp/cs-ccr-tim6/ >/dev/null 2>/dev/null
+LS=`ls -1 *.pid`
+popd >/dev/null 2>/dev/null
+
+for pidfile in $LS; do
+  proc=${pidfile:11}
+  namelength=`expr index "$proc" .pid`-1
+  proc=${proc:0:$namelength}
+  $DAQ_CONTROL stop $proc
+  sleep 1
+done
