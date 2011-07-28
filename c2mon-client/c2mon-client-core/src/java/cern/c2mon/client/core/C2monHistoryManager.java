e6fa8882504912373b6de1b77f964f7fbb98f2de
vdeila <vdeila@cern.ch>
2011-07-28 14:51:38 +0000

    TIMS-114: Move the history player from TIM1 to TIM2, implementing the history manager

--- c2mon-client/c2mon-client-core/src/java/cern/c2mon/client/core/C2monHistoryManager.java
+++ c2mon-client/c2mon-client-core/src/java/cern/c2mon/client/core/C2monHistoryManager.java
@@ -17,24 +17,75 @@
  ******************************************************************************/
 package cern.c2mon.client.core;
 
+import cern.c2mon.client.common.history.HistoryPlayer;
+import cern.c2mon.client.common.history.HistoryPlayerEvents;
+import cern.c2mon.client.common.history.HistoryProvider;
+import cern.c2mon.client.common.history.HistoryProviderType;
+import cern.c2mon.client.common.history.Timespan;
+import cern.c2mon.client.common.history.event.HistoryPlayerListener;
+import cern.c2mon.client.common.history.exception.HistoryPlayerNotActiveException;
+import cern.c2mon.client.common.history.exception.NoHistoryProviderException;
+
 /**
- * This interface describes the methods which are provided by
- * the C2MON history manager singleton. The history manager
- * handles the communication with the C2MON STL database which is used
- * for the history player and for trending.
+ * This interface describes the methods which are provided by the C2MON history
+ * manager singleton. The history manager handles the communication with the
+ * C2MON STL database which is used for the history player and for trending.
  * <code>SessionListener</code>.
- *
+ * 
  * @author Matthias Braeger
  */
 public interface C2monHistoryManager {
-  
+
   /**
-   * Starts the history player. 
+   * Starts the history player.
+   * 
+   * @param provider
+   *          the provider to use to get history data
+   * @param timespan
+   *          the time frame to ask for
    */
-  void startHistoryPlayerMode();
-  
+  void startHistoryPlayerMode(final HistoryProvider provider, final Timespan timespan);
+
   /**
    * Stops the history player.
    */
   void stopHistoryPlayerMode();
+
+  /**
+   * 
+   * @return <code>true</code> if history mode is active
+   */
+  boolean isHistoryModeEnabled();
+
+  /**
+   * 
+   * @param type
+   *          the type of provider to get
+   * @return a history provider of the given type
+   * @throws NoHistoryProviderException
+   *           is thrown if something goes wrong in fetching the
+   *           {@link HistoryProvider}. For example if no url, username and
+   *           password is given.
+   */
+  HistoryProvider getHistoryProvider(final HistoryProviderType type) throws NoHistoryProviderException;
+
+  /**
+   * 
+   * @return the history player instance currently running.
+   * @throws HistoryPlayerNotActiveException
+   *           if the history player is not currently active, it is not possible
+   *           to get the history player
+   */
+  HistoryPlayer getHistoryPlayer() throws HistoryPlayerNotActiveException;
+
+  /**
+   * With this method you get an interface which you can use to register
+   * {@link HistoryPlayerListener} without that the history player needs to be
+   * activated.
+   * 
+   * @return an interface where you can add {@link HistoryPlayerListener} to the
+   *         history player without it being activated. It never returns
+   *         <code>null</code>.
+   */
+  HistoryPlayerEvents getHistoryPlayerEvents();
 }
--- c2mon-client/c2mon-client-core/src/java/cern/c2mon/client/core/manager/HistoryManager.java
+++ c2mon-client/c2mon-client-core/src/java/cern/c2mon/client/core/manager/HistoryManager.java
@@ -17,58 +17,227 @@
  ******************************************************************************/
 package cern.c2mon.client.core.manager;
 
+import java.util.Collection;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import cern.c2mon.client.common.history.HistoryPlayer;
+import cern.c2mon.client.common.history.HistoryPlayerEvents;
+import cern.c2mon.client.common.history.HistoryProvider;
+import cern.c2mon.client.common.history.HistoryProviderType;
+import cern.c2mon.client.common.history.Timespan;
+import cern.c2mon.client.common.history.exception.HistoryPlayerNotActiveException;
+import cern.c2mon.client.common.history.exception.NoHistoryProviderException;
+import cern.c2mon.client.common.listener.TagUpdateListener;
+import cern.c2mon.client.common.tag.ClientDataTag;
 import cern.c2mon.client.core.C2monHistoryManager;
 import cern.c2mon.client.core.cache.BasicCacheHandler;
 import cern.c2mon.client.core.listener.TagSubscriptionListener;
+import cern.c2mon.client.history.HistoryProviderFactory;
+import cern.c2mon.client.history.dbaccess.exceptions.HistoryException;
+import cern.c2mon.client.history.playback.HistoryPlayerCoreAccess;
+import cern.c2mon.client.history.playback.HistoryPlayerImpl;
+import cern.c2mon.client.jms.SupervisionListener;
+import cern.c2mon.shared.client.supervision.SupervisionConstants.SupervisionEntity;
 
 @Service
 public class HistoryManager implements C2monHistoryManager, TagSubscriptionListener {
 
+  /** Log4j logger for this class */
+  private static final Logger LOG = Logger.getLogger(HistoryManager.class);
+  
   /** Reference to the <code>TagManager</code> singleton */
   private final CoreTagManager tagManager;
-  
+
   /** Reference to the <code>ClientDataTagCache</code> */
   private final BasicCacheHandler cache;
   
+  /** the history player */
+  private HistoryPlayerCoreAccess historyPlayer;
+
   @Autowired
   protected HistoryManager(final CoreTagManager pTagManager, final BasicCacheHandler pCache) {
     this.tagManager = pTagManager;
     this.cache = pCache;
+    this.historyPlayer = null;
   }
-  
+
   /**
    * Inner method to initialize the STL database connection.
    */
+  @SuppressWarnings("unused")
+  @PostConstruct
   private void init() {
-    
+    tagManager.addTagSubscriptionListener(this);
   }
 
   @Override
-  public void startHistoryPlayerMode() {
-    tagManager.addTagSubscriptionListener(this);
-    
+  public void startHistoryPlayerMode(final HistoryProvider provider, final Timespan timespan) {
+    cache.setHistoryMode(true);
+
+    synchronized (cache.getHistoryModeSyncLock()) {
+      if (cache.isHistoryModeEnabled()) {
+        if (this.historyPlayer == null) {
+          this.historyPlayer = new HistoryPlayerImpl();
+        }
+        
+        // Configures the history player with the given provider and time span
+        this.historyPlayer.configure(provider, timespan);
+        
+        // Activating the history player
+        this.historyPlayer.activateHistoryPlayer();
+        
+        // Subscribes all the current subscribed tags
+        subscribeTagsToHistory(cache.getAllSubscribedDataTags());
+      }
+    }
   }
 
   @Override
   public void stopHistoryPlayerMode() {
-    tagManager.removeTagSubscriptionListener(this);
+    cache.setHistoryMode(false);
     
+    synchronized (cache.getHistoryModeSyncLock()) {
+      if (this.historyPlayer != null) {
+        this.historyPlayer.deactivateHistoryPlayer();
+      }
+    }
   }
 
   @Override
   public void onNewTagSubscriptions(final Set<Long> tagIds) {
-    // TODO Auto-generated method stub
-    
+    synchronized (cache.getHistoryModeSyncLock()) {
+      if (cache.isHistoryModeEnabled()
+          && this.historyPlayer != null 
+          && this.historyPlayer.isHistoryPlayerActive()) {
+        subscribeTagsToHistory(tagIds);
+      }
+    }
   }
 
   @Override
   public void onUnsubscribe(final Set<Long> tagIds) {
-    // TODO Auto-generated method stub
+    unsubscribeTagsFromHistory(tagIds);
+  }
+
+  /**
+   * Subscribes the tags to the history player
+   * 
+   * @param tagIds
+   *          The tags to subscribes
+   */
+  private void subscribeTagsToHistory(final Set<Long> tagIds) {
+    subscribeTagsToHistory(cache.get(tagIds).values());
+  }
+
+  /**
+   * Subscribes the tags to the history player
+   * 
+   * @param clientDataTags
+   *          The tags to subscribes
+   */
+  private void subscribeTagsToHistory(final Collection<ClientDataTag> clientDataTags) {
+    synchronized (cache.getHistoryModeSyncLock()) {
+      if (cache.isHistoryModeEnabled()
+          && this.historyPlayer != null 
+          && this.historyPlayer.isHistoryPlayerActive()) {
+        
+        // Registers all the tag update listeners and supervision listeners  
+        for (final ClientDataTag cdt : clientDataTags) {
+          
+          
+          // Registers to tag updates
+          try {
+            this.historyPlayer.registerTagUpdateListener(
+                (TagUpdateListener) cdt,
+                cdt.getId(),
+                cdt.clone());
+          }
+          catch (CloneNotSupportedException e) {
+            LOG.error("subscribeTagsToHistory() - Unable to clone ClientDataTag with id " + cdt.getId(), e);
+            throw new UnsupportedOperationException("Unable to clone ClientDataTag with id " + cdt.getId(), e);
+          }
+          
+          // Register to supervision events
+          this.historyPlayer.registerSupervisionListener(
+              SupervisionEntity.PROCESS,
+              (SupervisionListener) cdt,
+              cdt.getProcessIds());
+          
+          this.historyPlayer.registerSupervisionListener(
+              SupervisionEntity.EQUIPMENT,
+              (SupervisionListener) cdt,
+              cdt.getEquipmentIds());
+          
+//          For the day the SupervisionEntity.SUBEQUIPMENT also comes
+//          this.historyPlayer.registerSupervisionListener(
+//              SupervisionEntity.SUBEQUIPMENT,
+//              (SupervisionListener) clientDataTag,
+//              clientDataTag.getSubEquipmentIds());
+        }
+        
+        // Begins the loading process
+        this.historyPlayer.beginLoading();
+      }
+    }
+  }
+
+  /**
+   * Unsubscribes the tags to the history player
+   * 
+   * @param tagIds
+   *          The tags to unsubscribe
+   */
+  private void unsubscribeTagsFromHistory(final Set<Long> tagIds) {
+    synchronized (cache.getHistoryModeSyncLock()) {
+      if (cache.isHistoryModeEnabled()
+          && this.historyPlayer != null 
+          && this.historyPlayer.isHistoryPlayerActive()) {
+        // TODO Unregister the datatags
+        // TODO Unregister the equipment events, and the process events
+      }
+    }
     
   }
+
+  @Override
+  public HistoryPlayer getHistoryPlayer() throws HistoryPlayerNotActiveException {
+    if (isHistoryModeEnabled()) {
+      return historyPlayer;
+    }
+    else {
+      throw new HistoryPlayerNotActiveException("The history player is not active, and can therefore not be retrieved");
+    }
+  }
+  
+  @Override
+  public HistoryPlayerEvents getHistoryPlayerEvents() {
+    synchronized (cache.getHistoryModeSyncLock()) {
+      if (this.historyPlayer != null) {
+        this.historyPlayer = new HistoryPlayerImpl(); 
+      }
+      return this.historyPlayer;
+    }
+  }
+
+  @Override
+  public HistoryProvider getHistoryProvider(final HistoryProviderType type) throws NoHistoryProviderException {
+    try {
+      return HistoryProviderFactory.getInstance().createHistoryProvider(type);
+    }
+    catch (final HistoryException e) {
+      throw new NoHistoryProviderException("Failed to get a history provider", e);
+    }
+  }
+
+  @Override
+  public boolean isHistoryModeEnabled() {
+    return cache.isHistoryModeEnabled();
+  }
+
 }
