2ae0d1e6dc6d367e0f9bda5a04cb876b5babe28e
vdeila <vdeila@cern.ch>
2011-08-24 15:48:01 +0000

    TIMS-114: Move the history player from TIM1 to TIM2 TIMS-139: Speed up history requests by using the snapshot data TIMS-149: Make the history player request on UTC time TIMS-150: Add supervision events to the history charts and tables

---
+++ c2mon-client/c2mon-client-history/src/java/cern/c2mon/client/history/data/filter/DailySnapshotSmartFilter.java
@@ -0,0 +1,187 @@
+/******************************************************************************
+ * This file is part of the Technical Infrastructure Monitoring (TIM) project.
+ * See http://ts-project-tim.web.cern.ch
+ * 
+ * Copyright (C) 2004 - 2011 CERN This program is free software; you can
+ * redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received
+ * a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ * 
+ * Author: TIM team, tim.support@cern.ch
+ *****************************************************************************/
+package cern.c2mon.client.history.data.filter;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import cern.c2mon.client.common.history.HistoryTagValueUpdate;
+import cern.c2mon.client.common.history.Timespan;
+
+/**
+ * This class is used to check if a day of history is needed to be loaded for a
+ * tag or not. Is based on data from the daily snapshot table.
+ * 
+ * @author vdeila
+ * 
+ */
+public class DailySnapshotSmartFilter {
+
+  /** The threshold for a timespan to be added */
+  private static final Long IGNORE_TIMESPAN_SHORTER_THAN = 24L * 60L * 60L * 1000L;
+  
+  /** The values from the daily snapshot table */
+  private final Map<Long, List<Timespan>> timespansToSkip;
+
+  /**
+   * 
+   * @param dailySnapshotValues
+   *          The values from the daily snapshot table, which can be used to do
+   *          filtering.
+   * 
+   * @see {@link #addDailySnapshotValues(Collection)}
+   */
+  public DailySnapshotSmartFilter(final Collection<HistoryTagValueUpdate> dailySnapshotValues) {
+    this();
+    addDailySnapshotValues(dailySnapshotValues);
+  }
+
+  /**
+   * 
+   * @see {@link #addDailySnapshotValues(Collection)}
+   */
+  public DailySnapshotSmartFilter() {
+    this.timespansToSkip = new HashMap<Long, List<Timespan>>();
+  }
+
+  /**
+   * 
+   * @param dailySnapshotValues
+   *          The values from the daily snapshot table
+   */
+  public void addDailySnapshotValues(final Collection<HistoryTagValueUpdate> dailySnapshotValues) {
+    // Puts the values for each tag id into a list
+    final Map<Long, List<HistoryTagValueUpdate>> valuesByTagId = new HashMap<Long, List<HistoryTagValueUpdate>>();
+    for (final HistoryTagValueUpdate value : dailySnapshotValues) {
+      List<HistoryTagValueUpdate> values = valuesByTagId.get(value.getId());
+      if (values == null) {
+        values = new ArrayList<HistoryTagValueUpdate>();
+        valuesByTagId.put(value.getId(), values);
+      }
+      values.add(value);
+    }
+
+    // Comparator used for sorting each list ascending
+    final Comparator<HistoryTagValueUpdate> comparator = new Comparator<HistoryTagValueUpdate>() {
+      @Override
+      public int compare(final HistoryTagValueUpdate o1, final HistoryTagValueUpdate o2) {
+        return o1.getLogTimestamp().compareTo(o2.getLogTimestamp());
+      }
+    };
+
+    // Finds and adds the days which can be skipped
+    for (final Long tagId : valuesByTagId.keySet()) {
+      final List<HistoryTagValueUpdate> listValues = valuesByTagId.get(tagId);
+      final HistoryTagValueUpdate[] values = listValues.toArray(new HistoryTagValueUpdate[0]);
+      final List<Timespan> tagTimespans = new ArrayList<Timespan>();
+      Arrays.sort(values, comparator);
+      for (int i = 0; i < values.length; i++) {
+        final Timestamp end = values[i].getLogTimestamp();
+        final Timestamp start = values[i].getServerTimestamp();
+        if (end.getTime() - start.getTime() >= IGNORE_TIMESPAN_SHORTER_THAN) {
+          tagTimespans.add(new Timespan(start, end));
+        }
+      }
+      if (tagTimespans.size() > 0) {
+        // Merges timespans which overlap, assuming the list is sorted ascending
+        Timespan current = tagTimespans.get(0);
+        for (int i = 1; i < tagTimespans.size(); i++) {
+          Timespan previous = current;
+          current = tagTimespans.get(i);
+          
+          if (current.getStart().before(previous.getEnd())) {
+            // Removes the previous one
+            tagTimespans.remove(i - 1);
+            i--;
+            
+            // Updates the current timespan to overlap the area of them both
+            if (previous.getStart().before(current.getStart())) {
+              current.setStart(previous.getStart());
+            }
+          }
+        }
+        
+        timespansToSkip.put(tagId, tagTimespans);
+      }
+    }
+  }
+
+  /**
+   * 
+   * @param tagId
+   *          the tag id
+   * @param time
+   *          the time which will be inside the timespan
+   * @return if <code>time</code> is inside a period which can be skipped, it
+   *         will return the timespan of this period. <code>null</code> if the
+   *         time isn't inside any timespan which can be skipped.
+   */
+  public Timespan getTimespan(final Long tagId, final Timestamp time) {
+    final List<Timespan> timespans = this.timespansToSkip.get(tagId);
+    if (timespans != null) {
+      for (final Timespan timespan : timespans) {
+        if (timespan.getStart().compareTo(time) <= 0
+            && timespan.getEnd().compareTo(time) >= 0) {
+          return new Timespan(timespan);
+        }
+      }
+    }
+    return null;
+  }
+
+  /**
+   * 
+   * @param tagId
+   *          the tag id
+   * @return a list of days which can be skipped
+   */
+  public Collection<Timespan> getTimespansToSkip(final Long tagId) {
+    final List<Timespan> result = new ArrayList<Timespan>();
+    final List<Timespan> timespans = this.timespansToSkip.get(tagId);
+    if (timespans != null) {
+      for (Timespan timespan : timespans) {
+        result.add(new Timespan(timespan));
+      }
+    }
+    return result;
+  }
+
+  /**
+   * 
+   * @param tagId
+   *          the tag id of the values to remove (which will be forgotten by the
+   *          smart filtering)
+   */
+  public void deleteFilter(final Long tagId) {
+    this.timespansToSkip.remove(tagId);
+  }
+  
+  /**
+   * Removes all information / data
+   */
+  public void clear() {
+    this.timespansToSkip.clear();
+  }
+
+}
---
+++ c2mon-client/c2mon-client-history/src/java/cern/c2mon/client/history/data/utilities/WorkManager.java
@@ -0,0 +1,95 @@
+/******************************************************************************
+ * This file is part of the Technical Infrastructure Monitoring (TIM) project.
+ * See http://ts-project-tim.web.cern.ch
+ * 
+ * Copyright (C) 2004 - 2011 CERN This program is free software; you can
+ * redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received
+ * a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ * 
+ * Author: TIM team, tim.support@cern.ch
+ *****************************************************************************/
+package cern.c2mon.client.history.data.utilities;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * This class is used to keep track of elements that are currently being worked
+ * on. This class supports multithreading as all calls are thread safe.
+ * 
+ * Example of the logic: 
+ * 1. You add a list of elements that you want to
+ * start to work on. All the elements in the parameter is returned
+ * 2. You add another list of elements that you want to start
+ * to work on, but returns only the tags which is not already being worked on.
+ * 3. You unregister the tags you are finish working on.
+ * 
+ * @author vdeila
+ * 
+ * @param <T>
+ *          The type of elements you would like to add
+ */
+public class WorkManager<T> {
+  /**
+   * The list of elements that is currently being processed
+   */
+  private final List<T> currentlyProcessing = new ArrayList<T>();
+
+  /**
+   * The lock to access <code>currentlyProcessing</code>
+   */
+  private final ReentrantReadWriteLock currentlyProcessingLock = new ReentrantReadWriteLock();
+
+  /**
+   * 
+   * @param elements
+   *          The elements to unregister work on
+   */
+  public void unregisterWork(final Collection<T> elements) {
+    try {
+      this.currentlyProcessingLock.writeLock().lock();
+      this.currentlyProcessing.removeAll(elements);
+    }
+    finally {
+      this.currentlyProcessingLock.writeLock().unlock();
+    }
+  }
+
+  /**
+   * 
+   * @param elements
+   *          The elements you want to register
+   * @return A collection of the registered elements. If the <code>elements</code>
+   *         parameter contains an element which was not returned it means that
+   *         this element already is being worked on.
+   */
+  public Collection<T> registerWork(final Collection<T> elements) {
+    // Keeps a list of elements that are registered
+    final List<T> registeredElements = new ArrayList<T>();
+    try {
+      this.currentlyProcessingLock.writeLock().lock();
+
+      // Iterates through the list to see which is already being worked on
+      for (T element : elements) {
+        if (!this.currentlyProcessing.contains(element)) {
+          if (this.currentlyProcessing.add(element)) {
+            registeredElements.add(element);
+          }
+        }
+      }
+    }
+    finally {
+      this.currentlyProcessingLock.writeLock().unlock();
+    }
+    return registeredElements;
+  }
+}
