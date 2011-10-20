28957f77c2f3b31c03a618d30742ef0c8fda0301
Felix Ehm <felix.ehm@cern.ch>
2011-10-20 17:11:43 +0000

    APS-1756

--- c2mon-server/c2mon-server-laser/src/java/cern/c2mon/server/laser/publication/LaserPublisher.java
+++ c2mon-server/c2mon-server-laser/src/java/cern/c2mon/server/laser/publication/LaserPublisher.java
@@ -1,11 +1,25 @@
 package cern.c2mon.server.laser.publication;
 
+import java.sql.Timestamp;
+import java.util.Properties;
+
 import javax.annotation.PostConstruct;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.SmartLifecycle;
 import org.springframework.stereotype.Service;
 
+import cern.laser.source.alarmsysteminterface.ASIException;
+import cern.laser.source.alarmsysteminterface.AlarmSystemInterface;
+import cern.laser.source.alarmsysteminterface.AlarmSystemInterfaceFactory;
+import cern.laser.source.alarmsysteminterface.FaultState;
+//import cern.laser.source.ext.AlarmInstance;
+//import cern.laser.source.ext.AlarmInstanceBuilder;
+//import cern.laser.source.ext.AlarmInstanceManager;
+//import cern.laser.source.ext.AlarmSourceFactory;
+//import cern.laser.source.ext.AlarmSourceManager;
+//import cern.laser.source.ext.impl.FaultStateEncoder;
 import cern.tim.server.cache.CacheRegistrationService;
 import cern.tim.server.cache.TimCacheListener;
 import cern.tim.server.common.alarm.Alarm;
@@ -14,78 +28,183 @@ import cern.tim.server.common.config.ServerConstants;
 /**
  * Bean responsible for submitting C2MON alarms to LASER.
  */
-//starts as singleton bean in Spring context
+// starts as singleton bean in Spring context
 @Service
 public class LaserPublisher implements TimCacheListener<Alarm>, SmartLifecycle {
 
-  /**
-   * Flag for lifecycle calls.
-   */
-  private volatile boolean running = false;
-  
-  /**
-   * Service for registering as listener to C2MON caches.
-   */
-  private CacheRegistrationService cacheRegistrationService;
-    
-  /**
-   * Autowired constructor.
-   * @param cacheRegistrationService the C2MON cache registration service bean
-   */
-  @Autowired
-  public LaserPublisher(final CacheRegistrationService cacheRegistrationService) {
-    super();
-    this.cacheRegistrationService = cacheRegistrationService;
-  }
-  
-  /**
-   * Called at server startup.
-   */
-  @PostConstruct
-  public void init() {
-    cacheRegistrationService.registerToAlarms(this);
-  }
-  
-  @Override
-  public void notifyElementUpdated(Alarm cacheable) {
-    //TODO submit alarm to laser
-  }
-  
-  
-  //below server lifecycle methods: complete start/stop (no need to allow for stop/restart, just final shutdown)
-  @Override
-  public boolean isAutoStartup() {
-    return true;
-  }
+	/**
+	 * The alarm source name this publisher is called.
+	 */
+	private String sourceName;
 
-  @Override
-  public void stop(Runnable callback) {
-    stop();
-    callback.run();
-  }
+	/**
+	 * Our Logger
+	 */
+	private Logger log = Logger.getLogger(LaserPublisher.class);
 
-  @Override
-  public boolean isRunning() {
-    return running;
-  }
+//	private AlarmSourceManager sourceManager = null;
 
-  @Override
-  public void start() {    
-    running = true;
-    //TODO start LASER connection
-  }
+	/** Reference to the LASER alarm system interface. */
+	private AlarmSystemInterface asi = null;
 
-  @Override
-  public void stop() {
-    running = false;
-    //TODO wait for any LASER publication to end and close resources
-  }
+	/**
+	 * Flag for lifecycle calls.
+	 */
+	private volatile boolean running = false;
 
-  @Override
-  public int getPhase() {
-    return ServerConstants.PHASE_STOP_LAST;
-  }
+	/**
+	 * Service for registering as listener to C2MON caches.
+	 */
+	private CacheRegistrationService cacheRegistrationService;
 
+	/**
+	 * Autowired constructor.
+	 * 
+	 * @param cacheRegistrationService
+	 *            the C2MON cache registration service bean
+	 */
+	@Autowired
+	public LaserPublisher(final CacheRegistrationService cacheRegistrationService) {
+		super();
+		this.cacheRegistrationService = cacheRegistrationService;
 
+	}
+
+	/**
+	 * 
+	 * @param sourceName
+	 *            the alarm source name this publisher should be called.
+	 */
+	public void setSourceName(String sourceName) {
+		this.sourceName = sourceName;
+	}
+
+	/**
+	 * 
+	 * @return the alarm source name this publisher is called.
+	 */
+	public String getSourceName() {
+		return sourceName;
+	}
+
+	/**
+	 * Called at server startup.
+	 * 
+	 * @throws Exception
+	 *             in case the underlying alarm system could not be initiated.
+	 */
+	@PostConstruct
+	public void init() throws Exception {
+		cacheRegistrationService.registerToAlarms(this);
+//		sourceManager = AlarmSourceFactory.getSourceManager();
+//		sourceManager.setEnabled(true);
+//		sourceManager.setSourceName(getSourceName());
+//		sourceManager.init();
+
+		asi = AlarmSystemInterfaceFactory.createSource(getSourceName());
+	}
+
+	@Override
+	public void notifyElementUpdated(Alarm cacheable) {
+
+		FaultState fs = null;
+		fs = AlarmSystemInterfaceFactory.createFaultState(cacheable.getFaultFamily(), cacheable.getFaultMember(), cacheable.getFaultCode()); 
+		
+		if (cacheable.isActive()){
+			fs.setDescriptor(cacheable.getState());
+			fs.setUserTimestamp(cacheable.getTimestamp());
+	
+			if (cacheable.getInfo() != null) {
+				Properties prop = fs.getUserProperties();
+				prop.put(FaultState.ASI_PREFIX_PROPERTY, cacheable.getInfo());
+				fs.setUserProperties(prop);
+			}
+		}else{
+			fs.setDescriptor(FaultState.TERMINATE);
+		    fs.setUserTimestamp(new Timestamp(System.currentTimeMillis()));
+		}
+		
+		if (log.isDebugEnabled()) {
+			log.debug("Pushing alarm to LASER :\n" + fs);
+		}
+		boolean result = true;
+		try {
+			asi.push(fs);
+		} catch (ASIException e) {
+			// Ooops, didn't work. log the exception.
+			result = false;
+			StringBuilder str = new StringBuilder("Alarm System Interface Exception. Unable to send FaultState ");
+			str.append(cacheable.getFaultFamily());
+			str.append(':');
+			str.append(cacheable.getFaultMember());
+			str.append(':');
+			str.append(cacheable.getFaultCode());
+			str.append(" to LASER.");
+			log.error(str, e);
+		} catch (Exception e) {
+			result = false;
+			StringBuilder str = new StringBuilder("sendFaultState() : Unexpected Exception. Unable to send FaultState ");
+			str.append(cacheable.getFaultFamily());
+			str.append(':');
+			str.append(cacheable.getFaultMember());
+			str.append(':');
+			str.append(cacheable.getFaultCode());
+			str.append(" to LASER.");
+			log.error(str, e);
+		}
+		// Keep track of the sent alarm in the Alarm log
+		StringBuilder str = new StringBuilder();
+		str.append(cacheable.getTimestamp());
+		str.append("\t");
+		str.append(cacheable.getFaultFamily());
+		str.append(':');
+		str.append(cacheable.getFaultMember());
+		str.append(':');
+		str.append(cacheable.getFaultCode());
+		str.append('\t');
+		str.append(cacheable.getState());
+		if (cacheable.getInfo() != null) {
+			str.append('\t');
+			str.append(cacheable.getInfo());
+		}
+		if (result)
+			log.info(str);
+		else
+			log.error(str);
+	}
+
+	// below server lifecycle methods: complete start/stop (no need to allow for
+	// stop/restart, just final shutdown)
+	@Override
+	public boolean isAutoStartup() {
+		return true;
+	}
+
+	@Override
+	public void stop(Runnable callback) {
+		stop();
+		callback.run();
+	}
+
+	@Override
+	public boolean isRunning() {
+		return running;
+	}
+
+	@Override
+	public void start() {
+		running = true;
+	}
+
+	@Override
+	public void stop() {
+		asi.close();
+		running = false;
+	}
+
+	@Override
+	public int getPhase() {
+		return ServerConstants.PHASE_STOP_LAST;
+	}
 
 }
