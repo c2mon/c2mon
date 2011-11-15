d46e7982e0463c91fe6cc510c72c40b9ad3f4a9f
Mark Brightwell <markbrightwell@gmail.com>
2011-11-15 13:44:45 +0000

    TIMS-214 - initial commit of statistics-generator deploy project

---
+++ c2mon-statistics/c2mon-generator-deploy/modules/tim2/bin/statistics-generator.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+#STATISTICS PACKAGE: CHART WEB DEPLOYER
+#
+#This script is part of the statistics module that calculates
+#and displays statistics on TIM.
+#
+#This script generates the charts and html fragments
+#for the statistics module, and saves them in the appropriate
+#directories.
+#
+#It must be run on the machine that is hosting the chart and html
+#directories.
+#
+#To run on dev, switch HOME_TEST from dist to dev
+#
+
+########
+# JAVA #
+########
+
+export JAVA_HOME=/usr/java/jdk
+export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH/
+JAVA_BIN=$JAVA_HOME/jre/bin
+
+
+
+###############
+# DIRECTORIES #
+###############
+
+# get the current location
+SCRIPT=`readlink -f $(which $0)`
+SCRIPTPATH=`dirname $SCRIPT`
+STATISTICS_GENERATOR_HOME=$SCRIPTPATH/..
+
+#log director
+STATISTICS_GENERATOR_LOG_HOME=$STATISTICS_GENERATOR_HOME/log
+
+#configuration directory location
+STATISTICS_GENERATOR_CONF_HOME=$STATISTICS_GENERATOR_HOME/conf
+
+#name of web home directory on server root
+WEB_HOME=tim-web-statistics
+
+
+
+#################
+# CONFIGURATION #
+#################
+
+#log4j configuration file
+LOG4J_CONF_FILE=$STATISTICS_GENERATOR_CONF_HOME/log4j.xml
+
+#webpage chart configuration file
+GRAPH_CONF_FILE=$STATISTICS_GENERATOR_CONF_HOME/webconfig.xml
+
+# the directory to deploy to
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+#!! the image and html subdirectories of this directory    !!#
+#!! are deleted recursively at each run (if R option is    !!#
+#!! present)! Handle with care!                            !!#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+DEPLOY_HOME=$STATISTICS_GENERATOR_HOME/charts                                  #/opt/apache-tomcat/webapps/$WEB_HOME
+
+#directory name under which to save chart images
+IMAGE_DIR=chart-images
+
+#directory name under which to save chart html fragments
+HTML_DIR=chart-html
+
+
+##############
+# LIBRAIRIES #
+##############
+
+#run the java package
+
+. C2MON-STATISTICS-GENERATOR.jvm &
