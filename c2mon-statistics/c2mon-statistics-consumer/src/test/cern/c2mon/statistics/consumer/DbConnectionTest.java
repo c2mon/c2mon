827ec46b34d626e5bea10d4c9e2d0a894e8bfa3c
Mark Brightwell <markbrightwell@gmail.com>
2011-11-16 08:38:37 +0000

    TIMS-201 - in statistics consumer, added DB connection test and fixed type alias; added c2mon.test.properties support for testing

---
+++ c2mon-statistics/c2mon-statistics-consumer/src/test/cern/c2mon/statistics/consumer/DbConnectionTest.java
@@ -0,0 +1,23 @@
+package cern.c2mon.statistics.consumer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+
+/**
+ * Tests the DB connection starts up correctly.
+ * 
+ * @author Mark Brightwell
+ *
+ */
+public class DbConnectionTest {
+
+  @Test
+  public void initSqlMap() {
+    SqlMapClient client = SqlMapper.getSqlMapInstance();
+    assertNotNull(client);
+  }
+  
+}
