f7eb4ea762839cd30c49b2bc2f601a03bc08df3c
Matthias Braeger <matthias.braeger@cern.ch>
2012-06-19 12:38:20 +0000

    TIMS-599: Refactoring of the deployment project for c2mon-publisher-rda

---
+++ c2mon-publisher/c2mon-publisher-rda-deploy/modules/dmn2/bin/update-config-dev.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+PRGDIR=`dirname "$PRG"`
+
+# Set PUBLISHER_HOME
+PUBLISHER_HOME=`cd "$PRGDIR/.." >/dev/null; pwd`
+
+#the script to be called to get the rda-published tag id from db 
+JAPC_PUB_TID_GENERATOR=${PUBLISHER_HOME}/bin/tid-generator-dev.pl
+
+
+# The configuration file destination
+CONF_FILE=$PUBLISHER_HOME/conf/publisher.tid
+TEMP_FILE=$PUBLISHER_HOME/conf/publisher-new.tid 
+
+# Get the new configuration
+`${JAPC_PUB_TID_GENERATOR}`
+
+
+# If the new configuration is different from the old one:
+# (1) create a backup of the current configuration
+# (2) replace the current configuration by the new one
+
+if [ -s $CONF_FILE ]; then
+  diff $CONF_FILE $TEMP_FILE >/dev/null
+  if [ $? -eq 1 ] ; then
+    echo "copying file..."
+    cp $CONF_FILE $CONF_FILE.`date +%y%m%d_%k%M%S`
+    mv $TEMP_FILE $CONF_FILE
+    
+    echo "Configuration has changed! The Publisher will automatically subscribe to any new tag IDs within the next 60 seconds."
+  else
+    echo "The configuration has not changed since the last update!"
+    rm -rf $TEMP_FILE 2>&1 > /dev/null  
+  fi
+else
+  mv $TEMP_FILE $CONF_FILE
+  echo "Configuration has changed! The Publisher will automatically subscribe to any new tag IDs within the next 60 seconds."
+fi
