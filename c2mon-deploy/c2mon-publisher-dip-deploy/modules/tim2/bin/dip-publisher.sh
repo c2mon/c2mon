b1020fe1bb794e277c12c452f67d0cb51b3c0753
Matthias Braeger <matthias.braeger@cern.ch>
2012-07-03 10:50:37 +0000

    TIMS-611: dip publisher start script improvement

--- c2mon-deploy/c2mon-publisher-dip-deploy/modules/tim2/bin/dip-publisher.sh
+++ c2mon-deploy/c2mon-publisher-dip-deploy/modules/tim2/bin/dip-publisher.sh
@@ -1,20 +1,18 @@
-# TIM DIPPublisher. CERN. All rights reserved.
+#!/bin/sh
+
+# TIM DIP PUBLISHER
 #
-# T Nick:           Date:       Info:
-# -------------------------------------------------------------------------
-# P mbrightw   6/Dec/2010       Re-adding support for multiple DIP publishers
-# -------------------------------------------------------------------------
+# Author: Matthias Braeger (GS-ASE-SSE)
+# Date:   July 2012
+#
+# This script is used to launch the DIP Publisher on a linux
+# server. 
+# Please be aware that this script will NOT run from 
+# everywhere but from the APP_HOME directory.  
 
-# Start/stop/restart TIM DIPPublisher
 
-# Email address for notifications sent out by the silentcheck function
-NOTIFY=tim.support@cern.ch,ti.operation@cern.ch
-
-########
-# JAVA #
-########
-# Make sure the JAVA_BIN variable points to the java bin directory on Your machine
-JAVA_BIN=/usr/java/jdk/jre/bin
+# Notification list, in case of problems reported by silentcheck
+NOTIFY=tim.support@cern.ch
 
 # resolve links - $0 may be a softlink
 PRG="$0"
@@ -31,26 +29,84 @@ done
 
 PRGDIR=`dirname "$PRG"`
 
-# Set DIP_PUBLISHER_HOME
-DIP_PUBLISHER_HOME=`cd "$PRGDIR/.." >/dev/null; pwd`
+# application home which shall contain a /bin and /conf directory
+APP_HOME=`cd "$PRGDIR/.." >/dev/null; pwd`
+
+
+##############################
+# PROCESS specific variables #
+##############################
+
+PROCESS_COMMAND=$1
 
 #name of the DIP publisher passed as second argument
 export PROCESS_NAME=$2
 
-# The script which is actually calling the DIP publisher
-STARTUP_SCRIPT=${DIP_PUBLISHER_HOME}/bin/DIP-PUBLISHER-STARTUP.jvm
+# The script which is actually calling the RDA publisher
+STARTUP_SCRIPT=${APP_HOME}/bin/DIP-PUBLISHER-STARTUP.jvm
 
-# change into DIP_PUBLISHER_HOME
-cd $DIP_PUBLISHER_HOME
+TIME=`date +"%F %T.%3N"`
 
-PID_FILE="${DIP_PUBLISHER_HOME}/tmp/dippublisher_${PROCESS_NAME}.pid"
 
-export PATH=${JAVA_BIN}:$PATH
+###############
+# DIRECTORIES #
+###############
 
-# Call the runs function to find out if a process with
-# a certain pid runs. The "runs" function will return
-# 0 if the process is running, 1 if it is not.
-# #Example: runs 23049
+# configuration directory location
+PROCESS_CONF_HOME=$APP_HOME/conf
+
+#log file for this script
+SCRIPT_LOG_DIR=${APP_HOME}/script-log
+PROCESS_SCRIPT_LOG=$SCRIPT_LOG_DIR/$PROCESS_NAME.log
+if [ ! -d ${SCRIPT_LOG_DIR} ] ; then
+    mkdir ${SCRIPT_LOG_DIR}
+fi
+
+########
+# JAVA #
+########
+
+export JAVA_HOME=/usr/java/jdk
+export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH/
+JAVA_BIN=$JAVA_HOME/jre/bin
+
+############################
+# PID file and directories #
+############################
+
+PROCESS_HOST=`hostname -s`
+
+#Create the pid directory structure for the local host, if necessary
+if [ ! -d ${APP_HOME}/tmp/${PROCESS_HOST} ] ; then
+  mkdir ${APP_HOME}/tmp/${PROCESS_HOST}
+fi
+
+PID_FILE=$APP_HOME/tmp/$PROCESS_HOST/dippublisher_$PROCESS_NAME.pid
+
+########################### 
+# Source function library #
+###########################
+
+if [ -f /etc/init.d/functions ] ; then
+  . /etc/init.d/functions
+elif [ -f /etc/rc.d/init.d/functions ] ; then
+  . /etc/rc.d/init.d/functions
+else
+  exit 0
+fi
+
+
+###################
+# local functions #
+###################
+
+#---------------------------------------------------
+# The function will check whether a process with the
+# specified PID is currently running. It will return
+# 0 if the process is running, 1 if it isn't.
+#
+# Example: runs 23049
+#---------------------------------------------------
 runs() {
   pid=${1##*/}
   tmp=`ps -p $pid -o pid=`
@@ -61,120 +117,215 @@ runs() {
   fi
 }
 
+#-----------------------------------------------------------------
+# check if that process is already running on the local host
+# before starting it
+#-----------------------------------------------------------------
+PROCESS_start() {
+    cd ${APP_HOME}
+    
+    #if PID_FILE exists, check the process is not running anymore
+    if [ -f $PID_FILE ] ; then
+	pid=`cat $PID_FILE | awk {'print $1'}`
+	runs $pid
+	if [ $? -eq 1 ] ; then
+	    rm $PID_FILE
+	    really_start
+	else
+	 echo_warning
+         echo "A DIP publisher $PROCESS_NAME process seems to be running on host $PROCESS_HOST. Stop it first."
+         echo -e "$TIME\t$PROCESS_NAME\t$PROCESS_HOST\tSTART\tFAILED\t(running)" >> $PROCESS_SCRIPT_LOG
+	fi
+    else
+	really_start
+    fi   
+}
 
-DIPPublisher_start() {
 
-  cd ${DIP_PUBLISHER_HOME}
+#--------------------------------------------------------------------
+# start a process on the local machine on which the script is running
+#--------------------------------------------------------------------
+really_start() {
+  echo -n "Starting $PROCESS_NAME process on host ${PROCESS_HOST} ..."
+  ${STARTUP_SCRIPT} > ${APP_HOME}/log/${PROCESS_NAME}.out.log 2>&1 &
 
-  if [ -f $PID_FILE ] ; then
-    echo "Could not start DIP Publisher ${PROCESS_NAME} !"
-    echo "DipPublisher with that name is already running. Stop it first, please";
+  pid="$!"
+  sleep 5
+  runs $pid
+
+  if [ $? -eq 1 ] ; then
+    echo_failure
+    echo "DIP Publisher $PROCESS_NAME process could not be started."
+    echo
+    echo -e "$TIME\t$PROCESS_NAME\t$PROCESS_HOST\tSTART\tFAILED" >> $PROCESS_SCRIPT_LOG
+      
   else
-   echo "Starting DIP Publisher ${PROCESS_NAME}.."
-   ${STARTUP_SCRIPT} > ${DIP_PUBLISHER_HOME}/log/${PROCESS_NAME}.out.log 2>&1 &
-   echo "$!" > ${PID_FILE}
+    echo "$pid" > ${PID_FILE}
+    echo_success
+    echo
+    echo -e "$TIME\t$PROCESS_NAME\t$PROCESS_HOST\tSTART\tOK" >> $PROCESS_SCRIPT_LOG
   fi
+}
 
+#--------------------------------------------------------
+# stop the process, if it is running on the local machine
+#--------------------------------------------------------
+PROCESS_stop() {
+  cd $APP_HOME
+
+  #if PID file exists
+  if [ -f $PID_FILE ] ; then
+	echo -n "Stopping the DIP publisher $PROCESS_NAME process on ${PROCESS_HOST} ..."
+	pid=`cat $PID_FILE | awk {'print $1'}`
+	kill $pid >/dev/null 2>&1
+	runs $pid
+	proc_runs=$?
+	proc_wait=0
+	while [ $proc_runs -eq 0 ] ; do
+	    echo -n .
+	    sleep 1
+	    if [ $proc_wait -lt 10 ] ; then
+		let proc_wait=$proc_wait+1
+		runs $pid
+		proc_runs=$?
+	    else
+		proc_runs=1
+	    fi
+	done
+	runs $pid
+
+	if [ $? -eq 0 ] ; then  
+	    echo_warning
+	    echo
+	    echo -n "Unable to stop the $PROCESS_NAME Process gently... killing it..."
+	    kill -9 $pid
+	    sleep 1
+	    runs $pid
+
+	    if [ $? -eq 1 ] ; then
+		rm -f $PID_FILE 
+		echo_success
+		echo
+		echo -e "$TIME\t$PROCESS_NAME\t$PROCESS_HOST\tSTOP\tOK\t(kill -9)" >> $PROCESS_SCRIPT_LOG
+		RETVAL=0
+	    else       
+		echo_failure
+		echo
+		echo -e "$TIME\t$PROCESS_NAME\t$PROCESS_HOST\tSTOP\tFAILED" >> $PROCESS_SCRIPT_LOG
+		echo "Unable to stop the $PROCESS_NAME Process."
+		RETVAL=1
+	    fi
+
+	else
+	    echo_success
+	    echo
+	    echo -e "$TIME\t$PROCESS_NAME\t$PROCESS_HOST\tSTOP\tOK" >> $PROCESS_SCRIPT_LOG
+	    rm -f $PID_FILE
+	fi
+	
+    #if PID file does not exist, is not running
+    else
+	echo "No $PROCESS_NAME Process seems to be running on ${PROCESS_HOST}"
+    fi;
 }
 
 
-DIPPublisher_stop() {
-
- cd ${DIP_PUBLISHER_HOME}
-
- if [ -f $PID_FILE ] ; then
-   echo "Stopping DIP Publisher ${PROCESS_NAME}"
-   pid=`cat $PID_FILE`
-   kill -9 $pid >/dev/null 2>&1
-   rm -f $PID_FILE
- fi
-
-}
-
-DIPPublisher_status() {
-
- cd ${DIP_PUBLISHER_HOME}
-
- echo "Checking status of DIP Publisher ${PROCESS_NAME}"
-
- if [ -f $PID_FILE ] ; then
-   pid=`cat $PID_FILE`
-   runs $pid
-   if [ $? -eq 0 ] ; then
-     echo "The dippublisher service $PROCESS_NAME is running (pid=$pid)."
-     RETVAL=0
-   else
-     echo "A pid file exists ($PID_FILE) but the dippublisher process $PROCESS_NAME is not running."
-     RETVAL=1
-   fi
- else
-   echo "The dippublisher service $PROCESS_NAME does not seem to be running."
-   RETVAL=2
- fi
- return $RETVAL
+#----------------------------------------------------------------
+# check the status of the $PROCESS_NAME process on the local machine:
+# is an instance running or not?
+#----------------------------------------------------------------
+PROCESS_status() {
+  if [ -f $PID_FILE ]; then
+      pid=`cat $PID_FILE | awk {'print $1'}`
+      runs $pid
+      
+      if [ $? -eq 0 ] ; then
+	  echo "RUNNING (host: $PROCESS_HOST  pid: $pid)"
+	  RETVAL=0
+      else
+	  echo "DEAD - cleaning PID file"
+	  rm -f $PID_FILE
+	  RETVAL=1
+      fi
+  else
+      echo "STOPPED"
+      RETVAL=1
+  fi
+  exit $RETVAL
 }
 
 
-
-DIPPublisher_restart() {
-
-  DIPPublisher_stop
+#----------------------------------------------------------------
+# Restarts the $PROCESS_NAME process on the local machine
+#----------------------------------------------------------------
+PROCESS_restart() {
+  PROCESS_stop
   sleep 1
-  DIPPublisher_start
-
+  PROCESS_start
 }
 
+
+#----------------------------------------------------------------
 # This option is useful for a cronjob in order to
-# check whether the dippublisher is still running.
-# If this is not the case the dippublisher is restarted
-DIPPublisher_silentcheck() {
-  
-  cd ${DIP_PUBLISHER_HOME}
-  
+# check whether the $PROCESS_NAME is still running.
+# If this is not the case the $PROCESS_NAME is restarted
+#----------------------------------------------------------------
+PROCESS_silentcheck() {
   if [ -f $PID_FILE ]; then
     pid=`cat $PID_FILE`
     runs $pid
     if [ $? -ne 0 ]; then
-      cp -Rfp ${DIP_PUBLISHER_HOME}/log ${DIP_PUBLISHER_HOME}/`date +log_%Y-%m-%d_%H:%M:%S`
-      rm ${DIP_PUBLISHER_HOME}/log/${PROCESS_NAME}.*
-      DIPPublisher_restart
-      echo "The status check script has restarted the dippublisher process $PROCESS_NAME on $HOSTNAME. Please check why the process was down (crash) and if it is starting up correctly. A backup of the log files was created in ${DIP_PUBLISHER_HOME}/`date +log_%Y-%m-%d_%H:%M:%S`" | mail -s "Automatic dippublisher restart on $HOSTNAME" $NOTIFY
+      cp -Rfp ${APP_HOME}/log/${PROCESS_NAME}.* ${APP_HOME}/${PROCESS_NAME}_`date +log_%Y-%m-%d_%H:%M:%S`
+      rm ${APP_HOME}/log/${PROCESS_NAME}.*
+      PROCESS_restart
+      echo "The status check script has restarted the DIP publisher process $PROCESS_NAME on $HOSTNAME. Please check why the process was down (crash) and if it is starting up correctly. A backup of the log files was created in ${APP_HOME}/${PROCESS_NAME}_`date +log_%Y-%m-%d_%H:%M:%S`" | mail -s "Automatic DIP publisher $PROCESS_NAME process restart on $HOSTNAME" $NOTIFY
     fi
   fi
 }
 
-
-
-if [ "$2" == "" -o "$2" != "DIPPub01" -a "$2" != "DIPPub02" ] ; then
-  echo "**************************************************************"
-  echo " usage:"
-  echo "   $0 start|stop|restart|status publisher_name"
-  echo " e.g: $0 start DIPPub01"
-  echo "**************************************************************"
-else
-  case "$1" in
-
-   'start')
-     DIPPublisher_start
-     ;;
-   'stop')
-     DIPPublisher_stop
-     ;;
-   'restart')
-     DIPPublisher_restart
-     ;;
-    'status')
-     DIPPublisher_status
-     ;;
-   'silentcheck')
-     DIPPublisher_silentcheck
-     ;;
-   *)
-    echo "**************************************************************"
+#----------------------------------------------------------------
+# Prints the basic usage information
+#----------------------------------------------------------------
+PROCESS_printBasicUsageInfo() {
+    echo "*****************************************************************"
     echo " usage:"
-    echo "   $0 start|stop|restart|status publisher_name"
+    echo " $0 [start|stop|restart|status|silentcheck] <publisher-name>"
     echo " e.g: $0 start DIPPub01"
-    echo "**************************************************************"
+    echo
+    echo " Known publishers are: DIPPub01, DIPPub02"
+    echo "*****************************************************************"
+}
+
+##############################################################################
+############################### MAIN ROUTINE #################################
+##############################################################################
+
+if [ "$PROCESS_NAME" == "" -o "$PROCESS_NAME" != "DIPPub01" -a "$PROCESS_NAME" != "DIPPub02" ] ; then
+  PROCESS_printBasicUsageInfo
+else 
+  case "$PROCESS_COMMAND" in 
+    
+      'start')
+	  PROCESS_start
+	  ;;
+
+      'stop')
+	  PROCESS_stop
+	  ;;
+
+      'restart')
+	  PROCESS_restart
+	  ;;
+      'status')
+	  PROCESS_status
+	  ;;
+      'silentcheck')
+	  PROCESS_silentcheck
+	  ;;
+
+      *)
+	  PROCESS_printBasicUsageInfo
+	  ;;
   esac
 fi
 
