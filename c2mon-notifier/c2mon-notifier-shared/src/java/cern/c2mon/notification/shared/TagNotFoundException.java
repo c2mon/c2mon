e637f80e1a6762225368f9cc09a386624bd24185
Felix Ehm <felix.ehm@cern.ch>
2012-07-10 12:38:42 +0000

    new Exception for non-existing tag

---
+++ c2mon-notifier/c2mon-notifier-shared/src/java/cern/c2mon/notification/shared/TagNotFoundException.java
@@ -0,0 +1,19 @@
+/*
+ * $Id $
+ *
+ * $Date$
+ * $Revision$
+ * $Author$
+ *
+ * Copyright CERN ${year}, All Rights Reserved.
+ */
+package cern.c2mon.notification.shared;
+
+
+public class TagNotFoundException extends ServiceException {
+
+    public TagNotFoundException(String msg) {
+        super(msg);
+    }
+    
+}
