195bb4637dfe8392dcaddd6dd2f1a7892b444717
Felix Ehm <felix.ehm@cern.ch>
2012-07-16 07:30:17 +0000

    updated dependencies and increased version

--- c2mon-notifier/c2mon-client-notification/src/test/cern/c2mon/client/notification/Example.java
+++ c2mon-notifier/c2mon-client-notification/src/test/cern/c2mon/client/notification/Example.java
@@ -9,39 +9,32 @@
  */
 package cern.c2mon.client.notification;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import cern.c2mon.notification.shared.Subscriber;
 
 public class Example {
-
-    /** The path to the core Spring XML */
-    private static final String APPLICATION_SPRING_XML_PATH = "cern/c2mon/client/notification/config/client.xml";
     
     public static void main(String [] args) {
         
         System.setProperty("log4j.configuration", "cern/c2mon/client/notification/config/log4j.properties");
         
-        final ClassPathXmlApplicationContext xmlContext = 
-            new ClassPathXmlApplicationContext(APPLICATION_SPRING_XML_PATH);
         
-        
-        NotificationService service = xmlContext.getBean(NotificationServiceImpl.class);
+        NotificationService service = NotificationGateway.getService();
         try {
             Subscriber s = service.getSubscriber("felixehm");
-            service.removeSubscription(s, 1069821L);
+
+
+            service.subscribe(s.getUserName(), 2138729L);
             
-            s = service.getSubscriber("felixehm");
-            if (s.getSubscriptions().get(1069821L) != null) {
-                System.err.println("Still there!");
-            }
+            /**
+             * ! important otherwise, this spring context will not exit! 
+             */
+            NotificationGateway.close();
             
         }catch(Exception ex) {
             ex.printStackTrace();
             System.exit(1);
         }
         
-        xmlContext.close();
     }
 
 }
