515fbfac725e0b17dd6711b925732c50c9752196
Wojciech Buczak <wojciech.buczak@gmail.com>
2012-07-16 09:05:46 +0000

    japc_rule_pub_flag  renamed to: japc_limit_pub_flag

--- c2mon-deploy/c2mon-publisher-rda-deploy/modules/dmn2/bin/tid-generator.pl
+++ c2mon-deploy/c2mon-publisher-rda-deploy/modules/dmn2/bin/tid-generator.pl
@@ -47,7 +47,7 @@ my $dbh = DBI->connect( $dbiUrl, $dbiUser, $dbiPassword )
 my $fetch_equipments_sql = <<END;
 select metric_data_tag_id from dmn_metrics_v where japc_metric_pub_flag='Y'
 union
-select metric_rule_tag_id as metric_data_tag_id from dmn_metrics_v where japc_rule_pub_flag='Y'
+select metric_rule_tag_id as metric_data_tag_id from dmn_metrics_v where japc_limit_pub_flag='Y'
 END
 
 my $sth = $dbh->prepare("${fetch_equipments_sql}")  
