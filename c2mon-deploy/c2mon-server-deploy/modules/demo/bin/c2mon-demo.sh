97862a018d25fa2100786d85713a3b7e28e07e29
Mark Brightwell <markbrightwell@gmail.com>
2012-07-18 14:41:07 +0000

    TIMS-585 - fixed bug in script & incorrect module XML

--- c2mon-deploy/c2mon-server-deploy/modules/demo/bin/c2mon-demo.sh
+++ c2mon-deploy/c2mon-server-deploy/modules/demo/bin/c2mon-demo.sh
@@ -24,7 +24,7 @@ LOG_DIR=$C2MON_HOME/log
 
 CLASSPATH=`ls $C2MON_HOME/lib/*.jar | tr -s '\n' ':'`
 
-COMMON_JAVA_ARGS="-Xms512m -Xmx512 -XX:NewRatio=3 -XX:+PrintGCDetails -XX:+UseParallelGC -XX:MaxGCPauseMillis=100 -Dserver.process.name=$PROCESS_NAME -Dc2mon.process.name=$PROCESS_NAME -Dc2mon.home=$C2MON_HOME -Dlog4j.configuration=$LOG4J_CONF_FILE -Dc2mon.log.dir=$LOG_DIR -Dc2mon.properties.location=$C2MON_PROPERTIES"
+COMMON_JAVA_ARGS="-Xms512m -Xmx512m -XX:NewRatio=3 -XX:+PrintGCDetails -XX:+UseParallelGC -XX:MaxGCPauseMillis=100 -Dserver.process.name=$PROCESS_NAME -Dc2mon.process.name=$PROCESS_NAME -Dc2mon.home=$C2MON_HOME -Dlog4j.configuration=$LOG4J_CONF_FILE -Dc2mon.log.dir=$LOG_DIR -Dc2mon.properties.location=$C2MON_PROPERTIES"
 
 #uncomment for recovery start
 #C2MON_RECOVERY_ARG="-Dc2mon.recovery=true"
