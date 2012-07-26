e32800efcb9fc03c2483d8e2bf81665524f5e215
Emmanouil Koufakis <koufakis@outlook.com>
2012-07-26 15:56:09 +0000

    Refactoring.

--- c2mon-client/c2mon-web-configviewer/src/java/cern/c2mon/web/configviewer/service/ServiceGateway.java
+++ c2mon-client/c2mon-web-configviewer/src/java/cern/c2mon/web/configviewer/service/ServiceGateway.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import cern.c2mon.client.core.C2monCommandManager;
+import cern.c2mon.client.core.C2monHistoryManager;
 import cern.c2mon.client.core.C2monServiceGateway;
 import cern.c2mon.client.core.C2monTagManager;
 import cern.c2mon.web.configviewer.service.TagService;
@@ -16,56 +17,71 @@ import cern.c2mon.web.configviewer.service.TagService;
 @Service
 public class ServiceGateway {
 
-    /**
-     * ServiceGateway logger
-     * */
-    private static Logger logger = Logger.getLogger(TagService.class);
-    
-    /**
-     * Tag manager
-     * */
-    private C2monTagManager tagManager;
-    
-    /**
-     * Command manager
-     * */
-    private C2monCommandManager commandManager;
+  /**
+   * ServiceGateway logger
+   * */
+  private static Logger logger = Logger.getLogger(TagService.class);
 
-    /**
-     * Initializes the tagManager and commandManager
-     * */
-    @PostConstruct
-    public void init() {
-        startC2monServiceGateway();
-        tagManager = C2monServiceGateway.getTagManager();
-        logger.info("TagManager instance: " + (tagManager == null ? "NULL" : "OK"));
-        commandManager = C2monServiceGateway.getCommandManager();
-        logger.info("CommandManager instance: " + (commandManager == null ? "NULL" : "OK"));
-    }
-    
-    /**
-     * Starts the C2monServiceGateway. No need to sleep!
-     * 
-     **/
-    private void startC2monServiceGateway() {
-        logger.info("Starting C2MON Service Gateway...");
-        C2monServiceGateway.startC2monClientSynchronous();
-    }
-    
-    /**
-     * Getter for tagManager
-     * @return tag manager instance
-     * */
-    public C2monTagManager getTagManager(){
-        return this.tagManager;
-    }
-    
-    /**
-     * Getter for commandManager
-     * @return command manager instance
-     * */
-    public C2monCommandManager getCommandManager() {
-        return this.commandManager;
-    }
-    
+  /**
+   * Tag manager
+   * */
+  private C2monTagManager tagManager;
+
+  /**
+   * Command manager
+   * */
+  private C2monCommandManager commandManager;
+  
+  /**
+   * Command manager
+   * */
+  private C2monHistoryManager historyManager;
+
+  /**
+   * Initializes the tagManager and commandManager
+   * */
+  @PostConstruct
+  public void init() {
+    startC2monServiceGateway();
+    tagManager = C2monServiceGateway.getTagManager();
+    logger.info("TagManager instance started: " + (tagManager == null ? "NULL" : "OK"));
+    commandManager = C2monServiceGateway.getCommandManager();
+    logger.info("CommandManager instance started: " + (commandManager == null ? "NULL" : "OK"));
+    historyManager = C2monServiceGateway.getHistoryManager();
+    logger.info("HistoryManager instance started: " + (historyManager == null ? "NULL" : "OK")); 
+  }
+
+  /**
+   * Starts the C2monServiceGateway. No need to sleep!
+   * 
+   **/
+  private void startC2monServiceGateway() {
+    logger.info("Starting C2MON Service Gateway...");
+    C2monServiceGateway.startC2monClientSynchronous();
+  }
+
+  /**
+   * Getter for tagManager
+   * @return tag manager instance
+   * */
+  public C2monTagManager getTagManager() {
+    return this.tagManager;
+  }
+
+  /**
+   * Getter for commandManager
+   * @return command manager instance
+   * */
+  public C2monCommandManager getCommandManager() {
+    return this.commandManager;
+  }
+  
+  /**
+   * Getter for HistoryManager
+   * @return History Manager instance
+   * */
+  public C2monHistoryManager getHistoryManager() {
+    return historyManager;
+  }
+
 }
