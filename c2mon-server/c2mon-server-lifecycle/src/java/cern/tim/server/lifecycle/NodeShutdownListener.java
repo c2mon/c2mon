a41c510b78710fe4c7b640f70ae025f5318c0ede
Mark Brightwell <markbrightwell@gmail.com>
2012-08-23 09:56:33 +0000

    TIMS-322 - changed TC API used for node-left detection (wrong API from toolkit was being used)

--- c2mon-server/c2mon-server-lifecycle/src/java/cern/tim/server/lifecycle/NodeShutdownListener.java
+++ c2mon-server/c2mon-server-lifecycle/src/java/cern/tim/server/lifecycle/NodeShutdownListener.java
@@ -8,12 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.AbstractApplicationContext;
-import org.terracotta.cluster.ClusterEvent;
-import org.terracotta.cluster.ClusterInfo;
-import org.terracotta.cluster.ClusterListener;
-import org.terracotta.cluster.ClusterNode;
 
+import com.tc.cluster.DsoCluster;
+import com.tc.cluster.DsoClusterEvent;
+import com.tc.cluster.DsoClusterListener;
 import com.tc.injection.annotations.InjectedDsoInstance;
+import com.tcclient.cluster.DsoNode;
 
 /**
  * Whenever this Terracotta node leaves the cluster, this bean
@@ -27,52 +27,54 @@ import com.tc.injection.annotations.InjectedDsoInstance;
  * @author Mark Brightwell
  *
  */
-public class NodeShutdownListener implements ClusterListener, ApplicationContextAware {
+public class NodeShutdownListener implements DsoClusterListener, ApplicationContextAware {
   
   private static final Logger LOGGER = Logger.getLogger(NodeShutdownListener.class);
   
   @InjectedDsoInstance
-  private ClusterInfo clusterInfo;
-  
+  private DsoCluster dsoCluster;
+ 
   @Autowired
   private LifeCycleController lifeCycleController;
   
-  private ClusterNode thisNode;
+  private DsoNode thisNode;
   
   private AbstractApplicationContext context;
   
   @PostConstruct
-  public void init() {
-    thisNode = clusterInfo.getCurrentNode();
-    clusterInfo.addClusterListener(this);
+  public void init() {    
+    thisNode = dsoCluster.getCurrentNode();
+    dsoCluster.addClusterListener(this);
   }
 
   @Override
-  public void nodeJoined(final ClusterEvent arg0) {
+  public void nodeJoined(final DsoClusterEvent arg0) {
     //do nothing
   }
 
   @Override
-  public void nodeLeft(final ClusterEvent clusterEvent) {
+  public void nodeLeft(final DsoClusterEvent clusterEvent) {
     LOGGER.info("Detected Terracotta node left event for node " + clusterEvent.getNode().getId());
     if (clusterEvent.getNode().getId().equals(thisNode.getId())); {
       LOGGER.info("Initiating server shutdown since this node has left the Terracotta cluster.");
-      new Thread(new Runnable() {               
+      Thread shutdownThread = new Thread(new Runnable() {               
         public void run() {
           lifeCycleController.prepareForShutdown();
           context.close();          
         }
-      }, "Terracotta-node-left-thread").start();      
+      }, "Terracotta-node-left-thread");
+      shutdownThread.setDaemon(true);
+      shutdownThread.start();
     }        
   }
 
   @Override
-  public void operationsDisabled(final ClusterEvent arg0) {
+  public void operationsDisabled(final DsoClusterEvent arg0) {
     //do nothing 
   }
 
   @Override
-  public void operationsEnabled(final ClusterEvent arg0) {
+  public void operationsEnabled(final DsoClusterEvent arg0) {
     //do nothing
   }
 
@@ -80,7 +82,5 @@ public class NodeShutdownListener implements ClusterListener, ApplicationContext
   public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
     this.context = (AbstractApplicationContext) applicationContext;
   }
-
-  
   
 }
