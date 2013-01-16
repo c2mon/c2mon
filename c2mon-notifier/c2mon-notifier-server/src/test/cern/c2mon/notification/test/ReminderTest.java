bc7b7c156e089888d964671491ce49573961d3f6
Felix Ehm <felix.ehm@cern.ch>
2013-01-16 10:31:08 +0000

    Removed ReminderImpl from test package Added ReminderTest

---
+++ c2mon-notifier/c2mon-notifier-server/src/test/cern/c2mon/notification/test/ReminderTest.java
@@ -0,0 +1,42 @@
+/*
+ * $Id $
+ *
+ * $Date$
+ * $Revision$
+ * $Author$
+ *
+ * Copyright CERN ${year}, All Rights Reserved.
+ */
+package cern.c2mon.notification.test;
+
+/**
+ * 
+ * @author ${user} 
+ * @version $Revision$, $Date$, $Author$
+ */
+public class ReminderTest {
+
+    //
+    // -- CONSTRUCTORS -----------------------------------------------
+    //
+
+    //
+    // -- PUBLIC METHODS -----------------------------------------------
+    //
+
+    //
+    // -- implements XXXX -----------------------------------------------
+    //
+
+    //
+    // -- PROTECTED METHODS -----------------------------------------------
+    //
+
+    //
+    // -- PRIVATE METHODS -----------------------------------------------
+    //
+
+    // 
+    // -- INNER CLASSES -----------------------------------------------
+    //
+}
