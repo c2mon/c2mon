3e1a61a1d8c4ec598d85e3cf5a16e7858cd9f9d7
Felix Ehm <felix.ehm@cern.ch>
2013-01-28 11:39:52 +0000

    Added doc

--- c2mon-notifier/c2mon-notifier-server/src/java/cern/c2mon/notification/Mailer.java
+++ c2mon-notifier/c2mon-notifier-server/src/java/cern/c2mon/notification/Mailer.java
@@ -1,14 +1,24 @@
-/**
+/*
  * 
+ * Copyright CERN 2013, All Rights Reserved.
  */
 package cern.c2mon.notification;
 
 /**
+ * Interface which describes a facility to send emails.
+ * 
  * @author felixehm
- *
  */
 public interface Mailer {
 
-	public void sendEmail(String to, String subject, String content) throws Exception;
-	
+    /**
+     * Sends an email to a given address.
+     * 
+     * @param to the email address
+     * @param subject the subject
+     * @param content the body
+     * @throws Exception in case of an error
+     */
+    void sendEmail(String to, String subject, String content) throws Exception;
+
 }
--- c2mon-notifier/c2mon-notifier-server/src/java/cern/c2mon/notification/Main.java
+++ c2mon-notifier/c2mon-notifier-server/src/java/cern/c2mon/notification/Main.java
@@ -1,39 +1,45 @@
-/**
+/*
  * 
+ * Copyright CERN 2013, All Rights Reserved.
  */
 package cern.c2mon.notification;
 
+
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 
 /**
  * @author felixehm
- *
  */
 public class Main {
 
-	public static void main(String[] args) {
-			System.setProperty("log4j.configuration", System.getProperty(
-					"log4j.configuration",
-					"file:log4j.properties"));
-			
-			FileSystemXmlApplicationContext context = null;
-			try {
-				context = new FileSystemXmlApplicationContext(
-					System.getProperty("server-context", "classpath:cern/c2mon/notification/context.xml"));
-			// wait until we close, die or whatever
-				while (context.isRunning()) {
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			} catch(Exception ex) {
-				ex.printStackTrace();
-				System.exit(1);
-			}
-			
-	}
+    /** Starts the notifier spring context. 
+     * 
+     * You can override the default one (classpath) by specifying the JVM option 'server-context'.
+     *   
+     * @param args cmd args.
+     */
+    public static void main(String[] args) {
+        System.setProperty("log4j.configuration", System.getProperty("log4j.configuration", "file:log4j.properties"));
+
+        FileSystemXmlApplicationContext context = null;
+        try {
+            context = new FileSystemXmlApplicationContext(System.getProperty("server-context",
+                    "classpath:cern/c2mon/notification/context.xml"));
+            // wait until we close, die or whatever
+            while (context.isRunning()) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+
+    }
 
 }
