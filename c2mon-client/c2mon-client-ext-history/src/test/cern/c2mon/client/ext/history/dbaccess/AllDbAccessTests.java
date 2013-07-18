727c0846bba84590d9876ade0f84e914c8de1051
Emmanouil Koufakis <koufakis@outlook.com>
2013-07-18 15:31:42 +0000

    TIMS-779 Provide a "light" version of the C2MON client API without history capabilities
    removed test.. HistoryProvider test needs to be redone

--- c2mon-client/c2mon-client-ext-history/src/test/cern/c2mon/client/ext/history/dbaccess/AllDbAccessTests.java
+++ c2mon-client/c2mon-client-ext-history/src/test/cern/c2mon/client/ext/history/dbaccess/AllDbAccessTests.java
@@ -3,6 +3,8 @@ package cern.c2mon.client.ext.history.dbaccess;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import cern.c2mon.client.ext.history.dbaccess.spring.HistorySessionFactoryTest;
+
 /**
  * All the tests in the package
  * 
