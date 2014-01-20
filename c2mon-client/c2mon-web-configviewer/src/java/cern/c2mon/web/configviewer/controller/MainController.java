6b852a12c2957d7cbd95fd7a0b3f392dbf3aee5e
Emmanouil Koufakis <koufakis@outlook.com>
2014-01-20 11:09:39 +0000

    TIMS-875 c2mon-web: Documentation improvements + wiki
    fixed indentation

--- c2mon-client/c2mon-web-configviewer/src/java/cern/c2mon/web/configviewer/controller/MainController.java
+++ c2mon-client/c2mon-web-configviewer/src/java/cern/c2mon/web/configviewer/controller/MainController.java
@@ -9,27 +9,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
+ * 
+ * This is a super simple html page 
+ * (we also add the username of the user to show a personalised message once he is logged in).
  */
 @Controller
 @RequestMapping(value = "/")
 public class MainController {
-    
-    /**
-     * This is the application home page.
-     * @param model Spring MVC Model instance to be filled in before jsp processes it
-     * @param request Used to get the username
-     * @return name of a jsp page which will be displayed
-     */
-    @RequestMapping(method = RequestMethod.GET)
-    public String getCreateForm(final Model model, final HttpServletRequest request) {
-      
-        String username = "";
 
-        if (request.getUserPrincipal() != null) {
-            username = request.getUserPrincipal().getName();
-        }
+  /**
+   * This is the application home page.
+   * 
+   * @param model Spring MVC Model instance to be filled in before jsp processes it
+   * @param request Used to get the username
+   * @return name of a jsp page which will be displayed
+   */
+  @RequestMapping(method = RequestMethod.GET)
+  public String getCreateForm(final Model model, final HttpServletRequest request) {
 
-        model.addAttribute("username", username);
-        return "home";
-    } 
+    String username = "";
+
+    if (request.getUserPrincipal() != null) {
+      username = request.getUserPrincipal().getName();
+    }
+
+    model.addAttribute("username", username);
+    return "home";
+  }
 }
