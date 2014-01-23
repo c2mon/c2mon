013925afd0f26be0f69df837de4168ec2080d59d
Emmanouil Koufakis <koufakis@outlook.com>
2014-01-23 09:31:46 +0000

    TIMS-872 Advanced history search support in the online trend viewer
    - Added support for table view query: 
    
    1) FromTime -> ToTime.
    2) Last Days.
    3) MaxRecords.
    
    with the new format (using @RequestParam)

--- c2mon-client/c2mon-web-configviewer/src/java/cern/c2mon/web/configviewer/controller/HistoryController.java
+++ c2mon-client/c2mon-web-configviewer/src/java/cern/c2mon/web/configviewer/controller/HistoryController.java
@@ -1,6 +1,10 @@
 package cern.c2mon.web.configviewer.controller;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.transform.TransformerException;
@@ -32,8 +36,17 @@ public class HistoryController {
    * */
   public static final String HISTORY_URL = "/historyviewer/";
   
-  /** URL to define Last Records */
-  public static final String LAST_RECORDS_URL = "/records/";
+  /** Parameter: MAX RECORDS */
+  public static final String MAX_RECORDS_PARAMETER = "RECORDS";
+  
+  /** Parameter: LAST DAYS */
+  public static final String LAST_DAYS_PARAMETER = "DAYS";
+  
+  /** Start Date */
+  public static final String START_DATE_PARAMETER = "START";
+  
+  /** End Date */
+  public static final String END_DATE_PARAMETER = "END";
 
   /**
    * A URL to the history viewer with input form
@@ -49,6 +62,9 @@ public class HistoryController {
    * The URL to view the history of a tag in CSV format
    */
   public static final String HISTORY_CSV_URL = HISTORY_URL + "csv";
+
+  /** Date format used to pass a Date as a parameter */
+  private static final String DATE_FORMAT = "dd/MM/yyyy-HH:mm";
 
   /**
    * Title for the history form page
@@ -78,7 +94,7 @@ public class HistoryController {
    * @return Redirects to the form
    */
   @RequestMapping(value = HISTORY_URL, method = { RequestMethod.GET })
-  public String viewHistory(final Model model) {
+  public final String viewHistory(final Model model) {
     logger.info(HISTORY_URL);
     return ("redirect:" + HISTORY_FORM_URL);
   }    
@@ -89,47 +105,39 @@ public class HistoryController {
    * @param id the last 100 records of the given tag id are being shown
    * @param response the html result is written to that HttpServletResponse response
    * */
-  @RequestMapping(value = "/historyviewer/{id}", method = { RequestMethod.GET })
-  public String viewHistory(
-      @PathVariable(value = "id") final String id, final HttpServletResponse response) throws IOException {
+  @RequestMapping(value = HISTORY_URL + "{id}", method = { RequestMethod.GET })
+  public final String viewHistory(
+      @PathVariable(value = "id") final String id, 
+      @RequestParam(value = MAX_RECORDS_PARAMETER, required = false) final String maxRecords,
+      @RequestParam(value = LAST_DAYS_PARAMETER, required = false) final String lastDays,
+      @RequestParam(value = START_DATE_PARAMETER, required = false) final String startTime,
+      @RequestParam(value = END_DATE_PARAMETER, required = false) final String endTime,
+      final HttpServletResponse response) throws IOException {
     
     logger.info("/historyviewer/{id} " + id);
 
     try {
-      response.getWriter().println(service.generateHtmlResponse(id, HISTORY_RECORDS_TO_ASK_FOR));
+      
+      if (startTime != null && endTime != null) {
+        final String xml = service.getHistoryXml(id, 
+            stringToTimestamp(startTime), 
+            stringToTimestamp(endTime));
+        response.getWriter().println(service.generateHtmlResponse(xml));
+      }
+      else if (lastDays != null) {
+        final String xml = service.getHistoryXmlForLastDays(id, Integer.parseInt(lastDays));
+        response.getWriter().println(service.generateHtmlResponse(xml));
+      }
+      else if (maxRecords != null) {
+        response.getWriter().println(service.generateHtmlResponse(id, Integer.parseInt(maxRecords)));
+      }
+      else if (id != null) {
+        response.getWriter().println(service.generateHtmlResponse(id, HISTORY_RECORDS_TO_ASK_FOR));
+      }
+      return null;
+      
     } catch (TagIdException e) {
-      return ("redirect:" + "/historyviewer/errorform/" + id);
-    } catch (TransformerException e) {
-      response.getWriter().println(e.getMessage());
-      logger.error(e.getMessage());
-    }
-    catch (Exception e) {
-      response.getWriter().println(e.getMessage());
-      logger.error(e.getMessage());
-    }
-    return null;
-  }
-  
-
-  /**
-   * @return 
-   * Displays the history of a given id, for a specified by the user number of records.
-   * 
-   * @param id History of this Id is shown.
-   * @param records User can specify how many records to go back in history.
-   * @param response the html result is written to that HttpServletResponse response
-   * */
-  @RequestMapping(value = "/historyviewer/{id}/records/{records}", method = { RequestMethod.GET })
-  public String viewHistory(@PathVariable(value = "id") final String id,
-        @PathVariable(value = "records") final int lastRecords,
-          final HttpServletResponse response) throws IOException  {
-    
-    logger.info("/historyviewer/{id}/records/{records}" + id + " ," + lastRecords);
-
-    try {
-      response.getWriter().println(service.generateHtmlResponse(id, lastRecords));
-    } catch (TagIdException e) {
-      return ("redirect:" + "/historyviewer/errorform/" + id);
+      return ("redirect:" + HISTORY_FORM_URL + "?error=" + id);
     } catch (TransformerException e) {
       response.getWriter().println(e.getMessage());
       logger.error(e.getMessage());
@@ -148,7 +156,7 @@ public class HistoryController {
    * @param model Spring MVC Model instance to be filled in before jsp processes it
    * */
   @RequestMapping(value = HISTORY_XML_URL + "/{id}", method = { RequestMethod.GET })
-  public String viewXml(@PathVariable final String id,  final Model model) {
+  public final String viewXml(@PathVariable final String id,  final Model model) {
     logger.info(HISTORY_XML_URL + id);
     try {
       model.addAttribute("xml", service.getHistoryXml(id, HISTORY_RECORDS_TO_ASK_FOR));
@@ -163,53 +171,62 @@ public class HistoryController {
   }
   
   /**
-   * @return In case of an error this form is shown.
-   * It displays the error and you can also make a new query.
-   * 
-   * @param id tag id
-   * @param model Spring MVC Model instance to be filled in before jsp processes it
-   * */
-  @RequestMapping(value = "/historyviewer/errorform/{id}")
-  public String viewHistoryErrorForm(@PathVariable(value = "id") final String id, final Model model) {
-    logger.info("/historyviewer/errorform " + id);
-
-    model.addAllAttributes(FormUtility.getFormModel(HISTORY_FORM_TITLE, HISTORY_FORM_INSTR, HISTORY_FORM_URL, id, HISTORY_URL + id));
-    model.addAttribute("err", id);
-    return "notFoundErrorFormWithData";
-  }
-
-  /**
-   * @return Displays a form where a tag id can be entered.
-   * 
-   * @param id tag id
-   * @param model Spring MVC Model instance to be filled in before jsp processes it
-   * */
-  @RequestMapping(value = "/historyviewer/form/{id}", method = { RequestMethod.GET })
-  public String viewHistoryWithForm(@PathVariable final String id, final Model model) {
-    logger.info("/historyviewer/form/{id} " + id);
-    model.addAllAttributes(FormUtility.getFormModel(HISTORY_FORM_TITLE, HISTORY_FORM_INSTR, HISTORY_FORM_URL, id, HISTORY_URL + id));
-    return "formWithData";
-  }
-
-  /**
    * @return Displays an input form for a tag id, 
    * and if a POST was made with a tag id, it redirects to HISTORY_URL + id + LAST_RECORDS_URL + records
    * 
    * @param id tag id
    * @param model Spring MVC Model instance to be filled in before jsp processes it
    * */
-  @RequestMapping(value = "/historyviewer/form", method = { RequestMethod.GET, RequestMethod.POST })
-  public String viewHistoryFormPost(@RequestParam(value = "id", required = false) final String id,
+  @RequestMapping(value = HISTORY_URL + "form", method = { RequestMethod.GET, RequestMethod.POST })
+  public final String viewHistoryFormPost(@RequestParam(value = "id", required = false) final String id,
+      @RequestParam(value = "error", required = false) final String wrongId,
       @RequestParam(value = "records", required = false) final String records,
+      @RequestParam(value = "days", required = false) final String days,
+      @RequestParam(value = "start", required = false) final String startDate,
+      @RequestParam(value = "end", required = false) final String endDate,
+      @RequestParam(value = "startTime", required = false) final String startTime,
+      @RequestParam(value = "endTime", required = false) final String endTime,
       final Model model) {
-    logger.info("/historyviewer/form " + id);
+    
+    logger.info(HISTORY_URL + "form" + id);
+    
     if (id == null) {
       model.addAllAttributes(FormUtility
           .getFormModel(HISTORY_FORM_TITLE, HISTORY_FORM_INSTR, HISTORY_FORM_URL, null, null));
+      
+      if (wrongId != null) {
+        model.addAttribute("error", wrongId);
+      }
     }
-    else {
-      return ("redirect:" + HISTORY_URL + id + LAST_RECORDS_URL + records);
+    else if (days != null) {
+      return ("redirect:" + HISTORY_URL + id + "?" + LAST_DAYS_PARAMETER + "=" + days);
+    }
+    else if (startDate != null) {
+      return ("redirect:" + HISTORY_URL + id 
+          + "?" + START_DATE_PARAMETER + "=" + startDate + "-" + startTime 
+          + "&" + END_DATE_PARAMETER + "=" + endDate + "-" + endTime);
+    }
+    else if (records != null) {
+      return ("redirect:" + HISTORY_URL + id + "?" + MAX_RECORDS_PARAMETER + "=" + records);
     }
     return "trend_views/trend_view_form";
+  }
+  
+
+  /**
+   * @return Converts a string to Timestamp
+   * 
+   * @param dateString
+   * should represent a Date in the following format: {@link TrendViewController#DATE_FORMAT}
+   * 
+   * @throws ParseException in case of wrong Date Format
+   */
+  private static Timestamp stringToTimestamp(final String dateString) throws ParseException {
+    
+    DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+    dateFormat.setLenient(false);
+    java.util.Date date = dateFormat.parse(dateString);
+    final long time = date.getTime();
+    return new Timestamp(time);
   }
 }
