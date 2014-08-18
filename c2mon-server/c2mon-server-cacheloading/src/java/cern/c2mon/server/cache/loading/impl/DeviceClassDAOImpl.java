fef762c7eda636c177244409972ea601d76631de
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2014-08-18 17:31:34 +0000

    TIMS-942: Device structure persistence: added initial skeleton for compilation

---
+++ c2mon-server/c2mon-server-cacheloading/src/java/cern/c2mon/server/cache/loading/impl/DeviceClassDAOImpl.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * This file is part of the Technical Infrastructure Monitoring (TIM) project.
+ * See http://ts-project-tim.web.cern.ch
+ *
+ * Copyright (C) 2004 - 2014 CERN. This program is free software; you can
+ * redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received
+ * a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Author: TIM team, tim.support@cern.ch
+ ******************************************************************************/
+package cern.c2mon.server.cache.loading.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cern.c2mon.server.cache.dbaccess.DeviceClassMapper;
+import cern.c2mon.server.cache.loading.DeviceClassDAO;
+import cern.c2mon.server.cache.loading.common.AbstractBatchLoaderDAO;
+import cern.c2mon.server.common.device.DeviceClass;
+
+/**
+ * @author Justin Lewis Salmon
+ */
+@Service("deviceClassDAO")
+public class DeviceClassDAOImpl extends AbstractBatchLoaderDAO<DeviceClass> implements DeviceClassDAO {
+
+  private DeviceClassMapper deviceClassMapper;
+
+  /**
+   * @param batchLoaderMapper
+   */
+  @Autowired
+  public DeviceClassDAOImpl(final DeviceClassMapper deviceClassMapper) {
+    super(deviceClassMapper);
+    this.deviceClassMapper = deviceClassMapper;
+  }
+
+  @Override
+  protected DeviceClass doPostDbLoading(DeviceClass item) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
