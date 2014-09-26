b044384f3f5528ad263031b846ba304df7e58365
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2014-09-26 13:23:41 +0000

    TIMS-937: Added initial integration test class

---
+++ c2mon-client/c2mon-client-ext-device/src/test/cern/c2mon/client/ext/device/DeviceIntegrationTest.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * This file is part of the Technical Infrastructure Monitoring (TIM) project.
+ * See http://ts-project-tim.web.cern.ch
+ *
+ * Copyright (C) 2004 - 2014 CERN. This program is free software; you can
+ * redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received
+ * a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Author: TIM team, tim.support@cern.ch
+ ******************************************************************************/
+package cern.c2mon.client.ext.device;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+import cern.c2mon.client.common.tag.ClientDataTagValue;
+import cern.c2mon.client.ext.device.property.PropertyInfo;
+
+/**
+ * Integration test which takes all configured Devices of all classes and
+ * subscribes to their properties.
+ *
+ * Pass -Dc2mon.client.conf.url=http://timweb/test/conf/c2mon-client.properties
+ * to the VM to run.
+ *
+ * @author Justin Lewis Salmon
+ */
+public class DeviceIntegrationTest {
+
+  static Logger log = Logger.getLogger(DeviceIntegrationTest.class);
+
+  public static void main(String[] args) {
+    C2monDeviceManager manager = C2monDeviceGateway.getDeviceManager();
+
+    List<String> deviceClassNames = manager.getAllDeviceClassNames();
+    log.info("Retrieved the following DeviceClass names:");
+
+    for (String name : deviceClassNames) {
+      log.info("\t" + name);
+    }
+
+    for (String name : deviceClassNames) {
+      List<Device> devices = manager.getAllDevices(name);
+
+      log.info("Retrieved the following devices for device class " + name + ":");
+      for (Device device : devices) {
+        log.info("\t" + device.getName() + " (id: " + device.getId() + ")");
+      }
+
+      for (Device device : devices) {
+        Map<String, ClientDataTagValue> properties = device.getProperties();
+
+        log.info("Found the following properties for device " + device.getName() + ":");
+        for (Map.Entry<String, ClientDataTagValue> property : properties.entrySet()) {
+          log.info("\t" + property.getKey() + ": " + property.getValue().getName());
+        }
+
+        log.info("Subscribing to device " + device.getName() + "...");
+        Device subscribedDevice = manager.subscribeDevice(device, listener);
+
+        log.info("Subscribed and got the following properties for device " + subscribedDevice.getName() + ":");
+        for (Map.Entry<String, ClientDataTagValue> property : subscribedDevice.getProperties().entrySet()) {
+          log.info("\t" + property.getKey() + ": " + property.getValue().getName() + " (" + property.getValue().getValue() + ")");
+        }
+      }
+
+      log.info("Unsubscribing and re-subscribing to all devices");
+      manager.unsubscribeDevices((Set<Device>) devices, listener);
+      manager.subscribeDevices((Set<Device>) devices, listener);
+    }
+  }
+
+  static DeviceUpdateListener listener = new DeviceUpdateListener() {
+    @Override
+    public void onUpdate(Device device, PropertyInfo propertyInfo) {
+      log.info("onUpdate(): device=" + device.getName() + " property=" + propertyInfo.getPropertyName() + " value="
+          + device.getProperty(propertyInfo).getValue());
+
+    }
+  };
+}
