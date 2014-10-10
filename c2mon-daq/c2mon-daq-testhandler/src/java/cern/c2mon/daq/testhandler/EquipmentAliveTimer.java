8ae8b941d6244e02748e9839eaef969091991f11
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2014-10-10 14:10:23 +0000

    TIMS-951: Enhance TestMessageHandler to expose JMX methods for killing/resurrecting Equipments and SubEquipments via alive and commfault tags

--- c2mon-daq/c2mon-daq-testhandler/src/java/cern/c2mon/daq/testhandler/EquipmentAliveTimer.java
+++ c2mon-daq/c2mon-daq-testhandler/src/java/cern/c2mon/daq/testhandler/EquipmentAliveTimer.java
@@ -1,79 +1,147 @@
 package cern.c2mon.daq.testhandler;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import org.apache.log4j.Logger;
 
 import cern.c2mon.daq.common.EquipmentMessageHandler;
+import cern.c2mon.shared.daq.datatag.ISourceDataTag;
 
 /**
- * This class is responsible for generating the alive messages for the
- * simulated equipment.
- * 
+ * This class is responsible for generating the alive messages for the simulated
+ * equipment.
+ *
  * @author mbrightw
  *
  */
 public class EquipmentAliveTimer extends Timer {
-    
-    /**
-     * The time before the timer starts sending alive messages.
-     */
-    private static final long INITIAL_DELAY = 2000;
+
+  /**
+   * The time before the timer starts sending alive messages.
+   */
+  private static final long INITIAL_DELAY = 2000;
+
+  /**
+   * The timer for the Equipment alive tag
+   */
+  private Timer equipmentAliveTimer;
+
+  /**
+   * Map of SubEquipment alive tag IDs -> timers
+   */
+  private Map<Long, Timer> subEquipmentAliveTimers =  new HashMap<>();
+
+  /**
+   * A reference to the EquipmentMessageHandler's class
+   */
+  private EquipmentMessageHandler equipmentMessageHandler;
+
+  /**
+   * A reference to the static driver's logger
+   */
+  private Logger logger;
+
+  /**
+   * The AliveTimer constructor
+   *
+   * @param pEquipmentMessageHandler the EquipmentMessageHandler the timer is
+   *          associated to
+   */
+  public EquipmentAliveTimer(final EquipmentMessageHandler pEquipmentMessageHandler) {
+    logger = Logger.getLogger(EquipmentAliveTimer.class);
+    this.equipmentMessageHandler = pEquipmentMessageHandler;
+    // start the timer as a 'daemon'
+    equipmentAliveTimer = new Timer("EquipmentAliveTimer");
+  }
+
+  /**
+   * This method sets the timer's 'tick' interval
+   *
+   * @param interval the time between ticks (in milliseconds)
+   */
+  public final void scheduleEquipmentAliveTimer(final long interval) {
+    equipmentAliveTimer.schedule(new SendAliveTask(), INITIAL_DELAY, interval);
+  }
+
+  /**
+   * This method sets the timer's 'tick' interval for a sub equipment alive tag
+   *
+   * @param interval the time between ticks (in milliseconds)
+   */
+  public final void scheduleSubEquipmentAliveTimer(Long aliveTagId, final long interval) {
+    Timer subEquipmentAliveTimer = new Timer("SubEquipmentAliveTimer:" + aliveTagId);
+    subEquipmentAliveTimer.schedule(new SendSubEquipmentAliveTask(aliveTagId), INITIAL_DELAY, interval);
+
+    // Keep a reference to the timers so we can cancel them later
+    subEquipmentAliveTimers.put(aliveTagId, subEquipmentAliveTimer);
+  }
+
+  /**
+   * This method is used for timer's termination
+   */
+  public final void terminateEquipmentAliveTimer() {
+    for (Timer timer : subEquipmentAliveTimers.values()) {
+      timer.cancel();
+    }
+    equipmentAliveTimer.cancel();
+  }
+
+  /**
+   * Terminate the alive timer for a particular SubEquipment.
+   *
+   * @param aliveTagId the ID of the SubEquipment alive tag
+   */
+  public final void terminateSubEquipmentAliveTimer(Long aliveTagId) {
+    Timer timer = subEquipmentAliveTimers.get(aliveTagId);
+    if (timer != null) {
+      timer.cancel();
+    } else {
+      logger.debug("Alive timer not found for tag " + aliveTagId);
+    }
+  }
+
+  /**
+   * This class models the action/task that is taken each timer's 'tick'
+   */
+  class SendAliveTask extends TimerTask {
 
     /**
-     * The standard java timer
+     * Sends the alive message at every timer tick.
      */
-    private Timer timer;
+    @Override
+    public void run() {
+      logger.debug("sending equipment status ok message...");
+      equipmentMessageHandler.getEquipmentMessageSender().sendSupervisionAlive();
+      logger.debug("equipment status ok sent");
+    }
+  }
 
-    /**
-     * A reference to the EquipmentMessageHandler's class
-     */
-    private EquipmentMessageHandler equipmentMessageHandler;
+  /**
+   * This class models the action/task that is taken each timer's 'tick'
+   *
+   * @author Justin Lewis Salmon
+   */
+  class SendSubEquipmentAliveTask extends TimerTask {
 
-    /**
-     * A reference to the static driver's logger
-     */
-    private Logger logger;
+    private Long aliveTagId;
 
-    /**
-     * The AliveTimer constructor
-     * @param pEquipmentMessageHandler the EquipmentMessageHandler the timer is associated to
-     */
-    public EquipmentAliveTimer(final EquipmentMessageHandler pEquipmentMessageHandler) {
-        logger = Logger.getLogger(EquipmentAliveTimer.class);
-        this.equipmentMessageHandler = pEquipmentMessageHandler;
-        // start the timer as a 'deamon'
-        timer = new Timer("EquipmentAliveTimer");
+    public SendSubEquipmentAliveTask(Long aliveTagId) {
+      this.aliveTagId = aliveTagId;
     }
 
     /**
-     * This method sets the timer's 'tick' interval
-     * @param milisecondsInterval the time between ticks
+     * Sends the alive message at every timer tick.
      */
-    public final void setInterval(final long milisecondsInterval) {
-        timer.schedule(new SendAliveTask(), INITIAL_DELAY, milisecondsInterval);
+    @Override
+    public void run() {
+      logger.debug("sending sub equipment status ok message...");
+      ISourceDataTag aliveTag = equipmentMessageHandler.getEquipmentConfiguration().getSourceDataTag(aliveTagId);
+      Long timestamp = System.currentTimeMillis();
+      equipmentMessageHandler.getEquipmentMessageSender().sendTagFiltered(aliveTag, timestamp, timestamp, "SubEquipment is alive");
+      logger.debug("sub equipment status ok sent");
     }
-
-    /**
-     * This method is used for timer's termination
-     */
-    public final void terminateTimer() {
-        // Terminate the timer thread
-        timer.cancel();
-    }
-
-    /**
-     * This class models the action/task that is taken each timer's 'tick'
-     */
-    class SendAliveTask extends TimerTask {
-        /**
-         * Sends the alive message at every timer tick.
-         */
-        public void run() {
-            logger.debug("sending equipment status ok message...");
-            equipmentMessageHandler.getEquipmentMessageSender().sendSupervisionAlive();
-            logger.debug("equipment status ok sent");
-        }
-    }
+  }
 }
