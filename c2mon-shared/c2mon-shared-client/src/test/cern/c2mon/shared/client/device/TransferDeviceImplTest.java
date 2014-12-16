cc609efbf07b7c6d76314838b09adf83a3fb9607
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2014-12-16 09:51:27 +0000

    Fixed a test case

--- c2mon-shared/c2mon-shared-client/src/test/cern/c2mon/shared/client/device/TransferDeviceImplTest.java
+++ c2mon-shared/c2mon-shared-client/src/test/cern/c2mon/shared/client/device/TransferDeviceImplTest.java
@@ -1,6 +1,9 @@
 package cern.c2mon.shared.client.device;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
@@ -23,13 +26,14 @@ public class TransferDeviceImplTest {
   }
 
   @Test
-  public void testAddDeviceProperties() {
+  public void testAddDeviceProperties() throws ClassNotFoundException {
     TransferDeviceImpl dti = new TransferDeviceImpl(1L, "test_device_name", 1L);
     List<DeviceProperty> properties = new ArrayList<>();
-    properties.add(new DeviceProperty(1L, "cpuLoadInPercent", "987654", "tagId", null));
-    properties.add(new DeviceProperty(2L, "responsiblePerson", "Mr. Administrator", "constantValue", null));
-    properties.add(new DeviceProperty(3L, "someCalculations", "(#123 + #234) / 2", "clientRule", "Float"));
-    properties.add(new DeviceProperty(4L, "numCores", "4", "constantValue", "Integer"));
+    DeviceProperty p1 = new DeviceProperty(1L, "cpuLoadInPercent", "987654", "tagId", null);
+    DeviceProperty p2 = new DeviceProperty(2L, "responsiblePerson", "Mr. Administrator", "constantValue", null);
+    DeviceProperty p3 = new DeviceProperty(3L, "someCalculations", "(#123 + #234) / 2", "clientRule", "Float");
+    DeviceProperty p4 =new DeviceProperty(4L, "numCores", "4", "constantValue", "Integer");
+    properties.addAll(Arrays.asList(p1, p2, p3, p4));
 
     DeviceProperty propertyWithFields = new DeviceProperty(5L, "TEST_PROPERTY_WITH_FIELDS", "mappedProperty", new ArrayList<>(properties));
     properties.add(propertyWithFields);
@@ -40,38 +44,11 @@ public class TransferDeviceImplTest {
     TransferDevice received = TransferDeviceImpl.fromJson(jsonString);
     List<DeviceProperty> deviceProperties = received.getDeviceProperties();
 
-    Assert.assertTrue(deviceProperties.get(0).getName().equals("cpuLoadInPercent"));
-    Assert.assertTrue(deviceProperties.get(0).getValue().equals("987654"));
-    Assert.assertTrue(deviceProperties.get(0).getCategory().equals("tagId"));
-    Assert.assertTrue(deviceProperties.get(1).getName().equals("responsiblePerson"));
-    Assert.assertTrue(deviceProperties.get(1).getValue().equals("Mr. Administrator"));
-    Assert.assertTrue(deviceProperties.get(1).getCategory().equals("constantValue"));
-    Assert.assertTrue(deviceProperties.get(1).getResultType().equals("String"));
-    Assert.assertTrue(deviceProperties.get(2).getName().equals("someCalculations"));
-    Assert.assertTrue(deviceProperties.get(2).getValue().equals("(#123 + #234) / 2"));
-    Assert.assertTrue(deviceProperties.get(2).getCategory().equals("clientRule"));
-    Assert.assertTrue(deviceProperties.get(3).getName().equals("numCores"));
-    Assert.assertTrue(deviceProperties.get(3).getValue().equals("4"));
-    Assert.assertTrue(deviceProperties.get(3).getCategory().equals("constantValue"));
-    Assert.assertTrue(deviceProperties.get(3).getResultType().equals("Integer"));
-
-    Assert.assertTrue(deviceProperties.get(4).getName().equals("TEST_PROPERTY_WITH_FIELDS"));
-    List<DeviceProperty> fields = new ArrayList<>(deviceProperties.get(4).getFields().values());
-
-    Assert.assertTrue(fields.get(3).getName().equals("cpuLoadInPercent"));
-    Assert.assertTrue(fields.get(3).getValue().equals("987654"));
-    Assert.assertTrue(fields.get(3).getCategory().equals("tagId"));
-    Assert.assertTrue(fields.get(2).getName().equals("responsiblePerson"));
-    Assert.assertTrue(fields.get(2).getValue().equals("Mr. Administrator"));
-    Assert.assertTrue(fields.get(2).getCategory().equals("constantValue"));
-    Assert.assertTrue(fields.get(2).getResultType().equals("String"));
-    Assert.assertTrue(fields.get(1).getName().equals("someCalculations"));
-    Assert.assertTrue(fields.get(1).getValue().equals("(#123 + #234) / 2"));
-    Assert.assertTrue(fields.get(1).getCategory().equals("clientRule"));
-    Assert.assertTrue(fields.get(0).getName().equals("numCores"));
-    Assert.assertTrue(fields.get(0).getValue().equals("4"));
-    Assert.assertTrue(fields.get(0).getCategory().equals("constantValue"));
-    Assert.assertTrue(fields.get(0).getResultType().equals("Integer"));
+    assertDevicePropertyListContains(deviceProperties, p1);
+    assertDevicePropertyListContains(deviceProperties, p2);
+    assertDevicePropertyListContains(deviceProperties, p3);
+    assertDevicePropertyListContains(deviceProperties, p4);
+    assertDevicePropertyListContains(deviceProperties, propertyWithFields);
   }
 
   @Test
@@ -94,5 +71,28 @@ public class TransferDeviceImplTest {
     Assert.assertTrue(deviceCommands.get(1).getValue().equals("2000"));
     Assert.assertTrue(deviceCommands.get(2).getValue().equals("3000"));
     Assert.assertTrue(deviceCommands.get(3).getValue().equals("4000"));
+  }
+
+  public static void assertDevicePropertyEquals(DeviceProperty expectedObject, DeviceProperty cacheObject) throws ClassNotFoundException {
+    assertEquals(expectedObject.getId(), cacheObject.getId());
+    assertEquals(expectedObject.getName(), cacheObject.getName());
+    assertEquals(expectedObject.getCategory(), cacheObject.getCategory());
+
+    if ((expectedObject.getCategory() != null && expectedObject.getCategory().equals("mappedProperty")) || expectedObject.getFields() != null) {
+      for (DeviceProperty field : expectedObject.getFields().values()) {
+        assertDevicePropertyListContains(new ArrayList<>(cacheObject.getFields().values()), field);
+      }
+    } else {
+      assertEquals(expectedObject.getValue(), cacheObject.getValue());
+      assertEquals(expectedObject.getResultType(), cacheObject.getResultType());
+    }
+  }
+
+  public static void assertDevicePropertyListContains(List<DeviceProperty> deviceProperties, DeviceProperty expectedObject) throws ClassNotFoundException {
+    for (DeviceProperty deviceProperty : deviceProperties) {
+      if (deviceProperty.getName().equals(expectedObject.getName())) {
+        assertDevicePropertyEquals(expectedObject, deviceProperty);
+      }
+    }
   }
 }
