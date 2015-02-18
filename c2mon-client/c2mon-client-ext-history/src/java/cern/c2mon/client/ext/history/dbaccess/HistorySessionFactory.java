3be188ac6fe04ab4bc48bfcf5d872a7528d4997c
Matthias Braeger <matthias.braeger@cern.ch>
2015-02-18 16:03:36 +0000

    Removed ranges and use now LATEST keyword and upgraded to latest parent-pom

--- c2mon-client/c2mon-client-ext-history/src/java/cern/c2mon/client/ext/history/dbaccess/HistorySessionFactory.java
+++ c2mon-client/c2mon-client-ext-history/src/java/cern/c2mon/client/ext/history/dbaccess/HistorySessionFactory.java
@@ -20,7 +20,6 @@ package cern.c2mon.client.ext.history.dbaccess;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import cern.c2mon.client.common.tag.ClientDataTagValue;
 import cern.c2mon.client.ext.history.ClientDataTagRequestCallback;
