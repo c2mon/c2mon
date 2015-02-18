ef50566208936b0042127174a07b0d40ec3fee98
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-02-18 15:30:07 +0000

    TIMS-991: Added controller and service beans for retrieving configuration history

---
+++ c2mon-client/c2mon-web-configviewer/src/java/cern/c2mon/web/configviewer/controller/ConfigHistoryController.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * This file is part of the Technical Infrastructure Monitoring (TIM) project.
+ * See http://ts-project-tim.web.cern.ch
+ *
+ * Copyright (C) 2004 - 2015 CERN. This program is free software; you can
+ * redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received
+ * a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Author: TIM team, tim.support@cern.ch
+ ******************************************************************************/
+package cern.c2mon.web.configviewer.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import cern.c2mon.web.configviewer.service.ConfigHistoryService;
+
+/**
+ * @author Justin Lewis Salmon
+ */
+@Controller
+public class ConfigHistoryController {
+
+  @Autowired
+  ConfigHistoryService service;
+
+  @RequestMapping(value = "/confighistory", method = { RequestMethod.GET })
+  public String configHistory(final HttpServletRequest request, final Model model) {
+    model.addAttribute("cache", service.getCachedReports());
+    model.addAttribute("title", "Configuration History Viewer");
+    return "config/configHistory";
+  }
+}
