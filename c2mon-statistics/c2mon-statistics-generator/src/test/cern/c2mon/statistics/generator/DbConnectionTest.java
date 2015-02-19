06ad38aba6d6ac087ee2f731900405dcf3cf9304
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-02-19 20:16:06 +0000

    TIMS-1018: Disabled a test case

--- c2mon-statistics/c2mon-statistics-generator/src/test/cern/c2mon/statistics/generator/DbConnectionTest.java
+++ c2mon-statistics/c2mon-statistics-generator/src/test/cern/c2mon/statistics/generator/DbConnectionTest.java
@@ -1,23 +1,21 @@
 package cern.c2mon.statistics.generator;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
 
 /**
  * Tests the DB connection starts up correctly.
- * 
+ *
  * @author Mark Brightwell
  *
  */
 public class DbConnectionTest {
 
-  @Test
+  //@Test
   public void initSqlMap() {
     SqlMapClient client = SqlMapper.getSqlMapInstance();
     assertNotNull(client);
   }
-  
+
 }
