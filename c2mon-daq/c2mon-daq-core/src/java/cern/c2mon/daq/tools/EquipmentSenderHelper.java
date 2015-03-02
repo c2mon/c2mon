f4ef3062bdce8c27cec2e8d5d6f95e7065f3a913
Wojciech Buczak <wojciech.buczak@gmail.com>
2015-03-02 18:38:29 +0000

    TIMD-145 - conversion method changed

--- c2mon-daq/c2mon-daq-core/src/java/cern/c2mon/daq/tools/EquipmentSenderHelper.java
+++ c2mon-daq/c2mon-daq-core/src/java/cern/c2mon/daq/tools/EquipmentSenderHelper.java
@@ -5,14 +5,13 @@ import java.util.regex.Pattern;
 import cern.c2mon.shared.common.datatag.ISourceDataTag;
 import cern.c2mon.shared.common.datatag.SourceDataQuality;
 import cern.c2mon.shared.common.datatag.SourceDataTag;
+import cern.c2mon.shared.common.type.TypeConverter;
 
 /**
- * 
  * @author vilches
- *
  */
 public class EquipmentSenderHelper {
-	
+
     /**
      * maximum allowed length of the tag quality description (will be truncated to this if too long)
      */
@@ -23,9 +22,8 @@ public class EquipmentSenderHelper {
      */
     private static final Pattern INVALID_XML_CHARS = Pattern
             .compile("[^\\u0009\\u000A\\u000D\\u0020-\\uD7FF\\uE000-\\uFFFD\uD800\uDC00-\uDBFF\uDFFF]");
-	
-	
-	/**
+
+    /**
      * Converts the input value to the expected type
      * 
      * @param tag
@@ -33,44 +31,34 @@ public class EquipmentSenderHelper {
      * @return
      */
     public Object convertValue(ISourceDataTag tag, Object inValue) {
-        Object outValue = null;
-        if (inValue instanceof Number) {
-            outValue = TIMDriverSimpleTypeConverter.convert(tag, (Number) inValue);
-        } else if (inValue instanceof Boolean) {
-            outValue = inValue;
-        } else {
-            outValue = inValue.toString();
-        }
-
-        return outValue;
+        return TypeConverter.cast(inValue.toString(), tag.getDataType());
     }
-    
+
     /**
      * Creates a new quality object for a {@link SourceDataTag}
      * 
      * @param pQualityCode A quality code which is defined as constant in SourceDataQuality
      * @param pDescription A quality description
-     * 
      * @return A new instance of {@link SourceDataQuality}
      */
     public SourceDataQuality createTagQualityObject(final short pQualityCode, final String pDescription) {
-      SourceDataQuality sdQuality;
-      if (pDescription == null) {
-          sdQuality = new SourceDataQuality(pQualityCode);
-      } else {
-          String pQualityDesc;
-          if (pDescription.length() > MAX_QUALITY_DESC_LENGHT) {
-              pQualityDesc = pDescription.substring(0, MAX_QUALITY_DESC_LENGHT - 1);
-          } else {
-              pQualityDesc = pDescription;
-          }
+        SourceDataQuality sdQuality;
+        if (pDescription == null) {
+            sdQuality = new SourceDataQuality(pQualityCode);
+        } else {
+            String pQualityDesc;
+            if (pDescription.length() > MAX_QUALITY_DESC_LENGHT) {
+                pQualityDesc = pDescription.substring(0, MAX_QUALITY_DESC_LENGHT - 1);
+            } else {
+                pQualityDesc = pDescription;
+            }
 
-          // Strip out all invalid XML characters
-          pQualityDesc = INVALID_XML_CHARS.matcher(pQualityDesc).replaceAll("");
+            // Strip out all invalid XML characters
+            pQualityDesc = INVALID_XML_CHARS.matcher(pQualityDesc).replaceAll("");
 
-          sdQuality = new SourceDataQuality(pQualityCode, pQualityDesc);
-      }
-      
-      return sdQuality;
+            sdQuality = new SourceDataQuality(pQualityCode, pQualityDesc);
+        }
+
+        return sdQuality;
     }
 }
