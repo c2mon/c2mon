c67435899b1a856f4f3d7b865f700f2653819977
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-03-13 09:53:15 +0000

    Removed obsolete parameter from demo DAQ startup scripts

--- c2mon-deploy/c2mon-daq-testhandler-demo-deploy/bin/daqprocess-P_TESTHANDLER04-demo.sh
+++ c2mon-deploy/c2mon-daq-testhandler-demo-deploy/bin/daqprocess-P_TESTHANDLER04-demo.sh
@@ -36,5 +36,5 @@ ADDITIONAL_PARAMS="${ADDITIONAL_PARAMS} -c ${DAQ_CONF_HOME}/local/${PROCESS_NAME
 CLASSPATH=`ls $DAQ_HOME/lib/*.jar | tr -s '\n' ':'`
 
 # Execution
-exec -a `basename $0` $JAVA_HOME/bin/java -cp "$CLASSPATH" -Dc2mon.process.name="$PROCESS_NAME" -Dc2mon.log.dir="$DAQ_LOG_HOME" -Dc2mon.daq.spring.context="classpath:resources/daq-core-service-double.xml" -Djava.security.egd="file://dev/urandom" -Dapp.name="tim2-daq-testhandler-test"  -Dapp.version="1.2.5-SNAPSHOT"  $JVM_MEM "${JVM_OTHER_OPTS[@]}" cern.c2mon.daq.common.startup.DaqStartup -c2monProperties ${C2MON_PROPERTIES_FILE} -log4j ${LOG4J_CONF_FILE} -daqConf ${DAQ_CONF_HOME}/daq.conf -processName ${PROCESS_NAME} ${ADDITIONAL_PARAMS}
+exec -a `basename $0` $JAVA_HOME/bin/java -cp "$CLASSPATH" -Dc2mon.process.name="$PROCESS_NAME" -Dc2mon.log.dir="$DAQ_LOG_HOME" -Dc2mon.daq.spring.context="classpath:resources/daq-core-service-double.xml" -Dapp.name="tim2-daq-testhandler-test"  -Dapp.version="1.2.5-SNAPSHOT"  $JVM_MEM "${JVM_OTHER_OPTS[@]}" cern.c2mon.daq.common.startup.DaqStartup -c2monProperties ${C2MON_PROPERTIES_FILE} -log4j ${LOG4J_CONF_FILE} -daqConf ${DAQ_CONF_HOME}/daq.conf -processName ${PROCESS_NAME} ${ADDITIONAL_PARAMS}
 
