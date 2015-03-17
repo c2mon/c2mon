4e26dd22d06c4242d0d4b40550e694002f1b967f
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-03-17 09:42:31 +0000

    Run HSQLDB instance in background

--- c2mon-deploy/c2mon-server-deploy/modules/demo/bin/c2mon-demo.sh
+++ c2mon-deploy/c2mon-server-deploy/modules/demo/bin/c2mon-demo.sh
@@ -44,11 +44,11 @@ CLASSPATH=`ls $C2MON_HOME/lib/*.jar | tr -s '\n' ':'`
 
 
 HSQLDB_NAME=stl
-HSQLDB_START_CMD=$JAVA_HOME/jre/bin/java -cp "${CLASSPATH}" org.hsqldb.Server -database.0 file:$HSQLDB_NAME -dbname.0 $HSQLDB_NAME &
+HSQLDB_START_CMD="$JAVA_HOME/jre/bin/java -cp "${CLASSPATH}" org.hsqldb.Server -database.0 file:$HSQLDB_NAME -dbname.0 $HSQLDB_NAME"
 
 # start hsqldb
 echo "Starting local HSQLDB instance..."
-$HSQLDB_START_CMD
+$HSQLDB_START_CMD &
 
 COMMON_JAVA_ARGS="-Xms512m -Xmx512m -XX:NewRatio=3 -XX:+PrintGCDetails -XX:+UseParallelGC -XX:MaxGCPauseMillis=100 -Dserver.process.name=$PROCESS_NAME -Dc2mon.process.name=$PROCESS_NAME -Dc2mon.home=$C2MON_HOME -Dlog4j.configuration=$LOG4J_CONF_FILE -Dc2mon.log.dir=$LOG_DIR -Dc2mon.properties.location=$C2MON_PROPERTIES -Dcern.c2mon.cache.mode=single-nonpersistent"
 
