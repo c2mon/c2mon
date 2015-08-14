8770ff582872b95c686769bc52703f6bf8cca296
Mark Buttner <mark.buttner@cern.ch>
2015-08-14 13:35:35 +0000

    DMN-1924

--- c2mon-deploy/dmn2-publisher-rda-alarms-deploy/subscribe.sh
+++ c2mon-deploy/dmn2-publisher-rda-alarms-deploy/subscribe.sh
@@ -2,20 +2,20 @@
 #!/bin/bash
 cd deploy
 
-MAIN=cern.c2mon.publisher.rdaAlarms.RdaAlarmsMain
+MAIN=cern.c2mon.publisher.rdaAlarms.SubscribeSourceUnfiltered
+DEV=../../c2mon-publisher-rda-alarms/build
 
 PARAMS="\
--Dspring.profiles.active=PROD-DB \
 -Dlog4j.configuration=file:log4j.properties \
 -XX:+UnlockCommercialFeatures -XX:+FlightRecorder \
 -Dc2mon.client.conf.url=file:client.properties \
 -Dprovider.properties=file:provider.properties \
 -Dmgt.jmx.port=17017 \
+-Dspring.profiles.active=PROD \
 -Dapp.version=0.2 \
 -Dapp.name=DmnRdaAlarmsPub \
 -Dcmw.rda3.transport.server.multiThreadedPublisher=true \
 -Doracle.net.tns_admin=/etc"
 
-echo ${PARAMS}
-exec -a pub-alarm java ${PARAMS} -cp ".:../../c2mon-publisher-rda-alarms/build/classes:lib/*" ${MAIN}
+exec -a pub-alarm java ${PARAMS} -cp ".:${DEV}/classes:lib/*" ${MAIN} $1 $2 $3 $4 $5 $6 $7 $8 $9
 
