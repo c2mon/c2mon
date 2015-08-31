edeeda0a3cd44ddf286d9b6068a031cbfff96c9f
Felix Ehm <felix.ehm@cern.ch>
2015-08-31 09:44:25 +0000

    renamed test to correct naming convention

---
+++ c2mon-server/c2mon-server-shorttermlog/src/test/cern/c2mon/server/shorttermlog/structure/AlarnLogConverterTest.java
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2015 European Organisation for Nuclear Research (CERN), All Rights Reserved.
+ */
+
+package cern.c2mon.server.shorttermlog.structure;
+
+public class AlarnLogConverterTest {
+
+}
