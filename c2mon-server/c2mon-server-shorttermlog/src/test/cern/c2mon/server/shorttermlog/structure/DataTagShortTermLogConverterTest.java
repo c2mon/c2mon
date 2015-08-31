255111bcf30984eda7c537f317b76cb1b1c87553
Felix Ehm <felix.ehm@cern.ch>
2015-08-31 09:44:51 +0000

    renamed test to correct naming convention

---
+++ c2mon-server/c2mon-server-shorttermlog/src/test/cern/c2mon/server/shorttermlog/structure/DataTagShortTermLogConverterTest.java
@@ -0,0 +1,50 @@
+package cern.c2mon.server.shorttermlog.structure;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import cern.c2mon.server.common.datatag.DataTagCacheObject;
+import cern.c2mon.shared.common.datatag.TagQualityStatus;
+
+/**
+ * Unit test of DataTagShortTermLogConverter.
+ *
+ * @author Mark Brightwell
+ *
+ */
+public class DataTagShortTermLogConverterTest {
+
+  /**
+   * Class to test.
+   */
+  private DataTagShortTermLogConverter converter = new DataTagShortTermLogConverter();
+
+  /**
+   * Tests the quality code is correctly generated.
+   */
+  @Test
+  public void testCodeGeneration() {
+    DataTagCacheObject tag = new DataTagCacheObject(10L);
+    tag.getDataTagQuality().addInvalidStatus(TagQualityStatus.INACCESSIBLE, "desc1");
+    tag.getDataTagQuality().addInvalidStatus(TagQualityStatus.UNINITIALISED, "desc1");
+    tag.getDataTagQuality().addInvalidStatus(TagQualityStatus.VALUE_OUT_OF_BOUNDS, "desc1");
+    assertNotNull(tag.getDataTagQuality());
+    Loggable loggable = converter.convertToLogged(tag);
+    TagShortTermLog logTag = (TagShortTermLog) loggable;
+    assertEquals(11, logTag.getTagQualityCode());
+  }
+
+  /**
+   * Tests conversion for some other fields.
+   */
+  @Test
+  public void testConversion() {
+    DataTagCacheObject tag = new DataTagCacheObject(10L);
+    tag.setValueDescription("value desc");
+    TagShortTermLog logTag = (TagShortTermLog) converter.convertToLogged(tag);
+    assertEquals("value desc",logTag.getTagValueDesc());
+  }
+
+}
