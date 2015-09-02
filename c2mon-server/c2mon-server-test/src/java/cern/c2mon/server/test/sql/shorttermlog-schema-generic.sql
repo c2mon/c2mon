eabba177b484bd1a42783ef419c0783691070dcc
Felix Ehm <felix.ehm@cern.ch>
2015-09-02 12:51:08 +0000

    TIMS-1063: renamed table column  isactive -> active

--- c2mon-server/c2mon-server-test/src/java/cern/c2mon/server/test/sql/shorttermlog-schema-generic.sql
+++ c2mon-server/c2mon-server-test/src/java/cern/c2mon/server/test/sql/shorttermlog-schema-generic.sql
@@ -70,7 +70,7 @@ alarmid         INTEGER       NOT NULL,
 faultfamily     VARCHAR(60)   NOT NULL,
 faultmember     VARCHAR(60)   NOT NULL,
 faultcode       INTEGER       NOT NULL,
-isactive        INTEGER       NOT NULL,
+active          INTEGER       NOT NULL,
 servertime      TIMESTAMP(6)  NOT NULL,
-info         VARCHAR(100)  NOT NULL
+info            VARCHAR(100)  NOT NULL
 );
