c5e1fd560ff118e5c3d7a37c6197104f57284296
Felix Ehm <felix.ehm@cern.ch>
2015-09-15 07:51:51 +0000

    reverted last change

--- c2mon-server/c2mon-server-configuration/src/java/cern/c2mon/server/configuration/handler/transacted/DataTagConfigTransactedImpl.java
+++ c2mon-server/c2mon-server-configuration/src/java/cern/c2mon/server/configuration/handler/transacted/DataTagConfigTransactedImpl.java
@@ -149,13 +149,6 @@ public class DataTagConfigTransactedImpl extends TagConfigTransactedImpl<DataTag
           }
         }
         
-        // we also need to remove the old one
-        try {
-          configurableDAO.deleteItem(dataTag.getId());
-        } catch (Exception e) {
-          LOGGER.info("Couldn't rollback the creation of the datatag " + dataTag.getId());
-        }
-
         throw new UnexpectedRollbackException("Unexpected exception while creating a DataTag: rolling back the change", ex);
       }
     } finally {
