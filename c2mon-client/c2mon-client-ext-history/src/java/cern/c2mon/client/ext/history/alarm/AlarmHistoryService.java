4b66d560489e206d2e6c83d69f5b88ff0e49cca3
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-10-28 14:05:56 +0000

    TIMS-1064: Use in-memory db for tests

--- c2mon-client/c2mon-client-ext-history/src/java/cern/c2mon/client/ext/history/alarm/AlarmHistoryService.java
+++ c2mon-client/c2mon-client-ext-history/src/java/cern/c2mon/client/ext/history/alarm/AlarmHistoryService.java
@@ -2,7 +2,6 @@ package cern.c2mon.client.ext.history.alarm;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
