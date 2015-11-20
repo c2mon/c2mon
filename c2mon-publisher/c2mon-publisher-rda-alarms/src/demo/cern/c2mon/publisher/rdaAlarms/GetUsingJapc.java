dd85d27e7c4ffeca0b54216deb449c3f46c74e89
Mark Buttner <mark.buttner@cern.ch>
2015-11-20 15:33:59 +0000

    DMN-2447

---
+++ c2mon-publisher/c2mon-publisher-rda-alarms/src/demo/cern/c2mon/publisher/rdaAlarms/GetUsingJapc.java
@@ -0,0 +1,107 @@
+package cern.c2mon.publisher.rdaAlarms;
+/**
+ * Copyright (c) 2015 European Organisation for Nuclear Research (CERN), All Rights Reserved.
+ */
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cern.japc.AcquiredParameterValue;
+import cern.japc.MapParameterValue;
+import cern.japc.Parameter;
+import cern.japc.ParameterException;
+import cern.japc.Selector;
+import cern.japc.SimpleParameterValue;
+import cern.japc.factory.ParameterFactory;
+import cern.japc.factory.SelectorFactory;
+import cern.japc.factory.SimpleParameterValueFactory;
+
+/**
+ * Demonstrates a simple "alarm GET" operation using JAPC
+ *
+ * Notes:
+ * - if you do not provide a selector with filter, all alarms with an event since startup of the publisher
+ *   will be returned.
+ * - if you do provide a selector with filter, only the alarms matching the ids in the filter list will 
+ *   be returned, and this only if they really belong to the alarm source specified as property name.
+ * - the use of the filter also allows the renaming of alarms (see "client alarm id" vs "laser alarm id")
+ *   for internal use by the application.
+ * 
+ * @author mbuttner 
+ */
+public class GetUsingJapc {
+
+    /**
+     * The RDA device publishing the alarms
+     */
+    static final String DEVICE = "DMN.RDA.ALARMS";
+    
+    /**
+     * The source. This is something you need to take from the alarm system configuration
+     */
+    static final String sourceId = "TIMOPALARM";
+        
+    /**
+     * The alarm as identified in the alarm system. The alarm will only be correctly received
+     * if it is sent by the source. The alarm id acts as a filter on the property, which  contains
+     * all (at least the active) alarms sent by the source. 
+     */
+    static final String laserAlarmId = "SECU_FEU_LHC:SFDIN-00279:1663";
+        
+    /**
+     * The internal alarm id in your application.
+     */
+    static final String clientAlarmId = "MY_ALARM";
+
+
+    //
+    // --- MAIN -------------------------------------------------------------------------------------
+    //
+    public static void main(String[] args)
+    {        
+        System.setProperty("app.name", "japc-ext-laser DemoGet");
+        System.setProperty("app.version", "0.0.1");
+        
+        // standard logging setup stuff
+        String log4jConfigFile = System.getProperty("log4j.configuration", "log4j.properties");
+        PropertyConfigurator.configureAndWatch(log4jConfigFile, 60 * 1000);   
+        Logger log = LoggerFactory.getLogger(GetUsingJapc.class);
+        log.info("Starting " + GetUsingJapc.class.getName() + " ...");
+            
+        // create the filter parameter: This is a list of pairs internal alarm id / alarm sys alarm id
+        Map<String, SimpleParameterValue> filterParams = new HashMap<String, SimpleParameterValue>();        
+        filterParams.put(clientAlarmId, SimpleParameterValueFactory.newValue(laserAlarmId));        
+        filterParams.put("MY_FAKE", SimpleParameterValueFactory.newValue("FF:FM:FC"));        
+        // ... add others as needed ...
+        MapParameterValue filter = SimpleParameterValueFactory.newValue(filterParams);
+            
+        // selector The id value is used as dely to re-publish (!?)
+        Selector selector = SelectorFactory.newSelector("5000", filter);           
+
+        try
+        {
+            //
+            // create the parameter, do the GET operation and extract the status of the alarm
+            // from the result.
+            Parameter param = ParameterFactory.newInstance().newParameter("DMN.RDA.ALARMS","TIMOPALARM");
+            AcquiredParameterValue avalue = param.getValue(selector);
+            MapParameterValue value = (MapParameterValue)avalue.getValue();
+            log.info("Initial status of alarm " + clientAlarmId + "= " + value.toString());
+        }
+        catch ( ParameterException e )
+        {
+            e.printStackTrace();
+        }
+        log.info("Completed");
+        System.exit(0);
+    }
+
+               
+}
+
+
---
+++ c2mon-publisher/c2mon-publisher-rda-alarms/src/demo/cern/c2mon/publisher/rdaAlarms/SubscribeMultipleFiltered.java
@@ -0,0 +1,110 @@
+package cern.c2mon.publisher.rdaAlarms;
+/**
+ * Copyright (c) 2014 European Organisation for Nuclear Research (CERN), All Rights Reserved.
+ */
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.PropertyConfigurator;
+
+import cern.japc.AcquiredParameterValue;
+import cern.japc.MapParameterValue;
+import cern.japc.Parameter;
+import cern.japc.ParameterException;
+import cern.japc.ParameterValueListener;
+import cern.japc.Selector;
+import cern.japc.SimpleParameterValue;
+import cern.japc.SubscriptionHandle;
+import cern.japc.factory.MapParameterValueFactory;
+import cern.japc.factory.ParameterFactory;
+import cern.japc.factory.SelectorFactory;
+import cern.japc.factory.SimpleParameterValueFactory;
+
+
+/**
+ * Demonstrates how to listen to alarms coming from different alarm sources.
+ *
+ * @author mbuttner 
+ */
+public class SubscribeMultipleFiltered implements ParameterValueListener
+{    
+    private static Log log;
+    
+    private static String alarmId_1 = "PSBS:DE1.STP26:1000";
+    private static String sourceId_1 = "CPS";
+
+    //
+    // --- MAIN -----------------------------------------------------------------------------------
+    //
+    public static void main(String[] args)
+    {        
+        System.setProperty("app.name", "japc-ext-laser DemoMonitor");
+        System.setProperty("app.version", "0.0.1");
+        
+        // standard logging setup stuff
+        String log4jConfigFile = System.getProperty("log4j.configuration", "log4j.properties");
+        PropertyConfigurator.configureAndWatch(log4jConfigFile, 60 * 1000);   
+        log = LogFactory.getLog(SubscribeMultipleFiltered.class);
+        log.info("Starting " + SubscribeMultipleFiltered.class.getName() + " ...");
+        
+        // create the filter parameter: This is a list of pairs internal alarm id / alarm sys alarm id
+        Map<String, SimpleParameterValue> psbFilterParams = new HashMap<String, SimpleParameterValue>();        
+        psbFilterParams.put(sourceId_1, SimpleParameterValueFactory.newValue(alarmId_1));        
+        MapParameterValue psbFilter = SimpleParameterValueFactory.newValue(psbFilterParams);
+        Selector psbSelector = SelectorFactory.newSelector("5000", psbFilter /*, false*/);           
+
+        Map<String, SimpleParameterValue> leiFilterParams = new HashMap<String, SimpleParameterValue>();        
+        leiFilterParams.put("LEI", SimpleParameterValueFactory.newValue("XENERICSAMPLER_2106:ER.SCBSYNTH:4000"));        
+        MapParameterValue leiFilter = MapParameterValueFactory.newValue(leiFilterParams);
+        Selector leiSelector = SelectorFactory.newSelector("5000", leiFilter /*, false*/);           
+        
+        try
+        {
+            SubscribeMultipleFiltered mon = new SubscribeMultipleFiltered();
+            Parameter p1 = ParameterFactory.newInstance().newParameter("rda3:///DMN.RDA.ALARMS/" + sourceId_1);
+            SubscriptionHandle sh1 = p1.createSubscription(psbSelector, mon);            
+            sh1.startMonitoring();            
+
+            Parameter p2 = ParameterFactory.newInstance().newParameter("rda3:///DMN.RDA.ALARMS/LEI");
+            SubscriptionHandle sh2 = p2.createSubscription(leiSelector, mon);            
+            sh2.startMonitoring();            
+                        
+            Thread.sleep(2 * 60 * 1000); // let's run it for 2 minutes.
+
+            sh1.stopMonitoring();
+            sh2.stopMonitoring();
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
+        log.info(SubscribeMultipleFiltered.class.getName()  + " completed");
+        System.exit(0);
+    }
+
+    @Override
+    public void exceptionOccured(String parameterName, String description, ParameterException pe)
+    {
+        pe.printStackTrace();
+    }
+
+    @Override
+    public void valueReceived(String parameterName, AcquiredParameterValue avalue)
+    {
+        try
+        {
+            String value = avalue.getValue().toString();
+            log.info("Value received for " + parameterName + ": " + value.trim());
+        }
+        catch (Exception e)
+        {
+            log.error("Failed to retrieve value for " + parameterName, e);            
+        }
+    }
+
+
+}
