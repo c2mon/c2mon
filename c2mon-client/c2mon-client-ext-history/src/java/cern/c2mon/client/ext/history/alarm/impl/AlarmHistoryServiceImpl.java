1e33d11097472b9e559d796bcad3b3721e55a93e
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-01-04 17:12:09 +0000

    TIMS-1115: Localise historic alarm timestamps on retrieval

--- c2mon-client/c2mon-client-ext-history/src/java/cern/c2mon/client/ext/history/alarm/impl/AlarmHistoryServiceImpl.java
+++ c2mon-client/c2mon-client-ext-history/src/java/cern/c2mon/client/ext/history/alarm/impl/AlarmHistoryServiceImpl.java
@@ -4,6 +4,7 @@ import cern.c2mon.client.ext.history.alarm.Alarm;
 import cern.c2mon.client.ext.history.alarm.AlarmHistoryService;
 import cern.c2mon.client.ext.history.alarm.HistoricAlarmQuery;
 import cern.c2mon.client.ext.history.alarm.repository.AlarmRepository;
+import cern.c2mon.client.ext.history.dbaccess.util.TimeZoneUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
 
 /**
  * @author Justin Lewis Salmon
@@ -24,12 +26,27 @@ public class AlarmHistoryServiceImpl implements AlarmHistoryService {
 
   @Override
   public List<Alarm> findBy(HistoricAlarmQuery query) {
-    return toList(alarmRepository.findAll(query.getPredicate()));
+    return toList(localise(alarmRepository.findAll(query.getPredicate())));
   }
 
   @Override
   public Page<Alarm> findBy(HistoricAlarmQuery query, PageRequest page) {
-    return alarmRepository.findAll(query.getPredicate(), page);
+    return (Page<Alarm>) localise(alarmRepository.findAll(query.getPredicate(), page));
+  }
+
+  /**
+   * Modify the timestamps of the given {@link Iterable<Alarm>} list to match the current local timezone.
+   * Relies on the fact that the {@link Alarm} timestamps are stored as UTC.
+   *
+   * @param alarms the list of alarms to modify
+   * @return the same list, with localised timestamps (modified in-place)
+   */
+  private Iterable<Alarm> localise(Iterable<Alarm> alarms) {
+    for (Alarm alarm : alarms) {
+      alarm.setTimestamp(TimeZoneUtil.convertDateTimezone(TimeZone.getDefault(), alarm.getTimestamp(), TimeZone.getTimeZone("UTC")));
+    }
+
+    return alarms;
   }
 
   /**
