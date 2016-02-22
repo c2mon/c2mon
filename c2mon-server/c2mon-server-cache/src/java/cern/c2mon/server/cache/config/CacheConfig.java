6348e6959308a26050635ca3144577abde94fd2e
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-02-22 10:33:36 +0000

    TIMS-1146: Added Java-based configuration for Ehcache

---
+++ c2mon-server/c2mon-server-cache/src/java/cern/c2mon/server/cache/config/CacheConfig.java
@@ -0,0 +1,51 @@
+package cern.c2mon.server.cache.config;
+
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.IOException;
+
+import static java.lang.String.format;
+
+/**
+ * This class is responsible for configuring the in-memory cache.
+ *
+ * There are currently three possible cache "modes" available: single,
+ * single-nonpersistent and multi.
+ *
+ * @author Justin Lewis Salmon
+ */
+@Configuration
+public class CacheConfig {
+
+  @javax.annotation.Resource
+  private Environment environment;
+
+  @Bean(name = "cacheManager")
+  public EhCacheManagerFactoryBean cacheManager() throws IOException {
+    String cacheMode = environment.getRequiredProperty("c2mon.cache.mode");
+
+    switch (cacheMode) {
+      case "single-nonpersistent":
+        return getEhCacheManagerFactoryBean("classpath*:**/ehcache-notTCmode-nonpersistent.xml");
+      case "single":
+        return getEhCacheManagerFactoryBean("classpath*:**/ehcache-notTCmode.xml");
+      case "multi":
+        return getEhCacheManagerFactoryBean("classpath*:**/ehcache-TCmode.xml");
+    }
+
+    throw new RuntimeException(format("Unsupported cache mode specified: '%s'", cacheMode));
+  }
+
+  private EhCacheManagerFactoryBean getEhCacheManagerFactoryBean(String configLocation) throws IOException {
+    PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+    Resource resource = resolver.getResources(configLocation)[0];
+    EhCacheManagerFactoryBean bean = new EhCacheManagerFactoryBean();
+    bean.setConfigLocation(resource);
+    return bean;
+  }
+}
