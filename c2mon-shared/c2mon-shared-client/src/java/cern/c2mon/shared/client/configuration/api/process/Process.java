515bb21a8c9cfa83ffa227ee73d55e3920b0e9af
Franz Ritter <franz.ritter@cern.ch>
2016-04-11 15:50:47 +0000

    TIMS-1157: fixed bug

--- c2mon-shared/c2mon-shared-client/src/java/cern/c2mon/shared/client/configuration/api/process/Process.java
+++ c2mon-shared/c2mon-shared-client/src/java/cern/c2mon/shared/client/configuration/api/process/Process.java
@@ -1,16 +1,16 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
@@ -126,7 +126,7 @@ public class Process implements ConfigurationObject {
     this.maxMessageDelay = maxMessageDelay;
     this.statusTag = statusTag;
     this.aliveTag = aliveTag;
-    this.equipments = equipments == null ? new ArrayList<Equipment>() : equipments;
+    this.equipments = equipments == null || equipments.isEmpty() ? new ArrayList<Equipment>() : equipments;
   }
 
   public Process() {
