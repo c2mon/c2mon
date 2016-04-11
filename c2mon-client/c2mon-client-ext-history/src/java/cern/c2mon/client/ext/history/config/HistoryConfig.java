442b0eb413ee50e560a83701bd151d532671c869
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-04-11 15:18:13 +0000

    Move default history properties into the history module itself

--- c2mon-client/c2mon-client-ext-history/src/java/cern/c2mon/client/ext/history/config/HistoryConfig.java
+++ c2mon-client/c2mon-client-ext-history/src/java/cern/c2mon/client/ext/history/config/HistoryConfig.java
@@ -2,10 +2,12 @@ package cern.c2mon.client.ext.history.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author Justin Lewis Salmon
  */
 @Configuration
+@PropertySource("classpath:history.properties")
 @ImportResource("classpath:cern/c2mon/client/ext/history/config/spring-history.xml")
 public class HistoryConfig {}
