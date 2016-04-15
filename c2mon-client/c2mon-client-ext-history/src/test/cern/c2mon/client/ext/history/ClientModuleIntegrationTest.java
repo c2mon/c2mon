200fe16472f9d569f08d558d7dd668007cc31d1d
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-04-15 08:31:06 +0000

    Fix test

--- c2mon-client/c2mon-client-ext-history/src/test/cern/c2mon/client/ext/history/ClientModuleIntegrationTest.java
+++ c2mon-client/c2mon-client-ext-history/src/test/cern/c2mon/client/ext/history/ClientModuleIntegrationTest.java
@@ -1,16 +1,16 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ * <p>
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ * <p>
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ * <p>
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
@@ -19,22 +19,21 @@ package cern.c2mon.client.ext.history;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Integration test of client module with server core.
- * 
+ *
  * @author Mark Brightwell
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(
-    locations = 
-  {
-        "classpath:cern/c2mon/client/ext/history/c2mon-historymanager-test.xml"
-        ,
-        "classpath:spring-history.xml"
-  })
+@ContextConfiguration(locations = {
+    "classpath:cern/c2mon/client/ext/history/c2mon-historymanager-test.xml",
+    "classpath:spring-history.xml"
+})
+@TestPropertySource("classpath:application.properties")
 public class ClientModuleIntegrationTest {
 
   /**
@@ -44,5 +43,4 @@ public class ClientModuleIntegrationTest {
   public void moduleStartUp() {
     //do nothing
   }
-  
 }
