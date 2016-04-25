4c6144b8e7f25d559caabe985ab83cd085e7cc95
Felix Ehm <felix.ehm@cern.ch>
2016-04-25 13:30:09 +0000

    Removed obsolete log4j renderer

--- c2mon-publisher/c2mon-publisher-rda3/src/java/cern/c2mon/publisher/rda/RdaPublisher.java
+++ c2mon-publisher/c2mon-publisher-rda3/src/java/cern/c2mon/publisher/rda/RdaPublisher.java
@@ -21,7 +21,6 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import javax.annotation.PostConstruct;
 
-import org.apache.log4j.or.ObjectRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
