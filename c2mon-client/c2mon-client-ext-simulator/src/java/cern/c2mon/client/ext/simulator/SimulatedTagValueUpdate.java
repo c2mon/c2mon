6302283d1fa3351130fb766be1876e424fa46b1d
Matthias Braeger <matthias.braeger@cern.ch>
2016-04-25 09:21:50 +0000

    TIMS-1181: Added code which got deleted by mistake

---
+++ c2mon-client/c2mon-client-ext-simulator/src/java/cern/c2mon/client/ext/simulator/SimulatedTagValueUpdate.java
@@ -0,0 +1,179 @@
+/******************************************************************************
+ * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
+ * 
+ * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
+ * C2MON is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the license.
+ * 
+ * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+package cern.c2mon.client.ext.simulator;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import cern.c2mon.client.common.tag.Tag;
+import cern.c2mon.client.core.tag.ClientDataTagImpl;
+import cern.c2mon.shared.client.alarm.AlarmValue;
+import cern.c2mon.shared.client.tag.TagMode;
+import cern.c2mon.shared.client.tag.TagValueUpdate;
+import cern.c2mon.shared.common.datatag.DataTagQuality;
+import cern.c2mon.shared.common.datatag.DataTagQualityImpl;
+import cern.c2mon.shared.common.datatag.TagQualityStatus;
+
+/**
+ * This class is used internally by {@link TagSimulatorImpl} making a cache 
+ * update. When setting the simulated value this class creates a new timestamp.
+ * Source-, DAQ- and Server timestamp are then the same.
+ *
+ * @author Matthias Braeger
+ */
+class SimulatedTagValueUpdate implements TagValueUpdate {
+  
+  /** The client data tag clone */
+  private Tag tag;
+  
+  /** The simulated quality object */
+  private final DataTagQualityImpl quality = new DataTagQualityImpl();
+  
+  /** The new value */
+  private Object simulatedValue = null;
+  
+  /** The simulated source timestamp */
+  private Timestamp simulatedTimestamp = null;
+  
+  /**
+   * Copy constructor
+   * 
+   * @param cdt The tag that will receive a simulated value
+   * @throws CloneNotSupportedException In case that the Tag instance is not 
+   *         clonable.
+   */
+  protected SimulatedTagValueUpdate(final Tag cdt) {
+    this.tag = ((ClientDataTagImpl) cdt).clone();
+    quality.validate();
+  }
+  
+  /**
+   * Sets the simulated value and generates at the same time a new source timestamp
+   * @param value The simulated value
+   * @exception UnsupportedOperationException In case the value update object is not from the same
+   *            type as the tag.
+   * @see Tag#getType()
+   */
+  void setValue(final Object value) throws ClassCastException {
+    boolean setValue = false;
+    
+    if (tag.getDataTagQuality().isInitialised()) {
+      if (tag.getType().isInstance(value)) {
+        setValue = true;
+      }
+      else {
+        throw new ClassCastException(
+            "The simulated value update object for tag " + tag.getId() + " is not of type " + tag.getType().toString());
+      }
+    }
+    else {
+      setValue = true;
+    }
+    
+    if (setValue) {
+      this.simulatedValue = value;
+      quality.validate();
+      simulatedTimestamp = new Timestamp(System.currentTimeMillis());
+    }
+  }
+  
+  void invalidateTag(final TagQualityStatus status) {
+    quality.addInvalidStatus(status);
+    simulatedTimestamp = new Timestamp(System.currentTimeMillis());
+  }
+
+  @Override
+  public Long getId() {
+    return tag.getId();
+  }
+
+  @Override
+  public DataTagQuality getDataTagQuality() {
+    if (simulatedValue == null && quality.isValid()) { // allows overwriting initial quality flag
+      return tag.getDataTagQuality();
+    }
+    else {
+      return quality;
+    }
+  }
+
+  @Override
+  public Object getValue() {
+    if (simulatedValue == null) {
+      return tag.getValue();
+    }
+    else {
+      return simulatedValue;
+    }
+  }
+
+  @Override
+  public Timestamp getSourceTimestamp() {
+    if (simulatedTimestamp == null) {
+      return tag.getTimestamp();
+    }
+    else {
+      return simulatedTimestamp;
+    }
+  }
+
+  @Override
+  public Timestamp getDaqTimestamp() {
+    if (simulatedTimestamp == null) {
+      return tag.getDaqTimestamp();
+    }
+    else {
+      return simulatedTimestamp;
+    }
+  }
+
+  @Override
+  public Timestamp getServerTimestamp() {
+    if (simulatedTimestamp == null) {
+      return tag.getServerTimestamp();
+    }
+    else {
+      return simulatedTimestamp;
+    }
+  }
+
+  @Override
+  public String getDescription() {
+    return tag.getDescription();
+  }
+
+  @Override
+  public Collection<AlarmValue> getAlarms() {
+    return new ArrayList<AlarmValue>();
+  }
+
+  @Override
+  public TagMode getMode() {
+    return tag.getMode();
+  }
+
+  @Override
+  public boolean isSimulated() {
+    return true;
+  }
+
+  @Override
+  public String getValueDescription() {
+    return tag.getValueDescription();
+  }
+}
---
+++ c2mon-client/c2mon-client-ext-simulator/src/java/cern/c2mon/client/ext/simulator/TagSimulatorImpl.java
@@ -0,0 +1,192 @@
+/******************************************************************************
+ * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
+ * 
+ * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
+ * C2MON is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the license.
+ * 
+ * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+package cern.c2mon.client.ext.simulator;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cern.c2mon.client.common.tag.Tag;
+import cern.c2mon.client.core.cache.BasicCacheHandler;
+import cern.c2mon.client.core.tag.ClientDataTagImpl;
+import cern.c2mon.shared.common.datatag.TagQualityStatus;
+
+/**
+ * This class implements the {@link TagSimulator} interface. For more
+ * information please read the interface description.
+ * <p>
+ * Internally the functionalities are realized by making use of the history 
+ * cache which is provided by the {@link BasicCacheHandler}. It is therefore
+ * not possible to use the simulator whilst being in history mode.
+ *
+ * @author Matthias Braeger
+ */
+@Service
+class TagSimulatorImpl implements TagSimulator {
+  
+  /** Log4j instance */
+  private static final Logger LOG = LoggerFactory.getLogger(TagSimulatorImpl.class);
+  
+  /** Reference to the <code>ClientDataTagCache</code> */
+  private final BasicCacheHandler cache;
+  
+  /** Flag to remember, if the simulation is on or off */
+  private boolean simulationModeOn = false;
+
+  /**
+   * Default Constructor
+   * @param pCache Reference to the basic cache handler
+   */
+  @Autowired
+  protected TagSimulatorImpl(final BasicCacheHandler pCache) {
+    this.cache = pCache;
+  }
+  
+  @Override
+  public synchronized boolean startSimulationMode() {
+    if (simulationModeOn) {
+      return simulationModeOn;
+    }
+    
+    if (!simulationModeOn && !cache.isHistoryModeEnabled()) {
+      cache.setHistoryMode(true);
+      simulationModeOn = true;
+      return simulationModeOn;
+    }
+    
+    return false;
+  }
+
+  @Override
+  public synchronized void stopSimulationMode() {
+    if (simulationModeOn) {
+      cache.setHistoryMode(false);
+      simulationModeOn = false;
+    }
+  }
+
+  @Override
+  public boolean isSimulationModeEnabled() {
+    return simulationModeOn;
+  }
+
+  @Override
+  public boolean changeValue(final Long tagId, final Object value) throws ClassCastException {
+    boolean retval = false;
+    synchronized (cache.getHistoryModeSyncLock()) {
+      if (isSimulationModeEnabled()) {
+        Tag cdt = cache.get(tagId);
+        if (cdt != null) {
+          SimulatedTagValueUpdate update = new SimulatedTagValueUpdate(cdt);
+          update.setValue(value);
+          ((ClientDataTagImpl) cdt).update(update);
+          retval = true;
+        }
+      }
+    }
+    
+    return retval;
+  }
+
+  @Override
+  public boolean changeValues(final Map<Long, Object> tagValues) {
+    boolean allOk = true;
+    synchronized (cache.getHistoryModeSyncLock()) {
+      if (isSimulationModeEnabled()) {
+        Tag cdt;
+        for (Long tagId : tagValues.keySet()) {
+          cdt = cache.get(tagId);
+          if (cdt != null) {
+            try {
+              SimulatedTagValueUpdate update = new SimulatedTagValueUpdate(cdt);
+              update.setValue(tagValues.get(tagId));
+              ((ClientDataTagImpl) cdt).update(update);
+            }
+            catch (Exception ex) {
+              LOG.error("changeValues() - A problem occured whilst updating tag " + tagId, ex);
+              allOk = false;
+            }
+          }
+          else {
+            allOk = false;
+          }
+        } // end for loop
+      }
+      else {
+        allOk = false;
+      }
+    }
+    
+    return allOk;
+  }
+
+  @Override
+  public boolean invalidateTag(final  Long tagId, final TagQualityStatus status) {
+    boolean retval = false;
+    
+    synchronized (cache.getHistoryModeSyncLock()) {
+      if (isSimulationModeEnabled()) {
+        Tag cdt = cache.get(tagId);
+        if (cdt != null) {
+          SimulatedTagValueUpdate update = new SimulatedTagValueUpdate(cdt);
+          update.invalidateTag(status);
+          ((ClientDataTagImpl) cdt).update(update);
+          retval = true;
+        }
+      }
+    }
+    
+    return retval;
+  }
+
+  @Override
+  public boolean invalidateTags(final Set<Long> tagIds, final TagQualityStatus status) {
+    boolean allOk = true;
+    synchronized (cache.getHistoryModeSyncLock()) {
+      if (isSimulationModeEnabled()) {
+        Tag cdt;
+        for (Long tagId : tagIds) {
+          cdt = cache.get(tagId);
+          if (cdt != null) {
+            try {
+              SimulatedTagValueUpdate update = new SimulatedTagValueUpdate(cdt);
+              update.invalidateTag(status);
+              ((ClientDataTagImpl) cdt).update(update);
+            }
+            catch (Exception ex) {
+              LOG.error("invalidateTags() - A problem occured whilst updating tag " + tagId, ex);
+              allOk = false;
+            }
+          }
+          else {
+            allOk = false;
+          }
+        } // end for loop
+      }
+      else {
+        allOk = false;
+      }
+    }
+    
+    return allOk;
+  }
+
+}
