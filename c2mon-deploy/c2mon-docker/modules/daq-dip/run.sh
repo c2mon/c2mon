03b30e1f0d7ec6831dcff0b00778e1bfc0acb5ae
Brice Copy <brice.copy@cern.ch>
2016-05-18 13:10:46 +0000

    fixed DAQ startup command

--- c2mon-deploy/c2mon-docker/modules/daq-dip/run.sh
+++ c2mon-deploy/c2mon-docker/modules/daq-dip/run.sh
@@ -1,1 +1,1 @@
-docker run --rm --name daq-dip -ti --link c2mon:c2mon docker.cern.ch/c2mon/daq-dip
+docker run --rm --name daq-dip -ti --net=host -e "DAQ_PROCESS_NAME=P_DYNDIP" docker.cern.ch/c2mon-project/daq-dip
---
+++ c2mon-deploy/c2mon-docker/modules/daq-opc-ua/run.sh
@@ -0,0 +1,2 @@
+docker run --rm --name daq-opcua -ti --net=host -e "DAQ_PROCESS_NAME=P_DYNOPCUA" docker.cern.ch/c2mon-project/daq-opcua
+
