dc8714852388dfd5af53308efe429c18de9c71bc
Brice Copy <brice.copy@cern.ch>
2016-05-19 07:39:39 +0000

    added host network

--- c2mon-deploy/c2mon-docker/modules/daq-opcua/run.sh
+++ c2mon-deploy/c2mon-docker/modules/daq-opcua/run.sh
@@ -1,2 +1,1 @@
-docker run --rm --name daq-opcua -ti --net=host -e "DAQ_PROCESS_NAME=P_DYNOPCUA" docker.cern.ch/c2mon-project/daq-opcua
-
+docker  run --rm --name daq-opcua -ti --net=host -e "DAQ_PROCESS_NAME=P_DYNOPCUA" -e "C2MON_PORT_61616_TCP=tcp://localhost:61616" docker.cern.ch/c2mon-project/daq-opcua
