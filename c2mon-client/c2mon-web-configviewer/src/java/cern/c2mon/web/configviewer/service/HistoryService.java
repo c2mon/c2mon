59aae356e435848374b6981c3a7294d90d2f06e5
Franz Ritter <franz.ritter@cern.ch>
2016-05-19 13:55:30 +0000

    TIMS-1147: Fix compilation error

--- c2mon-client/c2mon-web-configviewer/src/java/cern/c2mon/web/configviewer/service/HistoryService.java
+++ c2mon-client/c2mon-web-configviewer/src/java/cern/c2mon/web/configviewer/service/HistoryService.java
@@ -1,16 +1,16 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
@@ -372,7 +372,7 @@ public class HistoryService {
     final int size = historyValues.size();
     if (size > 0) {
       final HistoryTagValueUpdate h = historyValues.iterator().next();
-      final String type = h.getDataType();
+      final String type = h.getValueClassName();
       final boolean isBooleanData = type.equals("Boolean");
       return isBooleanData;
     }
