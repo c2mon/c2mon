6d55dd5cd8c3c245eaca65c7546b38af2ea4ffd2
Brice Copy <brice.copy@cern.ch>
2016-06-09 15:18:31 +0000

    Added entrypoint for DAQ processes and forwarding of arguments

--- c2mon-deploy/c2mon-docker/modules/daq-dip/run.sh
+++ c2mon-deploy/c2mon-docker/modules/daq-dip/run.sh
@@ -1,1 +1,1 @@
-docker run --rm --name daq-dip -ti --net=host -e "DAQ_PROCESS_NAME=P_DYNDIP" -e "C2MON_PORT_61616_TCP=tcp://localhost:61616" docker.cern.ch/c2mon-project/daq-dip
+docker run --rm --name daq-dip -ti --net=host -e "C2MON_PORT_61616_TCP=tcp://localhost:61616" docker.cern.ch/c2mon-project/daq-dip $@
