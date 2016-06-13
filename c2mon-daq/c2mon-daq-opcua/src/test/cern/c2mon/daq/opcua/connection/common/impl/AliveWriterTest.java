e44b238028dbdb6b2d52b9e53c5c97f2b408519c
Matthias Braeger <matthias.braeger@cern.ch>
2016-06-13 12:59:35 +0000

    Cleanup of obsolete imports

--- c2mon-daq/c2mon-daq-opcua/src/test/cern/c2mon/daq/opcua/connection/common/impl/AliveWriterTest.java
+++ c2mon-daq/c2mon-daq-opcua/src/test/cern/c2mon/daq/opcua/connection/common/impl/AliveWriterTest.java
@@ -1,16 +1,16 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
@@ -21,7 +21,6 @@ import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
 
-import cern.c2mon.daq.common.conf.core.RunOptions;
 import cern.c2mon.daq.common.logger.EquipmentLogger;
 import cern.c2mon.daq.common.logger.EquipmentLoggerFactory;
 import cern.c2mon.daq.opcua.connection.common.IOPCEndpoint;
@@ -33,54 +32,54 @@ import cern.c2mon.shared.common.datatag.address.OPCHardwareAddress;
 import cern.c2mon.shared.common.datatag.address.impl.OPCHardwareAddressImpl;
 
 public class AliveWriterTest {
-    
+
     private OPCHardwareAddress hardwareAddress;
     private ISourceDataTag targetTag;
     private AliveWriter aliveWriter;
-    
+
     private EquipmentLogger equipmentLogger;
-    
+
     /**
      * Mocks
      */
-    private EquipmentLoggerFactory equipmentLoggerFactoryMock; 
+    private EquipmentLoggerFactory equipmentLoggerFactoryMock;
     private IOPCEndpoint endpointMock;
-        
-    
-    
+
+
+
     @Before
     public void setUp() throws ConfigurationException {
         this.endpointMock = EasyMock.createMock(IOPCEndpoint.class);
         this.hardwareAddress = new OPCHardwareAddressImpl("s");
         DataTagAddress address = new DataTagAddress(hardwareAddress);
         this.targetTag =new SourceDataTag(1L, "asd", true, (short) 0, "Integer", address);
-        
+
         this.equipmentLogger = new EquipmentLogger("Test Logger", "Test Logger", "AliveWriter");
         this.equipmentLoggerFactoryMock = EasyMock.createMock(EquipmentLoggerFactory.class);
     }
-    
+
     @Test
     public void testRun() {
-        
+
         EasyMock.expect(this.equipmentLoggerFactoryMock.getEquipmentLogger(AliveWriter.class)).andReturn(this.equipmentLogger).times(1);
-        
+
         for (int k=0; k < 3; k++) {
             for (int i=0; i < Byte.MAX_VALUE; i++) {
               this.endpointMock.write(this.hardwareAddress, Integer.valueOf(i));
             }
           }
-        
+
         EasyMock.replay(this.endpointMock, this.equipmentLoggerFactoryMock);
-       
+
         this.aliveWriter = new AliveWriter(this.endpointMock, 1000L, this.targetTag, this.equipmentLoggerFactoryMock);
-        
-        
+
+
         for (int k=0; k < 3; k++) {
           for (int i=0; i < Byte.MAX_VALUE; i++) {
             this.aliveWriter.run();
           }
         }
-        
+
         EasyMock.verify(this.endpointMock);
     }
 
