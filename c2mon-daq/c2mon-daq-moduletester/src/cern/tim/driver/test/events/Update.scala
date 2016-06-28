2c4dc9096662f8e25d2c64b79fff359fdb68395c
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-06-28 10:02:10 +0200

    Remove CERN-specific DAQ implementations from core repo

--- c2mon-daq/c2mon-daq-moduletester/src/cern/tim/driver/test/events/Update.scala
+++
@@ -1,8 +0,0 @@
-package cern.tim.driver.test.events
-import java.util.Date
-
-class Update(execTime:Date) extends Event {
-  executionTime = execTime
-  def run() = {}
-  override def toString() = eventId + "|Update()"
-}
