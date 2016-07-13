88f39840717438813a7f5616985095e7cada76a1
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-07-13 16:41:52 +0200

    Polishing

--- c2mon-client/c2mon-client-core/src/main/java/cern/c2mon/client/core/config/C2monApplicationListener.java
+++ c2mon-client/c2mon-client-core/src/main/java/cern/c2mon/client/core/config/C2monApplicationListener.java
@@ -1,6 +1,8 @@
 package cern.c2mon.client.core.config;
 
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
@@ -10,26 +12,26 @@ import org.springframework.core.io.support.ResourcePropertySource;
 import java.io.IOException;
 
 /**
- * Listens for the {@link ApplicationEnvironmentPreparedEvent} and injects ${c2mon.client.conf.url}
- * into the environment with the highest precedence (if it exists)
+ * Listens for the {@link ApplicationEnvironmentPreparedEvent} and injects
+ * ${c2mon.client.conf.url} into the environment with the highest precedence
+ * (if it exists). This is in order to allow users to point to an external
+ * properties file via ${c2mon.client.conf.url}.
  *
  * @author Justin Lewis Salmon
  */
 @Configuration
-public class C2monApplicationListener implements ApplicationListener {
+public class C2monApplicationListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
 
   @Override
-  public void onApplicationEvent(ApplicationEvent event) {
-    if (event instanceof ApplicationEnvironmentPreparedEvent) {
-      ConfigurableEnvironment environment = ((ApplicationEnvironmentPreparedEvent) event).getEnvironment();
-      String propertySource = environment.getProperty("c2mon.client.conf.url");
+  public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+    ConfigurableEnvironment environment = event.getEnvironment();
+    String propertySource = environment.getProperty("c2mon.client.conf.url");
 
-      if (propertySource != null) {
-        try {
-          environment.getPropertySources().addAfter("systemEnvironment", new ResourcePropertySource(propertySource));
-        } catch (IOException e) {
-          throw new RuntimeException("Could not read property source", e);
-        }
+    if (propertySource != null) {
+      try {
+        environment.getPropertySources().addAfter("systemEnvironment", new ResourcePropertySource(propertySource));
+      } catch (IOException e) {
+        throw new RuntimeException("Could not read property source", e);
       }
     }
   }
