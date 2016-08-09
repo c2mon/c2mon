f488ae259c1cc638ce5ed7a17e2b63f3c76ba404
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-08-09 14:27:27 +0200

    Enhance versions.json

--- docs/build.sh
+++ docs/build.sh
@@ -7,25 +7,35 @@ function usage {
 [[ -z $1 ]] && usage && exit 1
 VERSION=$1
 
+LATEST_RELEASE=`git tag -l | head -n 1`
+
+if [[ ${VERSION} =~ "SNAPSHOT" ]]; then
+  LATEST_SNAPSHOT=${VERSION}
+fi
+
 # Invoke mkdocs
 mkdocs build --clean --site-dir=target/${VERSION}
 
 # Generate versions.json
 VERSIONS_FILE=target/versions.json
 
-echo '[' > ${VERSIONS_FILE}
+echo "{" > ${VERSIONS_FILE}
+echo "  \"release\":  \"$LATEST_RELEASE\""  >> ${VERSIONS_FILE}
+echo "  \"snapshot\": \"$LATEST_SNAPSHOT\""  >> ${VERSIONS_FILE}
+echo "  \"versions\": [" >> ${VERSIONS_FILE}
 
 # Include all released versions
 for v in `git tag -l`;
-  do echo "  \"${v}\"," >> ${VERSIONS_FILE};
+  do echo "    \"${v}\"," >> ${VERSIONS_FILE}
 done
 
 # If this is a snapshot build, also include the snapshot version
 if [[ ${VERSION} =~ "SNAPSHOT" ]]; then
-  echo "  \"${VERSION}\"" >> ${VERSIONS_FILE};
+  echo "    \"${VERSION}\"" >> ${VERSIONS_FILE}
 fi
 
-echo ']' >> ${VERSIONS_FILE}
+echo "  ]" >> ${VERSIONS_FILE}
+echo "}" >> ${VERSIONS_FILE}
 
 # If this is a snapshot build, copy the docs to the "snapshot" directory.
 # Otherwise, copy them to the "latest" directory. Note that a symlink will not
