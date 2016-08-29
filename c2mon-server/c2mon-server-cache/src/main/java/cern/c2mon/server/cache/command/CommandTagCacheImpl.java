5123998b79dd7259cce597c43492956b34864a33
Franz Ritter <franz.ritter@cern.ch>
2016-08-29 14:57:51 +0200

    Add the ehcache method for receiving a command tag id by name
    Issue: #41

--- c2mon-server/c2mon-server-cache/src/main/java/cern/c2mon/server/cache/command/CommandTagCacheImpl.java
+++ c2mon-server/c2mon-server-cache/src/main/java/cern/c2mon/server/cache/command/CommandTagCacheImpl.java
@@ -1,16 +1,16 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
@@ -18,9 +18,13 @@ package cern.c2mon.server.cache.command;
 
 import javax.annotation.PostConstruct;
 
+import cern.c2mon.server.cache.exception.CacheElementNotFoundException;
 import net.sf.ehcache.Ehcache;
 import net.sf.ehcache.loader.CacheLoader;
 
+import net.sf.ehcache.search.Attribute;
+import net.sf.ehcache.search.Query;
+import net.sf.ehcache.search.Results;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,7 +41,7 @@ import cern.c2mon.shared.common.command.CommandTag;
 
 /**
  * Implementation of the CommandTag cache.
- * 
+ *
  * @author Mark Brightwell
  *
  */
@@ -47,21 +51,21 @@ public class CommandTagCacheImpl extends AbstractCache<Long, CommandTag> impleme
   /**
    * Private class logger.
    */
-  private static final Logger LOGGER = LoggerFactory.getLogger(CommandTagCacheImpl.class); 
- 
+  private static final Logger LOGGER = LoggerFactory.getLogger(CommandTagCacheImpl.class);
+
   @Autowired
-  public CommandTagCacheImpl(final ClusterCache clusterCache, 
+  public CommandTagCacheImpl(final ClusterCache clusterCache,
                           @Qualifier("commandTagEhcache") final Ehcache ehcache,
-                          @Qualifier("commandTagEhcacheLoader") final CacheLoader cacheLoader, 
+                          @Qualifier("commandTagEhcacheLoader") final CacheLoader cacheLoader,
                           @Qualifier("commandTagCacheLoader") final C2monCacheLoader c2monCacheLoader,
                           @Qualifier("commandTagDAO") final SimpleCacheLoaderDAO<CommandTag> cacheLoaderDAO) {
-    super(clusterCache, ehcache, cacheLoader, c2monCacheLoader, cacheLoaderDAO);    
-  } 
-  
+    super(clusterCache, ehcache, cacheLoader, c2monCacheLoader, cacheLoaderDAO);
+  }
+
   @PostConstruct
-  public void init() {        
+  public void init() {
     LOGGER.info("Initializing the CommandTag cache...");
-    commonInit();        
+    commonInit();
     LOGGER.info("... CommandTag cache initialization complete.");
   }
 
@@ -74,7 +78,39 @@ public class CommandTagCacheImpl extends AbstractCache<Long, CommandTag> impleme
   protected C2monCacheName getCacheName() {
     return C2monCacheName.COMMAND;
   }
-  
+
+  @Override
+  public Long getCommandTagId(final String name) {
+    Long commandTagKey = null;
+    Results results = null;
+
+    if (name == null || name.equalsIgnoreCase("")) {
+      throw new IllegalArgumentException("Attempting to retrieve a CommandTag from the cache with a NULL or empty name parameter.");
+    }
+
+    try {
+      Attribute<String> commandTagName = getCache().getSearchAttribute("commandTagName");
+      Query query = getCache().createQuery();
+      results = query.includeKeys().addCriteria(commandTagName.eq(name)).maxResults(1).execute();
+
+      // Find the number of results -- the number of hits.
+      int size = results.size();
+      if (size == 0) {
+        LOGGER.info("Failed to find a command tag with name " + name + " in the cache.");
+      }
+
+      commandTagKey = (Long) results.all().get(0).getKey();
+    }
+    finally {
+      if (results != null) {
+        // Discard the results when done to free up cache resources.
+        results.discard();
+      }
+    }
+
+    return commandTagKey;
+  }
+
   @Override
   protected String getCacheInitializedKey() {
     return cacheInitializedKey;
