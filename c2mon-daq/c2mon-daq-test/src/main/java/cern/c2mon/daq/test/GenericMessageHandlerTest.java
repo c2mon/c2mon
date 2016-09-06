7f29b92f9a657f0e5673ba48bcc42390f80fb971
Szymon Halastra <szymon.edward.halastra@cern.ch>
2016-09-06 15:40:02 +0200

    Organize GenericMessageHandlerTest code

--- c2mon-daq/c2mon-daq-test/src/main/java/cern/c2mon/daq/test/GenericMessageHandlerTest.java
+++ c2mon-daq/c2mon-daq-test/src/main/java/cern/c2mon/daq/test/GenericMessageHandlerTest.java
@@ -17,14 +17,8 @@
 
 package cern.c2mon.daq.test;
 
-import static java.lang.String.format;
-import static org.easymock.EasyMock.createMock;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
-
 import java.lang.reflect.Method;
 
-import org.easymock.EasyMock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.xerces.parsers.DOMParser;
@@ -33,7 +27,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
-import org.springframework.context.ApplicationContext;
 import org.w3c.dom.Element;
 
 import cern.c2mon.daq.common.EquipmentMessageHandler;
@@ -42,7 +35,6 @@ import cern.c2mon.daq.common.conf.core.EquipmentConfigurationFactory;
 import cern.c2mon.daq.common.conf.core.EquipmentConfigurationHandler;
 import cern.c2mon.daq.common.impl.EquipmentCommandHandler;
 import cern.c2mon.daq.common.impl.EquipmentMessageSender;
-import cern.c2mon.daq.common.logger.EquipmentLoggerFactory;
 import cern.c2mon.daq.common.messaging.IProcessMessageSender;
 import cern.c2mon.daq.common.messaging.impl.RequestController;
 import cern.c2mon.daq.filter.IFilterMessageSender;
@@ -51,182 +43,185 @@ import cern.c2mon.daq.filter.dynamic.TimeDifferenceMovingAverageTimeDeadbandActi
 import cern.c2mon.shared.common.process.EquipmentConfiguration;
 import cern.c2mon.shared.common.process.ProcessConfiguration;
 
+import static java.lang.String.format;
+import static org.easymock.EasyMock.createMock;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
 /**
  * This class implements a common parent class for JUnit testing framework for EquipmentMessageHandlers.
+ *
  * @author wbuczak
  */
 
 public abstract class GenericMessageHandlerTest {
 
-    protected static final String TEST_PROCESS_NAME = "P_TEST_01";
+  protected static final String TEST_PROCESS_NAME = "P_TEST_01";
 
-    protected static final Long TEST_PROCESS_ID = 1L;
+  protected static final Long TEST_PROCESS_ID = 1L;
 
-    protected EquipmentMessageHandler msgHandler;
+  protected EquipmentMessageHandler msgHandler;
 
-    protected IProcessMessageSender messageSender;
+  protected IProcessMessageSender messageSender;
 
-    protected IFilterMessageSender filterMessageSender;
+  protected IFilterMessageSender filterMessageSender;
 
-    protected IDynamicTimeDeadbandFilterActivator medDynamicTimeDeadbandFilterActivator;
+  protected IDynamicTimeDeadbandFilterActivator medDynamicTimeDeadbandFilterActivator;
 
-    protected IDynamicTimeDeadbandFilterActivator lowDynamicTimeDeadbandFilterActivator;
+  protected IDynamicTimeDeadbandFilterActivator lowDynamicTimeDeadbandFilterActivator;
 
-    protected EquipmentMessageSender equipmentMessageSender;
+  protected EquipmentMessageSender equipmentMessageSender;
 
-    protected ProcessConfiguration pconf = new ProcessConfiguration();
+  protected ProcessConfiguration pconf = new ProcessConfiguration();
 
-    protected EquipmentConfiguration equipmentConfiguration;
+  protected EquipmentConfiguration equipmentConfiguration;
 
-    protected ConfigurationController configurationController;
+  protected ConfigurationController configurationController;
 
-    static Logger log = LoggerFactory.getLogger(GenericMessageHandlerTest.class);
+  static Logger log = LoggerFactory.getLogger(GenericMessageHandlerTest.class);
 
-    @Rule
-    public TestName testName = new TestName();
+  @Rule
+  public TestName testName = new TestName();
 
-    protected final String configFileName(final String testName) throws Exception {
+  protected final String configFileName(final String testName) throws Exception {
 
-        Method testMethod = this.getClass().getMethod(testName, (Class<?>[]) null);
+    Method testMethod = this.getClass().getMethod(testName, (Class<?>[]) null);
 
-        if (testMethod.isAnnotationPresent(UseConf.class)) {
-            return format("/conf/%s", testMethod.getAnnotation(UseConf.class).value());
-        }
-
-        return null;
+    if (testMethod.isAnnotationPresent(UseConf.class)) {
+      return format("/conf/%s", testMethod.getAnnotation(UseConf.class).value());
     }
 
-    protected final String getHandlerClass() throws Exception {
-        Class<? extends GenericMessageHandlerTest> clazz = this.getClass();
+    return null;
+  }
 
-        if (clazz.isAnnotationPresent(UseHandler.class)) {
-            return clazz.getAnnotation(UseHandler.class).value().getName();
-        }
+  protected final String getHandlerClass() throws Exception {
+    Class<? extends GenericMessageHandlerTest> clazz = this.getClass();
 
-        return null;
+    if (clazz.isAnnotationPresent(UseHandler.class)) {
+      return clazz.getAnnotation(UseHandler.class).value().getName();
     }
 
-
-    protected abstract void beforeTest() throws Exception;
-
-    protected abstract void afterTest() throws Exception;
+    return null;
+  }
 
 
-    @Before
-    public void setUp() throws Exception {
+  protected abstract void beforeTest() throws Exception;
 
-        int numTests = 0;
-        // check how many test cases are defined in that test class
-        for (Method m : this.getClass().getMethods()) {
-            if (m.isAnnotationPresent(Test.class)) {
-                numTests++;
-            }
-        }
-
-        log.debug(format("number of tests configured in that class: %d", numTests));
-
-        if (log.isDebugEnabled()) {
-            log.debug("****************************************************************\n");
-            log.debug(format("executing test: %s", testName.getMethodName()));
-            log.debug("****************************************************************\n");
-        }
-
-        // find out which configuration file shall be taken
-        String testConfig = configFileName(testName.getMethodName());
-
-        // configure the handler only if the test requires it - i.e. is annotated with UseConf annotation
-        if (testConfig != null) {
-
-            messageSender = createMock(IProcessMessageSender.class);
-            filterMessageSender = createMock(IFilterMessageSender.class);
-
-            lowDynamicTimeDeadbandFilterActivator = new TimeDifferenceMovingAverageTimeDeadbandActivator(10, 110, 150, 30000);
-
-            medDynamicTimeDeadbandFilterActivator = new TimeDifferenceMovingAverageTimeDeadbandActivator(6, 6000, 44, 44);
-
-            equipmentMessageSender = new EquipmentMessageSender(filterMessageSender, messageSender,
-                    medDynamicTimeDeadbandFilterActivator, lowDynamicTimeDeadbandFilterActivator);
-
-            configurationController = new ConfigurationController();
-            configurationController.setProcessConfiguration(pconf);
-            pconf.setProcessID(TEST_PROCESS_ID);
-            pconf.setProcessName(TEST_PROCESS_NAME);
-
-            pconf.setAliveTagID(1000L);
-            pconf.setAliveInterval(20000);
-
-            if (log.isDebugEnabled()) {
-                log
-                        .debug(format("configuring equipment with following test configuration file: %s\n",
-                                testConfig));
-            }
+  protected abstract void afterTest() throws Exception;
 
 
-            String confFileAbsPath = null;
-            try {
-                confFileAbsPath = this.getClass().getResource(testConfig).getPath();
-            }
-            catch (Exception ex) {
-                fail("configuration file not found: "+testConfig);
-            }
+  @Before
+  public void setUp() throws Exception {
+
+    int numTests = 0;
+    // check how many test cases are defined in that test class
+    for (Method m : this.getClass().getMethods()) {
+      if (m.isAnnotationPresent(Test.class)) {
+        numTests++;
+      }
+    }
+
+    log.debug(format("number of tests configured in that class: %d", numTests));
+
+    if (log.isDebugEnabled()) {
+      log.debug("****************************************************************\n");
+      log.debug(format("executing test: %s", testName.getMethodName()));
+      log.debug("****************************************************************\n");
+    }
+
+    // find out which configuration file shall be taken
+    String testConfig = configFileName(testName.getMethodName());
+
+    // configure the handler only if the test requires it - i.e. is annotated with UseConf annotation
+    if (testConfig != null) {
+
+      messageSender = createMock(IProcessMessageSender.class);
+      filterMessageSender = createMock(IFilterMessageSender.class);
+
+      lowDynamicTimeDeadbandFilterActivator = new TimeDifferenceMovingAverageTimeDeadbandActivator(10, 110, 150, 30000);
+
+      medDynamicTimeDeadbandFilterActivator = new TimeDifferenceMovingAverageTimeDeadbandActivator(6, 6000, 44, 44);
+
+      equipmentMessageSender = new EquipmentMessageSender(filterMessageSender, messageSender,
+              medDynamicTimeDeadbandFilterActivator, lowDynamicTimeDeadbandFilterActivator);
+
+      configurationController = new ConfigurationController();
+      configurationController.setProcessConfiguration(pconf);
+      pconf.setProcessID(TEST_PROCESS_ID);
+      pconf.setProcessName(TEST_PROCESS_NAME);
+
+      pconf.setAliveTagID(1000L);
+      pconf.setAliveInterval(20000);
+
+      if (log.isDebugEnabled()) {
+        log
+                .debug(format("configuring equipment with following test configuration file: %s\n",
+                        testConfig));
+      }
 
 
-            Element rootConfElement = parseEquipmentConfiguration(confFileAbsPath);
-            //ProcessConfigurationLoader processConfigurationLoader = new ProcessConfigurationLoader();
-            assertNotNull(rootConfElement);
+      String confFileAbsPath = null;
+      try {
+        confFileAbsPath = this.getClass().getResource(testConfig).getPath();
+      }
+      catch (Exception ex) {
+        fail("configuration file not found: " + testConfig);
+      }
 
 
-            assertNotNull("Did you annotate your test with @UseHandler annotation ?!", getHandlerClass());
+      Element rootConfElement = parseEquipmentConfiguration(confFileAbsPath);
+      //ProcessConfigurationLoader processConfigurationLoader = new ProcessConfigurationLoader();
+      assertNotNull(rootConfElement);
 
-            // update handler name
-            rootConfElement.getElementsByTagName("handler-class-name").item(0).getFirstChild().setNodeValue(
-                    getHandlerClass());
 
-            equipmentConfiguration = EquipmentConfigurationFactory.getInstance().createEquipmentConfiguration(rootConfElement);
-            long equipmentId = equipmentConfiguration.getId();
-            EquipmentLoggerFactory factory = EquipmentLoggerFactory.createFactory(equipmentConfiguration, pconf, false, false);
-            msgHandler = EquipmentMessageHandler.createEquipmentMessageHandler(
-                    equipmentConfiguration.getHandlerClassName(),
-                    new EquipmentCommandHandler(equipmentId, new RequestController(configurationController)),
-                    new EquipmentConfigurationHandler(equipmentId, configurationController),
-                    equipmentMessageSender, null);
-            msgHandler.setEquipmentLoggerFactory(factory);
+      assertNotNull("Did you annotate your test with @UseHandler annotation ?!", getHandlerClass());
+
+      // update handler name
+      rootConfElement.getElementsByTagName("handler-class-name").item(0).getFirstChild().setNodeValue(
+              getHandlerClass());
+
+      equipmentConfiguration = EquipmentConfigurationFactory.getInstance().createEquipmentConfiguration(rootConfElement);
+      long equipmentId = equipmentConfiguration.getId();
+      msgHandler = EquipmentMessageHandler.createEquipmentMessageHandler(
+              equipmentConfiguration.getHandlerClassName(),
+              new EquipmentCommandHandler(equipmentId, new RequestController(configurationController)),
+              new EquipmentConfigurationHandler(equipmentId, configurationController),
+              equipmentMessageSender, null);
 //            equipmentMessageSender.setEquipmentConfiguration(equipmentConfiguration);
 //            equipmentMessageSender.setEquipmentLoggerFactory(factory);
-            this.equipmentMessageSender.init(equipmentConfiguration, factory);
-            pconf.getEquipmentConfigurations().put(equipmentId, equipmentConfiguration);
-            msgHandler.setEquipmentMessageSender(equipmentMessageSender);
-            msgHandler.setEquipmentLoggerFactory(new EquipmentLoggerFactory(equipmentConfiguration.getHandlerClassName(), equipmentId,
-                    equipmentConfiguration.getName(), pconf.getProcessName(), false, false));
+      this.equipmentMessageSender.init(equipmentConfiguration);
+      pconf.getEquipmentConfigurations().put(equipmentId, equipmentConfiguration);
+      msgHandler.setEquipmentMessageSender(equipmentMessageSender);
 
 
-            // call some test-specific before test initializer
-            beforeTest();
+      // call some test-specific before test initializer
+      beforeTest();
 
-            Thread.sleep(120);
-        }
-
+      Thread.sleep(120);
     }
 
+  }
 
-    @After
-    public void cleanUp() throws Exception {
-        if (msgHandler != null)
-            msgHandler.disconnectFromDataSource();
-        afterTest();
+
+  @After
+  public void cleanUp() throws Exception {
+    if (msgHandler != null)
+      msgHandler.disconnectFromDataSource();
+    afterTest();
+  }
+
+
+  protected static final Element parseEquipmentConfiguration(final String confXML) {
+    Element result = null;
+    try {
+      DOMParser parser = new DOMParser();
+      parser.parse(confXML);
+      result = parser.getDocument().getDocumentElement();
     }
-
-
-    protected static final Element parseEquipmentConfiguration(final String confXML) {
-        Element result = null;
-        try {
-            DOMParser parser = new DOMParser();
-            parser.parse(confXML);
-            result = parser.getDocument().getDocumentElement();
-        } catch (Exception ex) {
-            log.error("could not parse configuration document", ex);
-        }
-        return result;
+    catch (Exception ex) {
+      log.error("could not parse configuration document", ex);
     }
+    return result;
+  }
 
 }
