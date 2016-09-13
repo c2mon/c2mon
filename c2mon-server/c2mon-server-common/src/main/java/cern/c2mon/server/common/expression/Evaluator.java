87aea953041738988f515a8468830ed033b4c073
Franz Ritter <franz.ritter@cern.ch>
2016-09-13 16:53:54 +0200

    Add an evaluator on the server in order to make an expression evaluation possible
    Issue: #49

---
+++ c2mon-server/c2mon-server-common/src/main/java/cern/c2mon/server/common/expression/Evaluator.java
@@ -0,0 +1,31 @@
+package cern.c2mon.server.common.expression;
+
+import cern.c2mon.server.common.tag.Tag;
+import cern.c2mon.shared.client.expression.Expression;
+import cern.c2mon.shared.rule.expression.Interpreter;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Franz Ritter
+ */
+@Service
+@Slf4j
+public class Evaluator {
+
+  public Tag evaluate(Tag updateTag) {
+    for (Expression expression : updateTag.getExpressions()) {
+      if (updateTag.getValue() != null) {
+        Boolean newEvaluation = Interpreter.evaluateExpression(expression.getExpression(), updateTag.getValue());
+        expression.setResult(newEvaluation);
+      } else {
+        log.warn("Tried to evaluate a expression with a null value: Tag id:{}, Tag name:{}",
+            updateTag.getId(), updateTag.getName());
+      }
+    }
+
+    return updateTag;
+  }
+
+}
