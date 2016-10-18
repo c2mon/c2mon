7bce10620fca7ee207b51fe486607cfc8e27a0ca
Franz Ritter <franz.ritter@cern.ch>
2016-10-18 10:53:09 +0200

    Change the mybatis batch loading for tags and alarms
    The previous batch loading is based on ids. This means that the batches from the database are loaded in between the range of two ids.
    The new batch loading uses the range of two row numbers to load a batch.
    
    Issue: #82

--- c2mon-server/c2mon-server-cachedbaccess/src/main/java/cern/c2mon/server/cache/dbaccess/BatchLoaderMapper.java
+++ c2mon-server/c2mon-server-cachedbaccess/src/main/java/cern/c2mon/server/cache/dbaccess/BatchLoaderMapper.java
@@ -1,16 +1,16 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
@@ -36,18 +36,4 @@ public interface BatchLoaderMapper<T extends Cacheable> extends SimpleLoaderMapp
    * @return the list of records
    */
   List<T> getRowBatch(DBBatch dbBatch);
-
-  /**
-   * Returns the maximum id of the cache objects to load.
-   *
-   * @return max id
-   */
-  Long getMaxId();
-
-  /**
-   * Returns the minimum id of the cache objects to load.
-   *
-   * @return min id
-   */
-  Long getMinId();
 }
