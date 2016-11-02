786c850ec94bbd162b5476292f2130514d415608
Szymon Halastra <szymon.edward.halastra@cern.ch>
2016-11-02 16:07:17 +0100

    #59: add missing import

--- c2mon-daq/c2mon-daq-core/src/main/java/cern/c2mon/daq/config/ProcessMessageSenderConfig.java
+++ c2mon-daq/c2mon-daq-core/src/main/java/cern/c2mon/daq/config/ProcessMessageSenderConfig.java
@@ -22,10 +22,7 @@ import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 import org.springframework.jms.core.JmsTemplate;
 
 import cern.c2mon.daq.common.conf.core.ConfigurationController;
