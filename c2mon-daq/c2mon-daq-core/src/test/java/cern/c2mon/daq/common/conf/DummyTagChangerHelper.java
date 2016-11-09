5106ff98c716a568dda578e1392c81d257ac64bb
Martin Flamm <martin.flamm@cern.ch>
2016-11-09 14:14:11 +0100

    #90: Added DummyTagChangerHelper for testes

---
+++ c2mon-daq/c2mon-daq-core/src/test/java/cern/c2mon/daq/common/conf/DummyTagChangerHelper.java
@@ -0,0 +1,10 @@
+package cern.c2mon.daq.common.conf;
+
+import cern.c2mon.daq.common.conf.equipment.TagChangerHelper;
+
+/**
+ * @author Martin Flamm
+ */
+public class DummyTagChangerHelper extends TagChangerHelper{
+
+}
