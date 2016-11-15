e66d52388b243b94ad65e856705c849502d9dae0
Martin Flamm <martin.flamm@cern.ch>
2016-11-15 14:37:11 +0100

    #90: Changed copyright header, using DefaultDataTagChanger now.
    I changed the copyright header to a newer version available. Instead of using an new dummy implementation of the TagChangerHelper I use the class DefaultDataTagChanger.

--- c2mon-daq/c2mon-daq-core/src/test/java/cern/c2mon/daq/common/conf/TagChangerHelperTest.java
+++ c2mon-daq/c2mon-daq-core/src/test/java/cern/c2mon/daq/common/conf/TagChangerHelperTest.java
@@ -1,17 +1,16 @@
-/**
- * /******************************************************************************
+/******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * <p>
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * <p>
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * <p>
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
@@ -20,7 +19,7 @@ package cern.c2mon.daq.common.conf;
 import org.junit.Before;
 import org.junit.Test;
 
-import cern.c2mon.daq.common.conf.equipment.TagChangerHelper;
+import cern.c2mon.daq.common.conf.core.DefaultDataTagChanger;
 import cern.c2mon.shared.common.ConfigurationException;
 import cern.c2mon.shared.common.datatag.address.HardwareAddress;
 
@@ -29,13 +28,13 @@ import static junit.framework.TestCase.assertTrue;
 
 public class TagChangerHelperTest {
 
-  TagChangerHelper dummyTagChangerHelper;
-  HardwareAddress hardwareAddress;
-  HardwareAddress oldHardwareAddress;
+  private DefaultDataTagChanger dataTagChanger;
+  private HardwareAddress hardwareAddress;
+  private HardwareAddress oldHardwareAddress;
 
   @Before
   public void setUp() {
-    dummyTagChangerHelper = new DummyTagChangerHelper();
+    dataTagChanger = new DefaultDataTagChanger();
     hardwareAddress = new HardwareAddress() {
       @Override
       public String toConfigXML() {
@@ -73,16 +72,16 @@ public class TagChangerHelperTest {
 
   @Test
   public void hasHardwareAddressChangedOldIsNull() {
-    assertTrue("Hardware address change expected.", dummyTagChangerHelper.hasHardwareAddressChanged(hardwareAddress, null));
+    assertTrue("Hardware address change expected.", dataTagChanger.hasHardwareAddressChanged(hardwareAddress, null));
   }
 
   @Test
   public void hasHardwareAddressChangedNewIsNull() {
-    assertTrue("Hardware address change expected.", dummyTagChangerHelper.hasHardwareAddressChanged(null, oldHardwareAddress));
+    assertTrue("Hardware address change expected.", dataTagChanger.hasHardwareAddressChanged(null, oldHardwareAddress));
   }
 
   @Test
   public void hasHardwareAddressChangedEqualAdresses() {
-    assertFalse("Hardware address has changed, but it shouln't.", dummyTagChangerHelper.hasHardwareAddressChanged(hardwareAddress, oldHardwareAddress));
+    assertFalse("Hardware address has changed, but it shouln't.", dataTagChanger.hasHardwareAddressChanged(hardwareAddress, oldHardwareAddress));
   }
 }
