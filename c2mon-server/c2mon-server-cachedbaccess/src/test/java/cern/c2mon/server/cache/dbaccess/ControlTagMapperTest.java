994a0361961f2eb7a18dae531683a5d5b327cedd
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-11-18 11:08:56 +0100

    Fix ControlTagMapperTest

--- c2mon-server/c2mon-server-cachedbaccess/src/test/java/cern/c2mon/server/cache/dbaccess/ControlTagMapperTest.java
+++ c2mon-server/c2mon-server-cachedbaccess/src/test/java/cern/c2mon/server/cache/dbaccess/ControlTagMapperTest.java
@@ -1,16 +1,16 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
@@ -25,7 +25,9 @@ import static org.junit.Assert.assertTrue;
 import java.sql.Timestamp;
 import java.util.List;
 
+import cern.c2mon.server.cache.dbaccess.junit.DatabasePopulationRule;
 import org.junit.After;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,9 +59,11 @@ public class ControlTagMapperTest {
    */
   @Autowired
   private ControlTagMapper controlTagMapper;
+
+  @Rule
   @Autowired
-  private DataTagMapper dataTagMapper;
-  
+  public DatabasePopulationRule databasePopulationRule;
+
 
   @After
   public void deleteTestTag() {
@@ -112,7 +116,7 @@ public class ControlTagMapperTest {
   public void testUpdateControlTag() {
     ControlTagCacheObject cacheObject = CacheObjectCreation.createTestControlTag();
     controlTagMapper.insertControlTag(cacheObject);
-    
+
     assertTrue(cacheObject.getDataType().equals("Float"));
     cacheObject.setValue(Float.valueOf(1999f));
     cacheObject.setCacheTimestamp(new Timestamp(System.currentTimeMillis()));
