b4ccafa4c219c545e1709b49016834a0785fe429
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-12-08 16:25:07 +0100

    #117: Use latest test annotations

--- c2mon-server/c2mon-server-lifecycle/src/test/java/cern/c2mon/server/ServerStartupIntegrationTest.java
+++ c2mon-server/c2mon-server-lifecycle/src/test/java/cern/c2mon/server/ServerStartupIntegrationTest.java
@@ -3,14 +3,14 @@ package cern.c2mon.server;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author Justin Lewis Salmon
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = ServerStartup.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServerStartup.class)
 @Ignore("This test sporadically hangs for some reason...")
 public class ServerStartupIntegrationTest {
 
