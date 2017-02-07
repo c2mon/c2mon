ffc7ed116a19d4207f1371fbd4d669e25822b364
Matthias Braeger <Matthias.Braeger@cern.ch>
2017-02-07 16:07:33 +0100

    Add missing @Element annotation required for c2mon-web-ui

--- c2mon-shared/c2mon-shared-common/src/main/java/cern/c2mon/shared/common/datatag/address/impl/JMXHardwareAddressImpl.java
+++ c2mon-shared/c2mon-shared-common/src/main/java/cern/c2mon/shared/common/datatag/address/impl/JMXHardwareAddressImpl.java
@@ -1,24 +1,25 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 package cern.c2mon.shared.common.datatag.address.impl;
 
+import org.simpleframework.xml.Element;
+
 import cern.c2mon.shared.common.ConfigurationException;
 import cern.c2mon.shared.common.datatag.address.JMXHardwareAddress;
-import org.simpleframework.xml.Element;
 
 /**
  * Implementation of the <code>JMXHardwareAddress</code> interface
@@ -33,6 +34,7 @@ public class JMXHardwareAddressImpl extends HardwareAddressImpl implements JMXHa
     @Element(name = "object-name")
     protected String objectName;
 
+    @Element
     protected String attribute;
 
     @Element(name = "call-method")
@@ -42,6 +44,7 @@ public class JMXHardwareAddressImpl extends HardwareAddressImpl implements JMXHa
      * for JMX attributes which are implement java.util.List, this attribute specifies an index of the element to look
      * for This parameter is not mandatory
      */
+    @Element
     protected Integer index;
 
     @Element(name = "composite-field")
