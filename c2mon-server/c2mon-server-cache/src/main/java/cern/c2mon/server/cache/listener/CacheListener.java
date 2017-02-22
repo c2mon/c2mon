32998079c39c5bcc49a45df02f7f7e10caa3a1cf
Matthias Braeger <Matthias.Braeger@cern.ch>
2017-02-22 20:30:08 +0100

    Log refactoring and rename of variable timCacheListener to cacheListener

--- c2mon-server/c2mon-server-cache/src/main/java/cern/c2mon/server/cache/listener/CacheListener.java
+++ c2mon-server/c2mon-server-cache/src/main/java/cern/c2mon/server/cache/listener/CacheListener.java
@@ -1,23 +1,22 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 package cern.c2mon.server.cache.listener;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.ApplicationObjectSupport;
 
 import cern.c2mon.server.cache.C2monCacheListener;
@@ -28,58 +27,54 @@ import cern.c2mon.shared.util.threadhandler.ThreadHandler;
 /**
  * Asynchronous cache listener that passes events received by the Ehcache listener
  * to the wrapped {@link C2monCacheListener} on a single thread.
- * 
+ *
  * <p>A new CacheListener is created each time a new (asynchronous) listener is registered, wrapping
  * the C2monCacheListener implemented by the module listener class.
- * 
+ *
  * <p>It instantiates threads for each cache notification method, and passes each
  * received object to the appropriate thread.
- * 
+ *
  * <p><b>This class is deprecated and the {@link MultiThreadedCacheListener} should
  * preferably be used instead (with #threads = 1 for a single-threaded listener)</b>
- * 
+ *
  * @author Mark Brightwell
  *
  */
+@Slf4j
 public final class CacheListener<T extends Cacheable> extends ApplicationObjectSupport implements C2monCacheListener<T>, Lifecycle {
 
-  /**
-   * Logger.
-   */
-  private static final Logger LOGGER = LoggerFactory.getLogger(CacheListener.class);  
-  
   /**
    * The threads dealing with updates.
    */
   private ThreadHandler notifyUpdateThreadHandler;
   private ThreadHandler statusConfirmationHandler;
-      
+
   /**
    * Indicates if the listener is enabled (if not, notifications are ignored and exception is thrown).
    */
   private volatile boolean running = false;
-  
+
   /**
    * Constructor returning a CacheListener wrapping the {@link C2monCacheListener}.
    * Instantiates and starts the required threads.
-   * 
-   * @param timCacheListener the wrapped listener object
+   *
+   * @param cacheListener the wrapped listener object
    */
-  public CacheListener(final C2monCacheListener<T> timCacheListener) {
-    super();    
-    
+  public CacheListener(final C2monCacheListener<T> cacheListener) {
+    super();
+
     try {
-      this.notifyUpdateThreadHandler = new ThreadHandler(timCacheListener, C2monCacheListener.class.getMethod("notifyElementUpdated", new Class< ? >[] {Cacheable.class}));
-      this.statusConfirmationHandler = new ThreadHandler(timCacheListener, C2monCacheListener.class.getMethod("confirmStatus", new Class< ? >[] {Cacheable.class}));
+      this.notifyUpdateThreadHandler = new ThreadHandler(cacheListener, C2monCacheListener.class.getMethod("notifyElementUpdated", new Class< ? >[] {Cacheable.class}));
+      this.statusConfirmationHandler = new ThreadHandler(cacheListener, C2monCacheListener.class.getMethod("confirmStatus", new Class< ? >[] {Cacheable.class}));
 
       this.notifyUpdateThreadHandler.setName("NotifyUpdater");
       this.statusConfirmationHandler.setName("StatusConfirm");
     } catch (SecurityException e) {
-      LOGGER.error("Security exception caught.", e);
+      log.error("Security exception caught.", e);
     } catch (NoSuchMethodException e) {
-      LOGGER.error("No such method found in C2monCacheListener.", e);      
+      log.error("No such method found in C2monCacheListener.", e);
     }
-        
+
   }
 
 
@@ -90,19 +85,19 @@ public final class CacheListener<T extends Cacheable> extends ApplicationObjectS
 
 
   @Override
-  public void notifyElementUpdated(final T cacheable) {  
+  public void notifyElementUpdated(final T cacheable) {
     notifyUpdateThreadHandler.put(new Object[] {cacheable});
   }
- 
+
 
   /**
    * For management purposes.
    * @return the size of the task queue for onUpdate calls
-   */  
+   */
   public int getTaskQueueSize() {
     return notifyUpdateThreadHandler.getTaskQueueSize();
   }
-  
+
   /**
    * For management purposes.
    * @return the size of the task queue for status confirmations
@@ -110,7 +105,7 @@ public final class CacheListener<T extends Cacheable> extends ApplicationObjectS
   public int getConfirmStatusQueueSize() {
     return statusConfirmationHandler.getTaskQueueSize();
   }
-  
+
   @Override
   public boolean isRunning() {
     return running;
@@ -125,7 +120,7 @@ public final class CacheListener<T extends Cacheable> extends ApplicationObjectS
       running = true;
       notifyUpdateThreadHandler.start();
       statusConfirmationHandler.start();
-    }    
+    }
   }
 
   /**
@@ -137,12 +132,12 @@ public final class CacheListener<T extends Cacheable> extends ApplicationObjectS
   public void stop() {
     if (running) {
       running = false;
-      if (LOGGER.isDebugEnabled()) {
-        LOGGER.debug("Shutting down CacheListener threads.");
+      if (log.isDebugEnabled()) {
+        log.debug("Shutting down CacheListener threads.");
       }
-      notifyUpdateThreadHandler.shutdown(); 
-      statusConfirmationHandler.shutdown();      
-    }    
+      notifyUpdateThreadHandler.shutdown();
+      statusConfirmationHandler.shutdown();
+    }
   }
 
 
