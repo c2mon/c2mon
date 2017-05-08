6d8d7ec8780afdd1a59dff4be0b1906c967711c5
Martin Flamm <martin.flamm@cern.ch>
2017-05-08 15:57:07 +0200

    Revert "#148: Fix spelling mistake"
    This reverts commit c6bedc4f1cb999941387b63a19cefda7336a0930.

--- c2mon-shared/c2mon-shared-client/src/main/java/cern/c2mon/shared/client/alarm/AlarmValueImpl.java
+++ c2mon-shared/c2mon-shared-client/src/main/java/cern/c2mon/shared/client/alarm/AlarmValueImpl.java
@@ -71,7 +71,7 @@ public final class AlarmValueImpl extends ClientRequestReport implements AlarmVa
   /** LASER alarm fault member */
   @NotNull
   @Element
-  private String faultMember;
+  private String faultMemeber;
 
   /** Free text for additional information about the alarm */
   @Element(required = false)
@@ -129,7 +129,7 @@ public final class AlarmValueImpl extends ClientRequestReport implements AlarmVa
                         final boolean isActive) {
     id = pId;
     faultCode = pFaultCode;
-    faultMember = pFaultMember;
+    faultMemeber = pFaultMember;
     faultFamily = pFaultFamily;
     info = pInfo;
     tagId = pTagId;
@@ -160,7 +160,7 @@ public final class AlarmValueImpl extends ClientRequestReport implements AlarmVa
                         final boolean isActive) {
     id = pId;
     faultCode = pFaultCode;
-    faultMember = pFaultMember;
+    faultMemeber = pFaultMember;
     faultFamily = pFaultFamily;
     info = pInfo;
     tagId = pTagId;
@@ -173,7 +173,7 @@ public final class AlarmValueImpl extends ClientRequestReport implements AlarmVa
   @Override
   @JsonIgnore
   public String getFaultMember() {
-    return this.faultMember;
+    return this.faultMemeber;
   }
 
   @Override
