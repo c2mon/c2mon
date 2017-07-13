621776b41ffaeb21711b78348c685d14a5d05b67
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-07-13 17:41:54 +0200

    Modify way how EntryProcessor executes - enum with commands added

---
+++ c2mon-server/c2mon-server-jcache-ref/src/main/java/cern/c2mon/server/jcacheref/prototype/alive/operations/AliveTimerOperation.java
@@ -0,0 +1,12 @@
+package cern.c2mon.server.jcacheref.prototype.alive.operations;
+
+/**
+ * @author Szymon Halastra
+ */
+
+public enum AliveTimerOperation {
+  START,
+  STOP,
+  UPDATE,
+  HAS_EXPIRED
+}
