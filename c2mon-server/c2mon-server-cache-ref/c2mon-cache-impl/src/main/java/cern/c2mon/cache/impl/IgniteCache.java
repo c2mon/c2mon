91ee0a013b6d27f2757c08e2fe7fec7c0e507b05
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-08-09 16:39:17 +0200

    Set up prototype cache created using wrapper

--- c2mon-server/c2mon-server-cache-ref/c2mon-cache-impl/src/main/java/cern/c2mon/cache/impl/IgniteCache.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache-impl/src/main/java/cern/c2mon/cache/impl/IgniteCache.java
@@ -2,6 +2,12 @@ package cern.c2mon.cache.impl;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.ignite.IgniteSpringBean;
+import org.apache.ignite.configuration.CacheConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import cern.c2mon.cache.api.C2monCache;
 
 /**
@@ -9,33 +15,49 @@ import cern.c2mon.cache.api.C2monCache;
  */
 public class IgniteCache extends C2monCache {
 
+  @Autowired
+  protected IgniteSpringBean C2monIgnite;
+
+  org.apache.ignite.IgniteCache cache;
+  CacheConfiguration cacheCfg;
+
+  public IgniteCache(CacheConfiguration cacheCfg) {
+    this.cacheCfg = cacheCfg;
+  }
+
+  @PostConstruct
+  public void init() {
+    cache = C2monIgnite.getOrCreateCache(cacheCfg);
+    C2monIgnite.addCacheConfiguration(cacheCfg);
+  }
+
   @Override
   protected Object get(Object key) {
-    return null;
+    return cache.get(key);
   }
 
   @Override
   protected boolean containsKey(Object key) {
-    return false;
+    return cache.containsKey(key);
   }
 
   @Override
   protected void put(Object key, Object value) {
-
+    cache.put(key, value);
   }
 
   @Override
   protected boolean remove(Object key) {
-    return false;
+    return cache.remove(key);
   }
 
   @Override
   protected String getName() {
-    return null;
+    return ""; //TODO: rethink if this method should be here
   }
 
   @Override
   protected List getKeys() {
-    return null;
+    return null; //TODO: rethink if this method is required
   }
 }
--- c2mon-server/c2mon-server-cache-ref/c2mon-cache-impl/src/main/java/cern/c2mon/cache/impl/IgniteFactory.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache-impl/src/main/java/cern/c2mon/cache/impl/IgniteFactory.java
@@ -1,16 +1,28 @@
 package cern.c2mon.cache.impl;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ignite.configuration.CacheConfiguration;
+
 import cern.c2mon.cache.api.AbstractFactory;
 import cern.c2mon.cache.api.C2monCache;
 
 /**
  * @author Szymon Halastra
  */
+
+@Slf4j
 public class IgniteFactory extends AbstractFactory {
 
   @Override
   public C2monCache createCache(String name, Class<?> keyType, Class<?> valueType) {
-    return null;
+    CacheConfiguration cacheConfiguration = new CacheConfiguration();
+
+    cacheConfiguration.setName(name);
+    cacheConfiguration.setIndexedTypes(keyType, valueType);
+
+    log.info("And here it is, working IgniteFactory");
+
+    return new IgniteCache(cacheConfiguration);
   }
 
   @Override
