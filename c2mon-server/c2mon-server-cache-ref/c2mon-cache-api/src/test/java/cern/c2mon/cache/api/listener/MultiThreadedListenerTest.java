e983f0abebde089897222d2534c7440d49b5461c
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-08-10 14:30:58 +0200

    Add test for listeners

--- c2mon-server/c2mon-server-cache-ref/c2mon-cache-api/src/test/java/cern/c2mon/cache/api/listener/MultiThreadedListenerTest.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache-api/src/test/java/cern/c2mon/cache/api/listener/MultiThreadedListenerTest.java
@@ -1,16 +1,16 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
@@ -30,17 +30,16 @@ import static org.easymock.EasyMock.*;
 /**
  * Unit test of the MultiThreadedCacheListener class
  * (so no Spring context).
- * 
- * @author Mark Brightwell
  *
+ * @author Mark Brightwell
  */
-public class MultiThreadedListenerTest {
+public class MultiThreadedListenerTest extends CacheListenerBaseTest {
 
   /**
    * Class to test.
    */
   private MultiThreadedCacheListener multiThreadedListener;
-  
+
   /**
    * Mock listener.
    */
@@ -51,29 +50,12 @@ public class MultiThreadedListenerTest {
     mockTimCacheListener = createMock(C2monCacheListener.class);
     multiThreadedListener = new MultiThreadedCacheListener(mockTimCacheListener, 2, 2);
   }
-  
+
   @Test
   public void testNotification() throws CloneNotSupportedException, InterruptedException {
-    final Cacheable mockCacheable = createMock(Cacheable.class);
-    CountDownLatch latch = new CountDownLatch(1);
-
-    // expect the C2monCacheListener to be notified of the update by the ThreadHandler in the
-    // asynchronous listener
-    mockTimCacheListener.notifyElementUpdated(mockCacheable);
-    expectLastCall().andAnswer(() -> {
-      latch.countDown();
-      return null;
-    });
-    
-    //replay the scenario, notifying of the update
-    replay(mockCacheable);        
-    replay(mockTimCacheListener);
-    multiThreadedListener.notifyElementUpdated(mockCacheable);
-    latch.await();
-    verify(mockTimCacheListener);
-    verify(mockCacheable);
+    testNotification(multiThreadedListener, mockTimCacheListener);
   }
-  
+
   @Test
   public void testTwoNotifications() throws CloneNotSupportedException, InterruptedException {
     final Cacheable mockCacheable = createMock(Cacheable.class);
@@ -90,9 +72,9 @@ public class MultiThreadedListenerTest {
       latch.countDown();
       return null;
     });
-    
+
     //replay the scenario, notifying of the update
-    replay(mockCacheable);  
+    replay(mockCacheable);
     replay(mockCacheable2);
     replay(mockTimCacheListener);
     multiThreadedListener.notifyElementUpdated(mockCacheable);
@@ -100,20 +82,21 @@ public class MultiThreadedListenerTest {
     latch.await();
     verify(mockTimCacheListener);
     verify(mockCacheable);
-    verify(mockCacheable2);   
+    verify(mockCacheable2);
   }
-  
+
   /**
    * Is now allowed but logs a warning
+   *
    * @throws InterruptedException
    */
   @Test //(expected=CacheException.class)
   public void testShutdown() throws InterruptedException {
     multiThreadedListener.stop();
-    final Cacheable mockCacheable = createMock(Cacheable.class);            
-    multiThreadedListener.notifyElementUpdated(mockCacheable);    
+    final Cacheable mockCacheable = createMock(Cacheable.class);
+    multiThreadedListener.notifyElementUpdated(mockCacheable);
   }
-  
+
   @After
   public void shutdown() {
     multiThreadedListener.stop();
