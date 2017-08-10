4069710003491eaa986db6ec8aed49daa1434b55
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-08-10 17:46:06 +0200

    Create C2monListenerService

--- c2mon-server/c2mon-server-cache-ref/c2mon-cache-api/src/main/java/cern/c2mon/cache/api/C2monCache.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache-api/src/main/java/cern/c2mon/cache/api/C2monCache.java
@@ -4,16 +4,23 @@ import java.util.List;
 
 import org.springframework.context.support.ApplicationObjectSupport;
 
+import cern.c2mon.cache.api.listener.C2monBufferedCacheListener;
+import cern.c2mon.cache.api.listener.C2monCacheListener;
+import cern.c2mon.server.common.component.Lifecycle;
+import cern.c2mon.shared.common.Cacheable;
+
 /**
  * @param <K> cache key type
  * @param <V> cache element type
  *
  * @author Szymon Halastra
  */
-public abstract class C2monCache<K, V> extends ApplicationObjectSupport {
+public abstract class C2monCache<K, V> extends ApplicationObjectSupport implements C2monListener {
+
+  C2monListener<Cacheable> listenerService;
 
   protected C2monCache() {
-
+    this.listenerService = new C2monListenerService();
   }
 
   protected abstract V get(K key);
@@ -27,4 +34,39 @@ public abstract class C2monCache<K, V> extends ApplicationObjectSupport {
   protected abstract String getName();
 
   protected abstract List<K> getKeys();
+
+  @Override
+  public void notifyListenersOfUpdate(Cacheable cacheable) {
+    this.listenerService.notifyListenersOfUpdate(cacheable);
+  }
+
+  @Override
+  public void notifyListenerStatusConfirmation(Cacheable cacheable, long timestamp) {
+    this.listenerService.notifyListenerStatusConfirmation(cacheable, timestamp);
+  }
+
+  @Override
+  public void registerSynchronousListener(C2monCacheListener cacheListener) {
+    this.listenerService.registerSynchronousListener(cacheListener);
+  }
+
+  @Override
+  public Lifecycle registerListener(C2monCacheListener cacheListener) {
+    return this.listenerService.registerListener(cacheListener);
+  }
+
+  @Override
+  public Lifecycle registerThreadedListener(C2monCacheListener cacheListener, int queueCapacity, int threadPoolSize) {
+    return this.listenerService.registerThreadedListener(cacheListener, queueCapacity, threadPoolSize);
+  }
+
+  @Override
+  public Lifecycle registerBufferedListener(C2monBufferedCacheListener c2monBufferedCacheListener, int frequency) {
+    return this.listenerService.registerBufferedListener(c2monBufferedCacheListener, frequency);
+  }
+
+  @Override
+  public Lifecycle registerKeyBufferedListener(C2monBufferedCacheListener bufferedCacheListener, int frequency) {
+    return this.listenerService.registerKeyBufferedListener(bufferedCacheListener, frequency);
+  }
 }
---
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache-api/src/main/java/cern/c2mon/cache/api/C2monListenerService.java
@@ -0,0 +1,113 @@
+package cern.c2mon.cache.api;
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+import lombok.extern.slf4j.Slf4j;
+
+import cern.c2mon.cache.api.listener.C2monBufferedCacheListener;
+import cern.c2mon.cache.api.listener.C2monCacheListener;
+import cern.c2mon.cache.api.listener.impl.BufferedKeyCacheListener;
+import cern.c2mon.cache.api.listener.impl.CacheListener;
+import cern.c2mon.cache.api.listener.impl.DefaultBufferedCacheListener;
+import cern.c2mon.cache.api.listener.impl.MultiThreadedCacheListener;
+import cern.c2mon.server.common.component.Lifecycle;
+import cern.c2mon.shared.common.Cacheable;
+
+/**
+ * @author Szymon Halastra
+ */
+@Slf4j
+public class C2monListenerService<K, V extends Cacheable> implements C2monListener<V> {
+
+  /**
+   * Reference to the Cache event listeners
+   */
+  private LinkedBlockingDeque<C2monCacheListener<? super V>> cacheListeners = new LinkedBlockingDeque<>();
+
+  public C2monListenerService() {
+  }
+
+
+  /**
+   * In init method there should be a line which more or less looks like this:
+   * <p>
+   * registeredEventListeners = cache.getCacheEventNotificationService();
+   * <p>
+   * Above line is strongly connected with Ehcache, so in our case it should be
+   * abstracted and explicit call to cache should be in IMPL module
+   */
+
+  /**
+   * Creates a new cache element and calls the Ehcache notifyElementUpdated
+   * method.
+   * <p>
+   * Notifies the listeners that an update occurred for this DataTag. Should
+   * be called *within a lock on the cache object* so the object is not modified
+   * before being passed to the listeners (using a clone).
+   *
+   * @param cacheable the cache object that has been updated
+   */
+  @Override
+  public void notifyListenersOfUpdate(final V cacheable) {
+//    registeredEventListeners.notifyElementUpdated(new Element(cacheable.getId(), null), false); //only for monitoring via Ehcache: not using Ehcache listeners o.w.
+    try {
+      @SuppressWarnings("unchecked")
+      V cloned = (V) cacheable.clone();
+      for (C2monCacheListener<? super V> listener : cacheListeners) {
+        listener.notifyElementUpdated(cloned);
+      }
+    }
+    catch (CloneNotSupportedException e) {
+      log.error("CloneNotSupportedException caught while cloning a cache element - this should never happen!", e);
+      throw new RuntimeException("CloneNotSupportedException caught while cloning a cache element - this should never happen!", e);
+    }
+  }
+
+  @Override
+  public void notifyListenerStatusConfirmation(final V cacheable, final long timestamp) {
+    try {
+      @SuppressWarnings("unchecked")
+      V cloned = (V) cacheable.clone();
+      for (C2monCacheListener<? super V> listener : cacheListeners) {
+        listener.confirmStatus(cloned);
+      }
+    }
+    catch (CloneNotSupportedException e) {
+      log.error("CloneNotSupportedException caught while cloning a cache element - this should never happen!", e);
+      throw new RuntimeException("CloneNotSupportedException caught while cloning a cache element - this should never happen!", e);
+    }
+  }
+
+  @Override
+  public void registerSynchronousListener(C2monCacheListener<? super V> cacheListener) {
+    cacheListeners.add(cacheListener);
+  }
+
+  @Override
+  public Lifecycle registerListener(C2monCacheListener<? super V> cacheListener) {
+    CacheListener<? super V> wrappedCacheListener = new CacheListener<>(cacheListener);
+    cacheListeners.add(wrappedCacheListener);
+    return wrappedCacheListener;
+  }
+
+  @Override
+  public Lifecycle registerThreadedListener(C2monCacheListener<? super V> cacheListener, int queueCapacity, int threadPoolSize) {
+    MultiThreadedCacheListener<? super V> threadedCacheListener = new MultiThreadedCacheListener<>(cacheListener, queueCapacity, threadPoolSize);
+    cacheListeners.add(threadedCacheListener);
+    return threadedCacheListener;
+  }
+
+  @Override
+  public Lifecycle registerBufferedListener(final C2monBufferedCacheListener<Cacheable> c2monBufferedCacheListener, int frequency) {
+    DefaultBufferedCacheListener<Cacheable> bufferedCacheListener = new DefaultBufferedCacheListener<>(c2monBufferedCacheListener, frequency);
+    cacheListeners.add(bufferedCacheListener);
+    return bufferedCacheListener;
+  }
+
+  @Override
+  public Lifecycle registerKeyBufferedListener(final C2monBufferedCacheListener<Long> bufferedCacheListener, int frequency) {
+    BufferedKeyCacheListener<V> bufferedKeyCacheListener = new BufferedKeyCacheListener<V>(bufferedCacheListener, frequency);
+    cacheListeners.add(bufferedKeyCacheListener);
+    return bufferedKeyCacheListener;
+  }
+}
