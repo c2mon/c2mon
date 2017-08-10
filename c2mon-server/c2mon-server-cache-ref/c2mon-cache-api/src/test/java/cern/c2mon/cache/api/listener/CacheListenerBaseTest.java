a4fa7c135a55930436e3a2a2b58dcb1a6aec6729
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-08-10 14:30:58 +0200

    Add test for listeners

---
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache-api/src/test/java/cern/c2mon/cache/api/listener/CacheListenerBaseTest.java
@@ -0,0 +1,35 @@
+package cern.c2mon.cache.api.listener;
+
+import java.util.concurrent.CountDownLatch;
+
+import cern.c2mon.shared.common.Cacheable;
+
+import static org.easymock.EasyMock.*;
+
+/**
+ * @author Szymon Halastra
+ */
+public class CacheListenerBaseTest {
+
+  protected void testNotification(C2monCacheListener c2monCacheListener, C2monCacheListener mockTimCacheListener) throws InterruptedException {
+    final Cacheable mockCacheable = createMock(Cacheable.class);
+
+    // expect the C2monCacheListener to be notified of the update by the ThreadHandler in the
+    // asynchronous listener
+    CountDownLatch latch = new CountDownLatch(1);
+
+    mockTimCacheListener.notifyElementUpdated(mockCacheable);
+    expectLastCall().andAnswer(() -> {
+      latch.countDown();
+      return null;
+    });
+
+    //replay the scenario, notifying of the update
+    replay(mockCacheable);
+    replay(mockTimCacheListener);
+    c2monCacheListener.notifyElementUpdated(mockCacheable);
+    latch.await();
+    verify(mockTimCacheListener);
+    verify(mockCacheable);
+  }
+}
--- c2mon-server/c2mon-server-cache-ref/c2mon-cache-api/src/test/java/cern/c2mon/cache/api/listener/CacheListenerTest.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache-api/src/test/java/cern/c2mon/cache/api/listener/CacheListenerTest.java
@@ -1,48 +1,45 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 package cern.c2mon.cache.api.listener;
 
-import java.util.concurrent.CountDownLatch;
-
 import org.junit.Before;
 import org.junit.Test;
 
 import cern.c2mon.cache.api.listener.impl.CacheListener;
-import cern.c2mon.shared.common.Cacheable;
 
-import static org.easymock.EasyMock.*;
+import static org.easymock.EasyMock.createMock;
 
 /**
  * Unit test testing the CacheListener class functionality (asynchronous listener).
- * @author mbrightw
  *
+ * @author mbrightw
  */
-public class CacheListenerTest {
+public class CacheListenerTest extends CacheListenerBaseTest {
 
   /**
    * Class to test.
    */
   private CacheListener cacheListener;
-  
+
   /**
    * Mocked listener waiting to be notified of updates.
    */
   private C2monCacheListener mockTimCacheListener;
-  
+
   /**
    * Before each test method.
    */
@@ -52,33 +49,16 @@ public class CacheListenerTest {
     cacheListener = new CacheListener(mockTimCacheListener);
     cacheListener.start();
   }
-  
+
   /**
    * Tests the mock listener is notified of an update.
+   *
    * @throws CloneNotSupportedException should not normally be thrown
-   * @throws InterruptedException if exception while waiting (need to wait for second thread to process update)
+   * @throws InterruptedException       if exception while waiting (need to wait for second thread to process update)
    */
   @Test
   public void testNotifyElementUpdated() throws CloneNotSupportedException, InterruptedException {
-    final Cacheable mockCacheable = createMock(Cacheable.class);
-
-    // expect the C2monCacheListener to be notified of the update by the ThreadHandler in the
-    // asynchronous listener
-    CountDownLatch latch = new CountDownLatch(1);
-
-    mockTimCacheListener.notifyElementUpdated(mockCacheable);
-    expectLastCall().andAnswer(() -> {
-      latch.countDown();
-      return null;
-    });
-    
-    //replay the scenario, notifying of the update
-    replay(mockCacheable);        
-    replay(mockTimCacheListener);
-    cacheListener.notifyElementUpdated(mockCacheable);
-    latch.await();
-    verify(mockTimCacheListener);
-    verify(mockCacheable);
+    testNotification(cacheListener, mockTimCacheListener);
   }
-  
+
 }
