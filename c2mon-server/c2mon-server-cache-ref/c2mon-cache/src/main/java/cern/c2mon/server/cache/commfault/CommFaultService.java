8d23824be22b88e2bbd9b434668b4a8468b5e2be
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-08-17 14:10:34 +0200

    Add Commfault and Device Service

---
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/commfault/CommFaultService.java
@@ -0,0 +1,31 @@
+package cern.c2mon.server.cache.commfault;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cern.c2mon.cache.api.C2monCache;
+import cern.c2mon.server.common.commfault.CommFaultTag;
+import cern.c2mon.server.common.commfault.CommFaultTagCacheObject;
+import cern.c2mon.server.common.equipment.AbstractEquipment;
+
+/**
+ * @author Szymon Halastra
+ */
+@Slf4j
+@Service
+public class CommFaultService {
+
+  private final C2monCache<Long, CommFaultTag> commFaultTagCache;
+
+  @Autowired
+  public CommFaultService(final C2monCache<Long, CommFaultTag> commFaultTagCache) {
+    this.commFaultTagCache = commFaultTagCache;
+  }
+
+  public void generateFromEquipment(AbstractEquipment abstractEquipment) {
+    CommFaultTag commFaultTag = new CommFaultTagCacheObject(abstractEquipment.getCommFaultTagId(), abstractEquipment.getId(),
+            abstractEquipment.getName(), abstractEquipment.getAliveTagId(), abstractEquipment.getStateTagId());
+    commFaultTagCache.put(commFaultTag.getId(), commFaultTag);
+  }
+}
