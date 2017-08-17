e466c2a24097841042c1cbacd0943f06cd3d2142
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-08-17 14:10:34 +0200

    Add Commfault and Device Service

---
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/device/DeviceService.java
@@ -0,0 +1,24 @@
+package cern.c2mon.server.cache.device;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cern.c2mon.cache.api.C2monCache;
+import cern.c2mon.server.common.device.Device;
+
+/**
+ * @author Szymon Halastra
+ */
+
+@Slf4j
+@Service
+public class DeviceService {
+
+  private final C2monCache<Long, Device> deviceCache;
+
+  @Autowired
+  public DeviceService(final C2monCache<Long, Device> deviceCache) {
+    this.deviceCache = deviceCache
+  }
+}
