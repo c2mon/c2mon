b812acc97556ec66b04e1823c2e2ccb3e2bef9a9
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-08-24 11:44:36 +0200

    Create tests for SupervisedService

--- c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/test/java/cern/c2mon/cache/SupervisedServiceTest.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/test/java/cern/c2mon/cache/SupervisedServiceTest.java
@@ -10,18 +10,19 @@ import cern.c2mon.cache.api.C2monCache;
 import cern.c2mon.cache.api.service.SupervisedService;
 import cern.c2mon.server.cache.SupervisedServiceImpl;
 import cern.c2mon.server.cache.alivetimer.AliveTimerService;
-import cern.c2mon.server.common.equipment.Equipment;
 import cern.c2mon.server.common.equipment.EquipmentCacheObject;
+import cern.c2mon.server.common.process.ProcessCacheObject;
 import cern.c2mon.server.common.supervision.Supervised;
 import cern.c2mon.shared.client.supervision.SupervisionEvent;
 import cern.c2mon.shared.common.supervision.SupervisionConstants;
 
-import static org.easymock.EasyMock.eq;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.junit.Assert.*;
 
 /**
+ * For test purposes I will be using EquipmentCacheObject implementation or ProcessCacheObject
+ *
  * @author Szymon Halastra
  */
 public class SupervisedServiceTest {
@@ -51,9 +52,7 @@ public class SupervisedServiceTest {
     equipment.setStatusDescription(statusDescription);
     equipment.setSupervisionStatus(SupervisionConstants.SupervisionStatus.RUNNING);
 
-    expect(cache.get(equipment.getId())).andReturn(equipment);
-
-    replay(cache);
+    prepareMock(equipment);
 
     SupervisionEvent supervisionEvent = supervisedService.getSupervisionStatus(equipment.getId());
 
@@ -74,9 +73,7 @@ public class SupervisedServiceTest {
     equipment.setStatusDescription(null);
     equipment.setSupervisionStatus(SupervisionConstants.SupervisionStatus.RUNNING);
 
-    expect(cache.get(equipment.getId())).andReturn(equipment);
-
-    replay(cache);
+    prepareMock(equipment);
 
     SupervisionEvent supervisionEvent = supervisedService.getSupervisionStatus(equipment.getId());
 
@@ -93,9 +90,7 @@ public class SupervisedServiceTest {
 
     Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
-    expect(cache.get(equipment.getId())).andReturn(equipment);
-
-    replay(cache);
+    prepareMock(equipment);
 
     supervisedService.start(equipment.getId(), timestamp);
 
@@ -110,9 +105,7 @@ public class SupervisedServiceTest {
 
     Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
-    expect(cache.get(equipment.getId())).andReturn(equipment);
-
-    replay(cache);
+    prepareMock(equipment);
 
     supervisedService.stop(equipment.getId(), timestamp);
 
@@ -121,12 +114,114 @@ public class SupervisedServiceTest {
     assertTrue("Supervised should have information about stop in status description", equipment.getStatusDescription().contains("was stopped"));
   }
 
-  
+  @Test
+  public void resumeSupervisedWhenRunning() {
+    EquipmentCacheObject equipment = new EquipmentCacheObject(1L);
 
-  /** TODO: write tests for:
-   * 3. resume
-   * 4. suspend
-   * 5. isRunning
-   * 6. isUncertain
-   */
+    equipment.setSupervisionStatus(SupervisionConstants.SupervisionStatus.RUNNING);
+
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    String message = "test-message";
+
+    prepareMock(equipment);
+
+    supervisedService.resume(equipment.getId(), timestamp, message);
+
+    assertTrue("Supervised should not have status time set", equipment.getStatusTime() == null);
+    assertTrue("Supervised should not have status description set", equipment.getStatusDescription() == null);
+    assertTrue("Supervised should have RUNNING status set", equipment.getSupervisionStatus() == SupervisionConstants.SupervisionStatus.RUNNING);
+  }
+
+  @Test
+  public void resumeSupervisedWhenNotRunningAndNotProcess() {
+    EquipmentCacheObject equipment = new EquipmentCacheObject(1L);
+
+    equipment.setSupervisionStatus(SupervisionConstants.SupervisionStatus.DOWN);
+
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    String message = "test-message";
+
+    prepareMock(equipment);
+
+    supervisedService.resume(equipment.getId(), timestamp, message);
+
+    assertEquals("Supervised should have status time set", equipment.getStatusTime(), timestamp);
+    assertEquals("Supervised should have status description set", equipment.getStatusDescription(), message);
+    assertTrue("Supervised should have RUNNING status set", equipment.getSupervisionStatus() == SupervisionConstants.SupervisionStatus.RUNNING);
+  }
+
+  @Test
+  public void resumeSupervisedWhenNotRunningAndProcess() {
+    ProcessCacheObject process = new ProcessCacheObject(1L);
+
+    process.setSupervisionStatus(SupervisionConstants.SupervisionStatus.DOWN);
+    process.setLocalConfig(ProcessCacheObject.LocalConfig.Y);
+
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    String message = "test-message";
+
+    prepareMock(process);
+
+    supervisedService.resume(process.getId(), timestamp, message);
+
+    assertEquals("Supervised should not have status time set", process.getStatusTime(), timestamp);
+    assertEquals("Supervised should not have status description set", process.getStatusDescription(), message);
+    assertTrue("Supervised should have RUNNING_LOCAL status set", process.getSupervisionStatus() == SupervisionConstants.SupervisionStatus.RUNNING_LOCAL);
+  }
+
+  @Test
+  public void suspendWhenIsRunning() {
+    EquipmentCacheObject equipment = new EquipmentCacheObject(1L);
+    equipment.setSupervisionStatus(SupervisionConstants.SupervisionStatus.RUNNING);
+
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    String message = "test-message";
+
+    prepareMock(equipment);
+
+    supervisedService.suspend(equipment.getId(), timestamp, message);
+
+    assertEquals("Supervised should have DOWN status", equipment.getSupervisionStatus(), SupervisionConstants.SupervisionStatus.DOWN);
+    assertEquals("Supervised should have status time set", equipment.getStatusTime(), timestamp);
+    assertEquals("Supervised should have status description set", equipment.getStatusDescription(), message);
+  }
+
+  @Test
+  public void suspendWhenIsUncertain() {
+    EquipmentCacheObject equipment = new EquipmentCacheObject(1L);
+    equipment.setSupervisionStatus(SupervisionConstants.SupervisionStatus.UNCERTAIN);
+
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    String message = "test-message";
+
+    prepareMock(equipment);
+
+    supervisedService.suspend(equipment.getId(), timestamp, message);
+
+    assertEquals("Supervised should have DOWN status", equipment.getSupervisionStatus(), SupervisionConstants.SupervisionStatus.DOWN);
+    assertEquals("Supervised should have status time set", equipment.getStatusTime(), timestamp);
+    assertEquals("Supervised should have status description set", equipment.getStatusDescription(), message);
+  }
+
+  @Test
+  public void suspendWhenIsNotRunning() {
+    EquipmentCacheObject equipment = new EquipmentCacheObject(1L);
+
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    String message = "test-message";
+
+    prepareMock(equipment);
+
+    supervisedService.suspend(equipment.getId(), timestamp, message);
+
+    assertEquals("Supervised should have DOWN status", equipment.getSupervisionStatus(), SupervisionConstants.SupervisionStatus.DOWN);
+    assertTrue("Supervised should not have status time set", equipment.getStatusTime() == null);
+    assertTrue("Supervised should not have status description set", equipment.getStatusDescription() == null);
+  }
+
+  private void prepareMock(Supervised equipment) {
+    expect(cache.get(equipment.getId())).andReturn(equipment);
+
+    replay(cache);
+  }
 }
