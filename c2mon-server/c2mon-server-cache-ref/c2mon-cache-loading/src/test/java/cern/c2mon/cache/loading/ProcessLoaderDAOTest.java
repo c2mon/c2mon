51da9a9d0b580a8ec7e23bb2d2f63fc9e2bf092b
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-09-14 16:59:29 +0200

    Move configuration and tests for loading

---
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache-loading/src/test/java/cern/c2mon/cache/loading/ProcessLoaderDAOTest.java
@@ -0,0 +1,76 @@
+/******************************************************************************
+ * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
+ *
+ * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
+ * C2MON is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the license.
+ *
+ * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+package cern.c2mon.cache.loading;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cern.c2mon.server.cache.dbaccess.config.CacheDbAccessModule;
+import cern.c2mon.server.cache.loading.ProcessDAO;
+import cern.c2mon.server.cache.loading.config.CacheLoadingModule;
+import cern.c2mon.server.common.config.CommonModule;
+import cern.c2mon.server.common.process.Process;
+import cern.c2mon.server.test.DatabasePopulationRule;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {
+    CommonModule.class,
+    CacheDbAccessModule.class,
+    CacheLoadingModule.class,
+    DatabasePopulationRule.class
+})
+public class ProcessLoaderDAOTest {
+
+  @Rule
+  @Autowired
+  public DatabasePopulationRule databasePopulationRule;
+
+  @Autowired
+  private ProcessDAO processDAO;
+
+  @Test
+  public void testGetItem() {
+    assertNotNull(processDAO.getItem(50L));
+  }
+
+  /**
+   * Check the default property is picked up (should override that set in the
+   * cache object itself).
+   */
+  @Test
+  public void testGetItemDoPostDbLoading() {
+    Process process = processDAO.getItem(51L);
+    assertNotNull(process);
+  }
+
+  @Test
+  public void testGetAll() {
+    assertNotNull(processDAO.getAllAsMap());
+    assertEquals(2, processDAO.getAllAsMap().size());
+  }
+
+  @Test
+  public void testGetNumTags() {
+    assertTrue(processDAO.getNumTags(50L).equals(6));
+  }
+}
