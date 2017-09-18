3b23ffaaba77e4d2b3053f042da913e3944b46fb
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-09-18 14:29:20 +0200

    Modify Cache Loader structure

--- c2mon-server/c2mon-server-cache-ref/c2mon-cache-api/src/main/java/cern/c2mon/cache/api/loader/C2monCacheLoader.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache-api/src/main/java/cern/c2mon/cache/api/loader/C2monCacheLoader.java
@@ -1,11 +1,18 @@
 package cern.c2mon.cache.api.loader;
 
-import cern.c2mon.cache.api.C2monCache;
-
 /**
+ * Interface that must be implemented by all C2MON
+ * cache loading mechanisms.
+ *
  * @author Szymon Halastra
  */
 public interface C2monCacheLoader {
 
-  void loadCache(C2monCache cache);
+  /**
+   * At server start-up, loads the cache from the DB into memory.
+   * In distributed set-up, this is not performed once the
+   * cache has already been loaded once (only performed if the disk
+   * store is cleaned).
+   */
+  void preload();
 }
