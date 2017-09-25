175ebe46c2ffc25734c5aaf87a818c66ef76cfed
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-09-25 08:39:07 +0200

    #39: Create cache loading test for AliveTimer

---
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/alivetimer/AliveTimerCacheConfig.java
@@ -0,0 +1,42 @@
+package cern.c2mon.server.cache.alivetimer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import cern.c2mon.cache.api.C2monCache;
+import cern.c2mon.cache.api.factory.AbstractC2monCacheFactory;
+import cern.c2mon.cache.api.loader.C2monCacheLoader;
+import cern.c2mon.server.cache.C2monCacheName;
+import cern.c2mon.server.cache.loader.AliveTimerDAO;
+import cern.c2mon.server.cache.loader.common.BatchCacheLoader;
+import cern.c2mon.server.cache.loader.config.CacheLoaderProperties;
+import cern.c2mon.server.common.alive.AliveTimer;
+import cern.c2mon.shared.client.configuration.api.tag.CommFaultTag;
+
+/**
+ * @author Szymon Halastra
+ */
+
+@Configuration
+public class AliveTimerCacheConfig {
+
+  @Autowired
+  private ThreadPoolTaskExecutor cacheLoaderTaskExecutor;
+
+  @Autowired
+  private CacheLoaderProperties properties;
+
+  @Bean(name = C2monCacheName.Names.ALIVETIMER)
+  public C2monCache createCache(AbstractC2monCacheFactory cachingFactory, AliveTimerDAO aliveTimerDAORef) {
+    C2monCache cache = cachingFactory.createCache(C2monCacheName.ALIVETIMER.getLabel(), Long.class, CommFaultTag.class);
+
+    C2monCacheLoader cacheLoader = new BatchCacheLoader<Long, AliveTimer>(cacheLoaderTaskExecutor, cache, aliveTimerDAORef,
+            properties.getBatchSize(), "AliveTimerLoader-");
+
+    cache.setCacheLoader(cacheLoader);
+
+    return cache;
+  }
+}
--- c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/test/java/cern/c2mon/cache/alivetimer/AliveTimerCacheLoaderTest.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/test/java/cern/c2mon/cache/alivetimer/AliveTimerCacheLoaderTest.java
@@ -1,7 +1,51 @@
 package cern.c2mon.cache.alivetimer;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cern.c2mon.cache.AbstractCacheLoaderTest;
+import cern.c2mon.cache.api.C2monCache;
+import cern.c2mon.server.cache.dbaccess.AliveTimerMapper;
+import cern.c2mon.server.common.alive.AliveTimer;
+import cern.c2mon.server.common.alive.AliveTimerCacheObject;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 /**
  * @author Szymon Halastra
  */
-public class AliveTimerCacheLoaderTest {
+public class AliveTimerCacheLoaderTest extends AbstractCacheLoaderTest {
+
+  @Autowired
+  private C2monCache<Long, AliveTimer> aliveTimerCacheRef;
+
+  @Autowired
+  private AliveTimerMapper aliveTimerMapper;
+
+  @Before
+  public void prepare() {
+    aliveTimerCacheRef.init();
+  }
+
+  @Test
+  public void preloadCache() {
+    assertNotNull("AliveTimer cache should be not null", aliveTimerCacheRef);
+
+    List<AliveTimer> aliveList = aliveTimerMapper.getAll(); //IN FACT: GIVES TIME FOR CACHE TO FINISH LOADING ASYNCH BEFORE COMPARISON BELOW...
+
+    //test the cache is the same size as in DB
+    assertEquals("Size of cache and DB mapping should be equal", aliveList.size(), aliveTimerCacheRef.getKeys().size());
+    //compare all the objects from the cache and buffer
+    Iterator<AliveTimer> it = aliveList.iterator();
+    while (it.hasNext()) {
+      AliveTimerCacheObject currentTimer = (AliveTimerCacheObject) it.next();
+      //only compares one field so far
+      assertEquals("Cached AliveTimer should have the same name as AliveTimer in DB", currentTimer.getRelatedName(), ((aliveTimerCacheRef.get(currentTimer.getId())).getRelatedName()));
+    }
+  }
 }
