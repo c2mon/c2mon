72250242cddf68a2f61e146bb1be92fca58bd81a
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-09-25 11:35:10 +0200

    #39: Add integration test for preloading RuleTag Cache, configure cache loader

--- c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/rule/RuleTagCacheConfig.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/rule/RuleTagCacheConfig.java
@@ -1,11 +1,17 @@
 package cern.c2mon.server.cache.rule;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import cern.c2mon.cache.api.C2monCache;
 import cern.c2mon.cache.api.factory.AbstractC2monCacheFactory;
+import cern.c2mon.cache.api.loader.C2monCacheLoader;
 import cern.c2mon.server.cache.C2monCacheName;
+import cern.c2mon.server.cache.loader.RuleTagLoaderDAO;
+import cern.c2mon.server.cache.loader.common.BatchCacheLoader;
+import cern.c2mon.server.cache.loader.config.CacheLoaderProperties;
 import cern.c2mon.server.common.rule.RuleTag;
 
 /**
@@ -14,8 +20,20 @@ import cern.c2mon.server.common.rule.RuleTag;
 @Configuration
 public class RuleTagCacheConfig {
 
+  @Autowired
+  private ThreadPoolTaskExecutor cacheLoaderTaskExecutor;
+
+  @Autowired
+  private CacheLoaderProperties properties;
+
   @Bean(name = C2monCacheName.Names.RULE)
-  public C2monCache createCache(AbstractC2monCacheFactory cachingFactory) {
-    return cachingFactory.createCache(C2monCacheName.RULETAG.getLabel(), Long.class, RuleTag.class);
+  public C2monCache createCache(AbstractC2monCacheFactory cachingFactory, RuleTagLoaderDAO ruleTagLoaderDAO) {
+    C2monCache cache = cachingFactory.createCache(C2monCacheName.RULETAG.getLabel(), Long.class, RuleTag.class);
+
+    C2monCacheLoader cacheLoader = new BatchCacheLoader<Long, RuleTag>(cacheLoaderTaskExecutor, cache, ruleTagLoaderDAO,
+            properties.getBatchSize(), "RuleTagLoader-");
+    cache.setCacheLoader(cacheLoader);
+
+    return cache;
   }
 }
--- c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/test/java/cern/c2mon/cache/rule/RuleCacheLoaderTest.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/test/java/cern/c2mon/cache/rule/RuleCacheLoaderTest.java
@@ -1,7 +1,52 @@
 package cern.c2mon.cache.rule;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cern.c2mon.cache.AbstractCacheLoaderTest;
+import cern.c2mon.cache.api.C2monCache;
+import cern.c2mon.server.cache.dbaccess.RuleTagMapper;
+import cern.c2mon.server.common.rule.RuleTag;
+import cern.c2mon.server.common.rule.RuleTagCacheObject;
+
+import static org.junit.Assert.*;
+
 /**
  * @author Szymon Halastra
  */
-public class RuleCacheLoaderTest {
+public class RuleCacheLoaderTest extends AbstractCacheLoaderTest {
+
+  @Autowired
+  private C2monCache<Long, RuleTag> ruleTagCacheRef;
+
+  @Autowired
+  private RuleTagMapper ruleTagMapper;
+
+  @Before
+  public void init() {
+    ruleTagCacheRef.init();
+  }
+
+  @Test
+  public void preloadCache() {
+    assertNotNull("RuleTag Cache should not be null", ruleTagCacheRef);
+
+    List<RuleTag> ruleList = ruleTagMapper.getAll();
+
+    assertTrue("List of rule tags should not be empty", ruleList.size() > 0);
+
+    assertEquals("Size of cache and DB mapping should be equal", ruleList.size(), ruleTagCacheRef.getKeys().size());
+    //compare all the objects from the cache and buffer
+    Iterator<RuleTag> it = ruleList.iterator();
+    while (it.hasNext()) {
+      RuleTagCacheObject currentRule = (RuleTagCacheObject) it.next();
+      //only compares one field so far (name, which does not change when server is running!)
+      assertEquals("Cached RuleTag should have the same name as in DB",
+              currentRule.getName(), ((ruleTagCacheRef.get(currentRule.getId())).getName()));
+    }
+  }
 }
