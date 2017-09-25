2cf404fbf35d66686c4014fa0b98c1987e38f261
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-09-25 10:57:57 +0200

    #39: Add integration test for preloading DataTag Cache, configure cache loader

--- c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/datatag/DataTagCacheConfig.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/datatag/DataTagCacheConfig.java
@@ -1,11 +1,17 @@
 package cern.c2mon.server.cache.datatag;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import cern.c2mon.cache.api.C2monCache;
 import cern.c2mon.cache.api.factory.AbstractC2monCacheFactory;
+import cern.c2mon.cache.api.loader.C2monCacheLoader;
 import cern.c2mon.server.cache.C2monCacheName;
+import cern.c2mon.server.cache.loader.DataTagLoaderDAO;
+import cern.c2mon.server.cache.loader.common.BatchCacheLoader;
+import cern.c2mon.server.cache.loader.config.CacheLoaderProperties;
 import cern.c2mon.server.common.datatag.DataTag;
 
 /**
@@ -15,8 +21,20 @@ import cern.c2mon.server.common.datatag.DataTag;
 @Configuration
 public class DataTagCacheConfig {
 
+  @Autowired
+  ThreadPoolTaskExecutor cacheLoaderTaskExecutor;
+
+  @Autowired
+  CacheLoaderProperties properties;
+
   @Bean(name = C2monCacheName.Names.DATATAG)
-  public C2monCache createCache(AbstractC2monCacheFactory cachingFactory) {
-    return cachingFactory.createCache(C2monCacheName.DATATAG.getLabel(), Long.class, DataTag.class);
+  public C2monCache createCache(AbstractC2monCacheFactory cachingFactory, DataTagLoaderDAO dataTagLoaderDAO) {
+    C2monCache cache = cachingFactory.createCache(C2monCacheName.DATATAG.getLabel(), Long.class, DataTag.class);
+
+    C2monCacheLoader cacheLoader = new BatchCacheLoader<Long, DataTag>(cacheLoaderTaskExecutor, cache, dataTagLoaderDAO,
+            properties.getBatchSize(), "DataTagLoader-");
+    cache.setCacheLoader(cacheLoader);
+
+    return cache;
   }
 }
--- c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/test/java/cern/c2mon/cache/datatag/DataTagCacheLoaderTest.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/test/java/cern/c2mon/cache/datatag/DataTagCacheLoaderTest.java
@@ -1,7 +1,57 @@
 package cern.c2mon.cache.datatag;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cern.c2mon.cache.AbstractCacheLoaderTest;
+import cern.c2mon.cache.api.C2monCache;
+import cern.c2mon.server.cache.dbaccess.DataTagMapper;
+import cern.c2mon.server.common.control.ControlTag;
+import cern.c2mon.server.common.datatag.DataTag;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 /**
  * @author Szymon Halastra
  */
-public class DataTagCacheLoaderTest {
+public class DataTagCacheLoaderTest extends AbstractCacheLoaderTest {
+
+  @Autowired
+  private C2monCache<Long, DataTag> dataTagCacheRef;
+
+  @Autowired
+  private DataTagMapper dataTagMapper;
+
+  @Before
+  public void init() {
+    dataTagCacheRef.init();
+  }
+
+  @Test
+  public void preloadCache() {
+    assertNotNull("DataTag Cache should not be null", dataTagCacheRef);
+
+    List<DataTag> dataTagList = dataTagMapper.getAll();
+
+    Set<Long> keySet = dataTagList.stream().map(DataTag::getId).collect(Collectors.toSet());
+    assertTrue("List of data tags should not be empty", dataTagList.size() > 0);
+
+    assertEquals("Size of cache and DB mapping should be equal", dataTagList.size(), dataTagCacheRef.getKeys().size());
+    //compare all the objects from the cache and buffer
+    Iterator<DataTag> it = dataTagList.iterator();
+    while (it.hasNext()) {
+      DataTag currentTag = it.next();
+      //equality of DataTagCacheObjects => currently only compares names
+      assertEquals("Cached DataTag should have the same name as in DB",
+              currentTag.getName(), (dataTagCacheRef.get(currentTag.getId())).getName());
+    }
+  }
 }
