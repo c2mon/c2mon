04d3f2884bb2a34415408c5d6bc4f12968ee4d8b
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-09-25 10:37:14 +0200

    #39: Add integration test for preloading CommfaultTag Cache, configure cache loader

--- c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/commfault/CommFaultTagCacheConfig.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/commfault/CommFaultTagCacheConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 
 import cern.c2mon.cache.api.C2monCache;
 import cern.c2mon.cache.api.factory.AbstractC2monCacheFactory;
+import cern.c2mon.cache.api.loader.C2monCacheLoader;
 import cern.c2mon.server.cache.C2monCacheName;
+import cern.c2mon.server.cache.loader.CommFaultTagDAO;
+import cern.c2mon.server.cache.loader.common.SimpleCacheLoader;
 import cern.c2mon.shared.client.configuration.api.tag.CommFaultTag;
 
 /**
@@ -15,7 +18,12 @@ import cern.c2mon.shared.client.configuration.api.tag.CommFaultTag;
 public class CommFaultTagCacheConfig {
 
   @Bean(name = C2monCacheName.Names.COMMFAULT)
-  public C2monCache createCache(AbstractC2monCacheFactory cachingFactory) {
-    return cachingFactory.createCache(C2monCacheName.COMMFAULT.getLabel(), Long.class, CommFaultTag.class);
+  public C2monCache createCache(AbstractC2monCacheFactory cachingFactory, CommFaultTagDAO commFaultTagDAO) {
+    C2monCache cache = cachingFactory.createCache(C2monCacheName.COMMFAULT.getLabel(), Long.class, CommFaultTag.class);
+
+    C2monCacheLoader cacheLoader = new SimpleCacheLoader(cache, commFaultTagDAO);
+    cache.setCacheLoader(cacheLoader);
+
+    return cache;
   }
 }
--- c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/test/java/cern/c2mon/cache/commfault/CommfaultCacheLoaderTest.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/test/java/cern/c2mon/cache/commfault/CommfaultCacheLoaderTest.java
@@ -1,7 +1,55 @@
 package cern.c2mon.cache.commfault;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cern.c2mon.cache.AbstractCacheLoaderTest;
+import cern.c2mon.cache.api.C2monCache;
+import cern.c2mon.server.cache.dbaccess.CommFaultTagMapper;
+import cern.c2mon.server.common.commfault.CommFaultTag;
+import cern.c2mon.server.common.commfault.CommFaultTagCacheObject;
+
+import static org.junit.Assert.*;
+
 /**
  * @author Szymon Halastra
  */
-public class CommfaultCacheLoaderTest {
+public class CommfaultCacheLoaderTest extends AbstractCacheLoaderTest {
+
+  @Autowired
+  private C2monCache<Long, CommFaultTag> commFaultTagCacheRef;
+
+  @Autowired
+  private CommFaultTagMapper commFaultTagMapper;
+
+  @Before
+  public void init() {
+    commFaultTagCacheRef.init();
+  }
+
+  @Test
+  public void preloadCache() {
+    assertNotNull("Commfault Cache should not be null", commFaultTagCacheRef);
+
+    List<CommFaultTag> commFaultList = commFaultTagMapper.getAll();
+
+    Set<Long> keySet = commFaultList.stream().map(CommFaultTag::getId).collect(Collectors.toSet());
+    assertTrue("List of alarms should not be empty", commFaultList.size() > 0);
+
+    assertEquals("Size of cache and DB mapping should be equal", commFaultList.size(), commFaultTagCacheRef.getKeys().size());
+    //compare all the objects from the cache and buffer
+    Iterator<CommFaultTag> it = commFaultList.iterator();
+    while (it.hasNext()) {
+      CommFaultTagCacheObject currentTag = (CommFaultTagCacheObject) it.next();
+      //equality of DataTagCacheObjects => currently only compares names
+      assertEquals("Cached CommfaultTag should have the same name as in DB",
+              currentTag.getEquipmentId(), ((commFaultTagCacheRef.get(currentTag.getId())).getEquipmentId()));
+    }
+  }
 }
