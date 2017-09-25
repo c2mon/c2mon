55491e382a31f42831cb03a033c7ee118304fbe4
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-09-25 11:42:48 +0200

    #39: Add integration test for preloading SubEquipment Cache, configure cache loader

--- c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/subequipment/SubEquipmentCacheConfig.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/subequipment/SubEquipmentCacheConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 
 import cern.c2mon.cache.api.C2monCache;
 import cern.c2mon.cache.api.factory.AbstractC2monCacheFactory;
+import cern.c2mon.cache.api.loader.C2monCacheLoader;
 import cern.c2mon.server.cache.C2monCacheName;
+import cern.c2mon.server.cache.loader.SubEquipmentDAO;
+import cern.c2mon.server.cache.loader.common.SimpleCacheLoader;
 import cern.c2mon.server.common.subequipment.SubEquipment;
 
 /**
@@ -16,7 +19,12 @@ import cern.c2mon.server.common.subequipment.SubEquipment;
 public class SubEquipmentCacheConfig {
 
   @Bean(name = C2monCacheName.Names.SUBEQUIPMENT)
-  public C2monCache createCache(AbstractC2monCacheFactory cachingFactory) {
-    return cachingFactory.createCache(C2monCacheName.SUBEQUIPMENT.getLabel(), Long.class, SubEquipment.class);
+  public C2monCache createCache(AbstractC2monCacheFactory cachingFactory, SubEquipmentDAO subEquipmentDAO) {
+    C2monCache cache = cachingFactory.createCache(C2monCacheName.SUBEQUIPMENT.getLabel(), Long.class, SubEquipment.class);
+
+    C2monCacheLoader cacheLoader = new SimpleCacheLoader<>(cache, subEquipmentDAO);
+    cache.setCacheLoader(cacheLoader);
+
+    return cache;
   }
 }
--- c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/test/java/cern/c2mon/cache/subequipment/SubequipmentCacheLoaderTest.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/test/java/cern/c2mon/cache/subequipment/SubequipmentCacheLoaderTest.java
@@ -1,7 +1,52 @@
 package cern.c2mon.cache.subequipment;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cern.c2mon.cache.AbstractCacheLoaderTest;
+import cern.c2mon.cache.api.C2monCache;
+import cern.c2mon.server.cache.dbaccess.SubEquipmentMapper;
+import cern.c2mon.server.common.subequipment.SubEquipment;
+import cern.c2mon.server.common.subequipment.SubEquipmentCacheObject;
+
+import static org.junit.Assert.*;
+
 /**
  * @author Szymon Halastra
  */
-public class SubequipmentCacheLoaderTest {
+public class SubequipmentCacheLoaderTest extends AbstractCacheLoaderTest {
+
+  @Autowired
+  private C2monCache<Long, SubEquipment> subEquipmentCacheRef;
+
+  @Autowired
+  private SubEquipmentMapper subEquipmentMapper;
+
+  @Before
+  public void init() {
+    subEquipmentCacheRef.init();
+  }
+
+  @Test
+  public void preloadCache() {
+    assertNotNull("SubEquipment Cache should not be null", subEquipmentCacheRef);
+
+    List<cern.c2mon.server.common.subequipment.SubEquipment> subEquipmentList = subEquipmentMapper.getAll();
+
+    assertTrue("List of subEquipment tags should not be empty", subEquipmentList.size() > 0);
+
+    assertEquals("Size of cache and DB mapping should be equal", subEquipmentList.size(), subEquipmentCacheRef.getKeys().size());
+    //compare all the objects from the cache and buffer
+    Iterator<SubEquipment> it = subEquipmentList.iterator();
+    while (it.hasNext()) {
+      SubEquipmentCacheObject currentSubEquipment = (SubEquipmentCacheObject) it.next();
+      //only compares one field so far
+      assertEquals("Cached SubEquipment should have the same name as in DB",
+              currentSubEquipment.getName(), ((subEquipmentCacheRef.get(currentSubEquipment.getId())).getName()));
+    }
+  }
 }
