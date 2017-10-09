f8c34a6597a0756e0bad899c63e529861c5238a1
Martin Flamm <martin.flamm@cern.ch>
2017-10-09 19:58:37 +0200

    Implement ExpressionMapperTest

--- c2mon-server/c2mon-server-cachedbaccess/src/test/java/cern/c2mon/server/cache/dbaccess/ExpressionMapperTest.java
+++ c2mon-server/c2mon-server-cachedbaccess/src/test/java/cern/c2mon/server/cache/dbaccess/ExpressionMapperTest.java
@@ -2,12 +2,15 @@ package cern.c2mon.server.cache.dbaccess;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import cern.c2mon.server.common.expression.DslExpression;
+import cern.c2mon.server.common.expression.ExpressionCacheObject;
 import cern.c2mon.server.common.metadata.Metadata;
+
+import static org.junit.Assert.*;
 
 /**
  * @author Martin Flamm
@@ -19,15 +22,36 @@ public class ExpressionMapperTest extends AbstractMapperTest {
 
   @Test
   public void testInsertCompletes() {
-    DslExpression expression = createExpression(1L, "avg(q(name:'*/cpu.loadavg', '1m'))");
-
+    ExpressionCacheObject expression = createExpression(1L, "avg(q(name:'*/cpu.loadavg', '1m'))");
     expressionMapper.insertExpression(expression);
+
+    ExpressionCacheObject cachedObject = expressionMapper.getItem(1L);
+
+    assertEquals("avg(q(name:'*/cpu.loadavg', '1m'))", cachedObject.getExpression());
+    Metadata metadata = new Metadata();
+    metadata.addMetadata("responsible", "Joe Bloggs");
+    assertEquals(metadata, cachedObject.getMetadata());
+    assertTrue(1L == cachedObject.getId());
+    //assertEquals(Arrays.asList(1L, 2L, 3L), cachedObject.getAlarmIds());
+    assertEquals("This is a expression description", cachedObject.getDescription());
+    assertEquals("Java.lang.Float", cachedObject.getDataType());
+    assertEquals(1252516526L, cachedObject.getVersion());
+    assertEquals(22.25452D, cachedObject.getValue());
+
     expressionMapper.deleteExpression(expression.getId());
   }
 
-  private DslExpression createExpression(long id, String expressionString) {
-    DslExpression expression = new DslExpression();
-    expression.setId(id);
+  @Test
+  public void testDeleteCompletes() {
+    ExpressionCacheObject expression = createExpression(1L, "avg(q(name:'*/cpu.loadavg', '1m'))");
+    expressionMapper.insertExpression(expression);
+
+    expressionMapper.deleteExpression(expression.getId());
+    assertTrue(expressionMapper.getAll().isEmpty());
+  }
+
+  private ExpressionCacheObject createExpression(long id, String expressionString) {
+    ExpressionCacheObject expression = new ExpressionCacheObject(id);
     expression.setExpression(expressionString);
 
     Metadata metadata = new Metadata();
@@ -36,9 +60,10 @@ public class ExpressionMapperTest extends AbstractMapperTest {
 
     expression.setAlarmIds(new ArrayList<>(Arrays.asList(1L, 2L, 3L)));
     expression.setDescription("This is a expression description");
-    expression.setDataType("java.lang.Float");
-    expression.setVersion(12525216526L);
-    expression.setResult(22.25452F);
+    expression.setDataType("Java.lang.Float");
+    expression.setVersion(1252516526L);
+    expression.setValue(22.25452F);
+
     return expression;
   }
 }
