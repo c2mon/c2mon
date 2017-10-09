4e3c5aaec0ccc7f535e17551044957d129a9f1f6
Martin Flamm <martin.flamm@cern.ch>
2017-10-09 19:46:17 +0200

    Add ExpressionCacheObject

---
+++ c2mon-server/c2mon-server-common/src/main/java/cern/c2mon/server/common/expression/ExpressionCacheObject.java
@@ -0,0 +1,82 @@
+package cern.c2mon.server.common.expression;
+
+import java.sql.Timestamp;
+import java.util.Set;
+
+import lombok.Data;
+
+import cern.c2mon.server.common.metadata.Metadata;
+import cern.c2mon.server.common.rule.RuleTag;
+import cern.c2mon.server.common.tag.AbstractTagCacheObject;
+
+/**
+ * @author Martin Flamm
+ */
+
+@Data
+public class ExpressionCacheObject extends AbstractTagCacheObject implements RuleTag, Cloneable {
+
+  /**
+   * Version number of the class used during serialization/deserialization. This
+   * is to ensure that minor changes to the class do not prevent us from
+   * reading back ExpressionCacheObject we have serialized earlier. If fields are
+   * added/removed from the class, the version number needs to change.
+   */
+  private static final long serialVersionUID = -1458781710956275452L;
+
+  /**
+   * The expression which needs to be resolved.
+   * <p>
+   * The last result of the expression.
+   * <p>
+   * The current version of the expression.
+   * <p>
+   * Corresponding rule id in the ruleTagCache. Every Expression has a corresponding rule in the ruleTagCache in order
+   * to enable the ruleEngine to treat expression in the same way like rules.
+   */
+  private String expression;
+
+  /**
+   * The current version of the expression.
+   */
+  private long version;
+
+  /**
+   * Used for config loader.
+   *
+   * @param id
+   */
+  public ExpressionCacheObject(Long id) {
+    super(id);
+  }
+
+  @Override
+  public Object clone() throws CloneNotSupportedException {
+    return new CloneNotSupportedException();
+  }
+
+  @Override
+  public Timestamp getTimestamp() {
+    return null;
+  }
+
+  @Override
+  public Set<Long> getEquipmentIds() {
+    return null;
+  }
+
+  @Override
+  public Set<Long> getProcessIds() {
+    return null;
+  }
+
+  @Override
+  public Set<Long> getSubEquipmentIds() {
+    return null;
+  }
+
+  @Override
+  public String getRuleText() {
+    return null;
+  }
+}
