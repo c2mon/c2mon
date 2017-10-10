2a673771d5f691be1e920384dc196aedb84cf651
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-10-10 11:39:16 +0200

    #39: Modify AliveTimerGenerator Service, add additional tests

--- c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/alivetimer/AliveTimerGenerator.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/alivetimer/AliveTimerGenerator.java
@@ -9,6 +9,7 @@ import cern.c2mon.server.common.alive.AliveTimerCacheObject;
 import cern.c2mon.server.common.equipment.AbstractEquipment;
 import cern.c2mon.server.common.equipment.Equipment;
 import cern.c2mon.server.common.process.Process;
+import cern.c2mon.server.common.subequipment.SubEquipment;
 
 /**
  * @author Szymon Halastra
@@ -23,22 +24,19 @@ public class AliveTimerGenerator {
     this.aliveTimerCacheRef = aliveTimerCacheRef;
   }
 
-  public void generateFromEquipment(AbstractEquipment abstractEquipment) {
-    String type;
-    if (abstractEquipment instanceof Equipment) {
-      type = AliveTimer.ALIVE_TYPE_EQUIPMENT;
-    }
-    else {
-      type = AliveTimer.ALIVE_TYPE_SUBEQUIPMENT;
-    }
-    AliveTimer aliveTimer = new AliveTimerCacheObject(abstractEquipment.getAliveTagId(), abstractEquipment.getId(), abstractEquipment.getName(),
-            abstractEquipment.getStateTagId(), type, abstractEquipment.getAliveInterval());
+  public void generate(Equipment equipment) {
+    AliveTimer aliveTimer = new AliveTimerCacheObject(equipment.getAliveTagId(), equipment.getId(), equipment.getName(),
+            equipment.getStateTagId(), AliveTimer.ALIVE_TYPE_EQUIPMENT, equipment.getAliveInterval());
     aliveTimerCacheRef.put(aliveTimer.getId(), aliveTimer);
   }
 
-  //TODO: remove instanceof and replace by two methods with different type
+  public void generate(SubEquipment subEquipment) {
+    AliveTimer aliveTimer = new AliveTimerCacheObject(subEquipment.getAliveTagId(), subEquipment.getId(), subEquipment.getName(),
+            subEquipment.getStateTagId(), AliveTimer.ALIVE_TYPE_SUBEQUIPMENT, subEquipment.getAliveInterval());
+    aliveTimerCacheRef.put(aliveTimer.getId(), aliveTimer);
+  }
 
-  public void generateFromProcess(Process process) {
+  public void generate(Process process) {
     AliveTimer aliveTimer = new AliveTimerCacheObject(process.getAliveTagId(), process.getId(), process.getName(),
             process.getStateTagId(), AliveTimer.ALIVE_TYPE_PROCESS, process.getAliveInterval());
     aliveTimerCacheRef.put(aliveTimer.getId(), aliveTimer);
---
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/test/java/cern/c2mon/cache/alivetimer/AliveTimerGeneratorTest.java
@@ -0,0 +1,100 @@
+package cern.c2mon.cache.alivetimer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import cern.c2mon.cache.api.Cache;
+import cern.c2mon.cache.api.impl.SimpleCache;
+import cern.c2mon.server.cache.alivetimer.AliveTimerGenerator;
+import cern.c2mon.server.common.alive.AliveTimer;
+import cern.c2mon.server.common.alive.AliveTimerCacheObject;
+import cern.c2mon.server.common.equipment.EquipmentCacheObject;
+import cern.c2mon.server.common.process.ProcessCacheObject;
+import cern.c2mon.server.common.subequipment.SubEquipmentCacheObject;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Szymon Halastra
+ */
+public class AliveTimerGeneratorTest {
+
+  private static final Long KEY = 1L;
+  private static final Long ALIVE_TAG_ID = 10L;
+  private static final Long STATE_TAG_ID = 100L;
+
+  private static final int ALIVE_INTERVAL = 1000;
+
+  private Cache<Long, AliveTimer> aliveTimerCache;
+  private AliveTimerGenerator generator;
+
+  @Before
+  public void init() {
+    aliveTimerCache = new SimpleCache<>("aliveTimerCache");
+    generator = new AliveTimerGenerator(aliveTimerCache);
+  }
+
+  @After
+  public void cleanup() {
+    aliveTimerCache.remove(ALIVE_TAG_ID);
+  }
+
+  @Test
+  public void generateFromEquipment() {
+    EquipmentCacheObject equipmentCacheObject = new EquipmentCacheObject(KEY);
+    equipmentCacheObject.setAliveTagId(ALIVE_TAG_ID);
+    equipmentCacheObject.setName("equipment");
+    equipmentCacheObject.setStateTagId(STATE_TAG_ID);
+    equipmentCacheObject.setAliveInterval(ALIVE_INTERVAL);
+
+    generator.generate(equipmentCacheObject);
+
+    AliveTimer cachedAliveTimer = aliveTimerCache.get(ALIVE_TAG_ID);
+
+    assertTrue("AliveTimer should have Equipment type set", cachedAliveTimer.isEquipmentAliveType());
+    assertEquals("AliveTimer should have interval set", equipmentCacheObject.getAliveInterval(), cachedAliveTimer.getAliveInterval());
+    assertEquals("AliveTimer should have relatedId set", equipmentCacheObject.getId(), cachedAliveTimer.getRelatedId());
+    assertEquals("AliveTimer should have relatedName set", equipmentCacheObject.getName(), cachedAliveTimer.getRelatedName());
+    assertEquals("AliveTimer should have stateTagId set", equipmentCacheObject.getStateTagId(), cachedAliveTimer.getRelatedStateTagId());
+  }
+
+  @Test
+  public void generateFromSubEquipment() {
+    SubEquipmentCacheObject subEquipmentCacheObject = new SubEquipmentCacheObject(KEY);
+    subEquipmentCacheObject.setAliveTagId(ALIVE_TAG_ID);
+    subEquipmentCacheObject.setName("subEquipment");
+    subEquipmentCacheObject.setStateTagId(STATE_TAG_ID);
+    subEquipmentCacheObject.setAliveInterval(ALIVE_INTERVAL);
+
+    generator.generate(subEquipmentCacheObject);
+
+    AliveTimerCacheObject cachedAliveTimer = (AliveTimerCacheObject) aliveTimerCache.get(ALIVE_TAG_ID);
+
+    assertEquals("AliveTimer should have SubEquipment type set", AliveTimer.ALIVE_TYPE_SUBEQUIPMENT, cachedAliveTimer.getAliveType());
+    assertEquals("AliveTimer should have interval set", subEquipmentCacheObject.getAliveInterval(), cachedAliveTimer.getAliveInterval());
+    assertEquals("AliveTimer should have relatedId set", subEquipmentCacheObject.getId(), cachedAliveTimer.getRelatedId());
+    assertEquals("AliveTimer should have relatedName set", subEquipmentCacheObject.getName(), cachedAliveTimer.getRelatedName());
+    assertEquals("AliveTimer should have stateTagId set", subEquipmentCacheObject.getStateTagId(), cachedAliveTimer.getRelatedStateTagId());
+  }
+
+  @Test
+  public void generateFromProcess() {
+    ProcessCacheObject processCacheObject = new ProcessCacheObject(KEY);
+    processCacheObject.setAliveTagId(ALIVE_TAG_ID);
+    processCacheObject.setName("process");
+    processCacheObject.setStateTagId(STATE_TAG_ID);
+    processCacheObject.setAliveInterval(ALIVE_INTERVAL);
+
+    generator.generate(processCacheObject);
+
+    AliveTimer cachedAliveTimer = aliveTimerCache.get(ALIVE_TAG_ID);
+
+    assertTrue("AliveTimer should have Process type set", cachedAliveTimer.isProcessAliveType());
+    assertEquals("AliveTimer should have interval set", processCacheObject.getAliveInterval(), cachedAliveTimer.getAliveInterval());
+    assertEquals("AliveTimer should have relatedId set", processCacheObject.getId(), cachedAliveTimer.getRelatedId());
+    assertEquals("AliveTimer should have relatedName set", processCacheObject.getName(), cachedAliveTimer.getRelatedName());
+    assertEquals("AliveTimer should have stateTagId set", processCacheObject.getStateTagId(), cachedAliveTimer.getRelatedStateTagId());
+  }
+}
