572cd803615a839137f4618b710930344bcc18c7
Brice Copy <brice.copy@cern.ch>
2017-12-14 15:41:05 +0000

    #177: Resolve "Update MYSQL schema scripts for version 5.7"

--- c2mon-server/c2mon-server-history/src/main/resources/sql/history-schema-mysql.sql
+++ c2mon-server/c2mon-server-history/src/main/resources/sql/history-schema-mysql.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS SHORTTERMLOG (
   logdate           DATE         NOT NULL,
   tagid             INTEGER      NOT NULL,
-  tagname           VARCHAR(60),
+  tagname           VARCHAR(200),
   tagvalue          VARCHAR(4000),
   tagvaluedesc      VARCHAR(1000),
   tagdatatype       VARCHAR(200),
-  tagtime           TIMESTAMP(6),
-  tagservertime     TIMESTAMP(6) NOT NULL,
-  tagdaqtime        TIMESTAMP(6),
+  tagtime           TIMESTAMP DEFAULT '1970-01-02 00:00:01',
+  tagservertime     TIMESTAMP NOT NULL DEFAULT '1970-01-02 00:00:01',
+  tagdaqtime        TIMESTAMP DEFAULT '1970-01-02 00:00:01',
   tagstatus         INTEGER,
   tagstatusdesc     VARCHAR(1000),
   tagmode           INTEGER,
@@ -19,10 +19,11 @@ ALTER TABLE SHORTTERMLOG ADD INDEX tagservertime_tagid_idx (tagservertime, tagid
 CREATE TABLE IF NOT EXISTS SUPERVISION_LOG (
   SUL_ENTITY  VARCHAR(30)  NOT NULL,
   SUL_ID      INTEGER      NOT NULL,
-  SUL_DATE    TIMESTAMP(6) NOT NULL,
+  SUL_DATE    TIMESTAMP    NOT NULL DEFAULT '1970-01-02 00:00:01',
   SUL_STATUS  VARCHAR(20)  NOT NULL,
   SUL_MESSAGE VARCHAR(200)
 );
+
 
 CREATE TABLE IF NOT EXISTS STL_DAY_SNAPSHOT (
   logdate       DATE,
@@ -30,9 +31,9 @@ CREATE TABLE IF NOT EXISTS STL_DAY_SNAPSHOT (
   tagname       VARCHAR(60),
   tagvalue      VARCHAR(4000),
   tagdatatype   VARCHAR(200),
-  tagtime       TIMESTAMP(6),
-  tagservertime TIMESTAMP(6),
-  tagdaqtime    TIMESTAMP(6),
+  tagtime       TIMESTAMP DEFAULT '1970-01-02 00:00:01',
+  tagservertime TIMESTAMP DEFAULT '1970-01-02 00:00:01',
+  tagdaqtime    TIMESTAMP DEFAULT '1970-01-02 00:00:01',
   tagstatus     INTEGER,
   tagstatusdesc VARCHAR(1000),
   tagmode       INTEGER,
@@ -43,12 +44,12 @@ CREATE TABLE IF NOT EXISTS COMMANDTAGLOG (
   cmdid           INTEGER       NOT NULL,
   cmdname         VARCHAR(60)   NOT NULL,
   cmdmode         INTEGER       NOT NULL,
-  cmdtime         TIMESTAMP(6)  NOT NULL,
+  cmdtime         TIMESTAMP     NOT NULL DEFAULT '1970-01-02 00:00:01',
   cmdvalue        VARCHAR(4000),
   cmddatatype     VARCHAR(200),
   cmduser         VARCHAR(25),
   cmdhost         VARCHAR(60),
-  cmdreporttime   TIMESTAMP(6),
+  cmdreporttime   TIMESTAMP    DEFAULT '1970-01-02 00:00:01',
   cmdreportstatus VARCHAR(50),
   cmdreportdesc   VARCHAR(100)
 );
@@ -56,7 +57,7 @@ CREATE TABLE IF NOT EXISTS COMMANDTAGLOG (
 CREATE TABLE IF NOT EXISTS SERVER_LIFECYCLE_LOG (
   SLL_EVENT_TYPE  VARCHAR(10) NOT NULL,
   SLL_SERVER_NAME VARCHAR(20) NOT NULL,
-  SLL_TIME        TIMESTAMP(6) NOT NULL
+  SLL_TIME        TIMESTAMP NOT NULL DEFAULT '1970-01-02 00:00:01'
 );
 
 CREATE TABLE IF NOT EXISTS ALARMLOG (
@@ -67,7 +68,7 @@ CREATE TABLE IF NOT EXISTS ALARMLOG (
   faultmember     VARCHAR(60)   NOT NULL,
   faultcode       INTEGER       NOT NULL,
   active          VARCHAR(1)    NOT NULL,
-  servertime      TIMESTAMP(6)  NOT NULL,
+  servertime      TIMESTAMP  NOT NULL DEFAULT '1970-01-02 00:00:01',
   info            VARCHAR(100)  DEFAULT NULL
 );
 
