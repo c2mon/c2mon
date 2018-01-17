984c94109855f6564175d46f31bc5893458046ec
bcopy <brice.copy@gmail.com>
2018-01-17 11:24:41 +0100

    #4 remove unnecessary imports

--- src/main/java/cern/c2mon/client/ext/dynconfig/strategy/OpcUaConfigStrategy.java
+++ src/main/java/cern/c2mon/client/ext/dynconfig/strategy/OpcUaConfigStrategy.java
@@ -2,8 +2,6 @@ package cern.c2mon.client.ext.dynconfig.strategy;
 
 import java.net.URI;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
