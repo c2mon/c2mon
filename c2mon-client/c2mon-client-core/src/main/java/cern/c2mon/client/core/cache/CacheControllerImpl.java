0e891fdd45e5cd02c8fe6eed33f28b2d7a168a16
Matthias Braeger <Matthias.Braeger@cern.ch>
2018-03-16 11:20:18 +0100

    #200: Refactor variables naming in CacheControllerImpl

--- c2mon-client/c2mon-client-core/src/main/java/cern/c2mon/client/core/cache/CacheControllerImpl.java
+++ c2mon-client/c2mon-client-core/src/main/java/cern/c2mon/client/core/cache/CacheControllerImpl.java
@@ -1,16 +1,16 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
@@ -35,67 +35,67 @@ import cern.c2mon.client.core.tag.TagController;
 class CacheControllerImpl implements CacheController {
   /** Logger instance */
   private static final Logger LOG = LoggerFactory.getLogger(CacheControllerImpl.class);
-  
+
   /**
    * Pointer to the actual used cache instance (live or history)
    */
   private Map<Long, TagController> activeCache = null;
-  
-  /** Thread synchronization lock for avoiding a cache mode switch */ 
+
+  /** Thread synchronization lock for avoiding a cache mode switch */
   private final Object historyModeLock = new Object();
-  
-  /** 
+
+  /**
    * <code>Map</code> containing all subscribed data tags which are updated via the
    * <code>JmsProxy</code>
    */
   private final Map<Long, TagController> liveCache = new Hashtable<>(1500);
-  
-  /** 
+
+  /**
    * <code>Map</code> containing all subscribed data tags which are updated via the
    * <code>HistoryManager</code>
    */
   private final Map<Long, TagController> historyCache = new Hashtable<>(1500);
-  
+
   /**
    * Flag to remember whether the cache is in history mode or not
    */
   private volatile boolean historyMode = false;
-  
+
   /** Thread lock for access to the <code>dataTags</code> Map */
   private final ReentrantReadWriteLock cacheLock = new ReentrantReadWriteLock();
-  
+
   /**
    * Default Constructor
    */
   CacheControllerImpl() {
     activeCache = liveCache;
   }
-  
+
   @Override
   public Map<Long, TagController> getActiveCache() {
     return activeCache;
   }
-  
+
   @Override
   public Map<Long, TagController> getHistoryCache() {
     return historyCache;
   }
-  
+
   @Override
   public Map<Long, TagController> getLiveCache() {
     return liveCache;
   }
-  
+
   @Override
   public boolean isHistoryModeEnabled() {
     return historyMode;
   }
-  
+
   @Override
   public Object getHistoryModeSyncLock() {
     return historyModeLock;
   }
-  
+
   @Override
   public void setHistoryMode(final boolean enable) {
     synchronized (historyModeLock) {
@@ -103,7 +103,7 @@ class CacheControllerImpl implements CacheController {
         LOG.info("setHistoryMode() - The cache is already in history mode.");
         return;
       }
-      
+
       cacheLock.readLock().lock();
       try {
         if (enable) {
@@ -116,62 +116,64 @@ class CacheControllerImpl implements CacheController {
       finally {
         cacheLock.readLock().unlock();
       }
-      
+
       historyMode = enable;
     }
   }
-  
+
   @Override
   public WriteLock getWriteLock() {
     return cacheLock.writeLock();
   }
-  
+
   @Override
   public ReadLock getReadLock() {
     return cacheLock.readLock();
   }
-  
+
   /**
    * Inner method which moves all registered <code>DataTagUpdateListener</code>
    * listeners back to the live cache instance.
    */
   private void disableHistoryMode() {
-    TagController historyTag = null;
+    TagController historyTagController = null;
     Collection<BaseTagListener> listeners = null;
-    
+
     for (Entry<Long, TagController> entry : historyCache.entrySet()) {
-      historyTag = entry.getValue();
-      listeners = historyTag.getUpdateListeners();
-      
-      historyTag.removeAllUpdateListeners();
+      historyTagController = entry.getValue();
+
+      listeners = historyTagController.getUpdateListeners();
+      historyTagController.removeAllUpdateListeners();
+
       liveCache.get(entry.getKey()).addUpdateListeners(listeners);
     }
-    
+
     activeCache = liveCache;
     historyCache.clear();
   }
-  
+
   /**
    * Inner method which clones the entire live cache and moves all registered
    * <code>DataTagUpdateListener</code> listeners to the history cache instance.
    */
   private void enableHistoryMode() {
     historyCache.clear();
-    TagController liveTag = null;
-    TagController historyTag = null;
+    TagController liveTagController = null;
+    TagController historyTagController = null;
     Collection<BaseTagListener> listeners = null;
-    
+
     for (Entry<Long, TagController> entry : liveCache.entrySet()) {
-      liveTag = entry.getValue();
-      
-      historyTag = new TagController(liveTag.getTagImpl().clone());
-      
-      listeners = liveTag.getUpdateListeners();
-      liveTag.removeAllUpdateListeners();
-      historyTag.addUpdateListeners(listeners);
-      historyCache.put(entry.getKey(), historyTag);
+      liveTagController = entry.getValue();
+
+      listeners = liveTagController.getUpdateListeners();
+      liveTagController.removeAllUpdateListeners();
+
+      historyTagController = new TagController(liveTagController.getTagImpl().clone());
+      historyTagController.addUpdateListeners(listeners);
+
+      historyCache.put(entry.getKey(), historyTagController);
     }
-  
+
     activeCache = historyCache;
   }
 }
