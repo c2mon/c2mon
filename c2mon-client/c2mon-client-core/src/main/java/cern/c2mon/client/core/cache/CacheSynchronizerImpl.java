2de18090225d3bc5c70015a0892fbed23305a201
Matthias Braeger <Matthias.Braeger@cern.ch>
2018-03-16 11:21:15 +0100

    #200: Bug fix in synchronizeHistoryCache() method

--- c2mon-client/c2mon-client-core/src/main/java/cern/c2mon/client/core/cache/CacheSynchronizerImpl.java
+++ c2mon-client/c2mon-client-core/src/main/java/cern/c2mon/client/core/cache/CacheSynchronizerImpl.java
@@ -37,11 +37,10 @@ import cern.c2mon.client.core.jms.ConnectionListener;
 import cern.c2mon.client.core.jms.JmsProxy;
 import cern.c2mon.client.core.jms.RequestHandler;
 import cern.c2mon.client.core.listener.HeartbeatListener;
-import cern.c2mon.client.core.service.CoreSupervisionService;
 import cern.c2mon.client.core.manager.SupervisionServiceImpl;
+import cern.c2mon.client.core.service.CoreSupervisionService;
 import cern.c2mon.client.core.tag.TagController;
 import cern.c2mon.client.core.tag.TagImpl;
-
 import cern.c2mon.shared.client.supervision.Heartbeat;
 import cern.c2mon.shared.client.tag.TagUpdate;
 import cern.c2mon.shared.client.tag.TagValueUpdate;
@@ -530,7 +529,6 @@ public class CacheSynchronizerImpl implements CacheSynchronizer, HeartbeatListen
         // Adds the clone (without listeners) to the history cache.
         historyCache.put(tagId, historyTag);
       }
-      historyCacheUpdateList.clear();
     }
   }
 
