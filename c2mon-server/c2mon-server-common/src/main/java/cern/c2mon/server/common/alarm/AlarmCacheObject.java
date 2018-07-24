eb499dc3d85d701f4094129b85dcc316b840af33
Emiliano Piselli <emiliano.piselli@cern.ch>
2018-07-24 16:20:18 +0200

    Issue #145 Add oscillation flag with test class

--- c2mon-server/c2mon-server-common/src/main/java/cern/c2mon/server/common/alarm/AlarmCacheObject.java
+++ c2mon-server/c2mon-server-common/src/main/java/cern/c2mon/server/common/alarm/AlarmCacheObject.java
@@ -18,17 +18,17 @@ package cern.c2mon.server.common.alarm;
 
 import java.sql.Timestamp;
 
-import lombok.Data;
-
 import cern.c2mon.server.common.metadata.Metadata;
 import cern.c2mon.shared.common.Cacheable;
+import lombok.Data;
 
 /**
  * Alarm object held in the cache.
  * 
  * Imported more or less as-is into C2MON.
  * 
- * Note: in TIM1 care was taken to make sure this is "" and not null - be careful when sending to LASER as this may be the reason (?)
+ * Note: in TIM1 care was taken to make sure this is "" and not null - be
+ * careful when sending to LASER as this may be the reason (?)
  * 
  * @author Mark Brightwell
  * 
@@ -36,208 +36,268 @@ import cern.c2mon.shared.common.Cacheable;
 @Data
 public class AlarmCacheObject implements Cloneable, Cacheable, Alarm {
 
-  /** Serial version UID */
-  private static final long serialVersionUID = 794087757524662419L;
-    
-  /**
-   * This enum contains all the possible change state values
-   * which an alarm object can have.
-   */
-  public enum AlarmChangeState {
-    /** Alarm state hasn't changed since. */
-    CHANGE_NONE,
-    /** The alarm's state has changed. */
-    CHANGE_STATE,
-    /** The additional information about the alarm has changed since the last update call. */
-    CHANGE_PROPERTIES
-  };
+	/** Serial version UID */
+	private static final long serialVersionUID = 794087757524662419L;
 
-  /**
-   * Internal identifier of the AlarmCacheObject.
-   */
-  private Long id;
+	/**
+	 * This enum contains all the possible change state values which an alarm
+	 * object can have.
+	 */
+	public enum AlarmChangeState {
+		/** Alarm state hasn't changed since. */
+		CHANGE_NONE,
+		/** The alarm's state has changed. */
+		CHANGE_STATE,
+		/**
+		 * The additional information about the alarm has changed since the last
+		 * update call.
+		 */
+		CHANGE_PROPERTIES
+	};
 
-  /**
-   * Unique identifier of the DataTagCacheObject to which the alarm is attached.
-   * The Alarm is activated or terminated depending on the current value of this
-   * data tag.
-   */
-  private Long dataTagId;
+	/**
+	 * Internal identifier of the AlarmCacheObject.
+	 */
+	private Long id;
 
-  /**
-   * LASER fault family of the alarm.
-   **/
-  private String faultFamily;
+	/**
+	 * Unique identifier of the DataTagCacheObject to which the alarm is
+	 * attached. The Alarm is activated or terminated depending on the current
+	 * value of this data tag.
+	 */
+	private Long dataTagId;
 
-  /**
-   * LASER fault member of the alarm.
-   **/
-  private String faultMember;
+	/**
+	 * LASER fault family of the alarm.
+	 **/
+	private String faultFamily;
 
-  /**
-   * LASER fault code of the alarm.
-   **/
-  private int faultCode;
+	/**
+	 * LASER fault member of the alarm.
+	 **/
+	private String faultMember;
 
-  /**
-   * AlarmCondition used to determine the alarm's current state
-   **/
-  private AlarmCondition condition;
+	/**
+	 * LASER fault code of the alarm.
+	 **/
+	private int faultCode;
 
-  /**
-   * The meta data of the Alatm. The meta data can be arbitrary and of of the type String, Numeric and Boolean.
-   * Not every Alarm needs to have a meta data. Also the meta data don't have to be every time the same.
-   */
-  private Metadata metadata;
+	/**
+	 * AlarmCondition used to determine the alarm's current state
+	 **/
+	private AlarmCondition condition;
 
-  /** 
-   * The alarm's current state 
-   **/
-  private String state;
+	/**
+	 * The meta data of the Alatm. The meta data can be arbitrary and of of the
+	 * type String, Numeric and Boolean. Not every Alarm needs to have a meta
+	 * data. Also the meta data don't have to be every time the same.
+	 */
+	private Metadata metadata;
 
-  /**
-   * Member indicating whether the alarm's state or properties have changed 
-   * since the last update.
-   */
-  private AlarmChangeState alarmChangeState;
+	/**
+	 * The alarm's current state
+	 **/
+	private String state;
 
-   /**
-   * Timestamp of the last state change
-   **/
-  private Timestamp timestamp;
-  
-  /**
-   * Was the current alarm value published?
-   */
-  private boolean published = false;
+	/**
+	 * Member indicating whether the alarm's state or properties have changed
+	 * since the last update.
+	 */
+	private AlarmChangeState alarmChangeState;
 
-  /**
-   * Optional info property. 
-   * TODO in TIM1 care was taken to make sure this is "" and not null - be careful when sending to LASER as this may be the reason
-   **/
-  private String info;
-  
-  /**
-   * Latest state to be published.
-   */
-  private AlarmPublication lastPublication;
+	/**
+	 * Timestamp of the last state change
+	 **/
 
-  /**
-   * Name of the JMS topic on which the alarm will be distributed to clients.
-   * TODO remove because single topic now for alarm publication?
-   */
-  private String topic = "c2mon.client.alarm";
-  
-  /**
-   * Default constructor.
-   */
-  public AlarmCacheObject() {
-    // Initialise run-time parameters with default values 
-    // (overwritten on loading if DB has none null values)
-    this.state = AlarmCondition.TERMINATE;
-    this.alarmChangeState = AlarmChangeState.CHANGE_NONE;
-    this.timestamp = new Timestamp(0);
-    this.info = "";    
-  }
 
-  /**
-   * Constructor setting Alarm id.
-   * @param id the id of the Alarm
-   */
-  public AlarmCacheObject(final Long id) {
-    this();
-    this.id = id;
-  }
+	private boolean lastActiveState;
+	private int counterFault=0;
+	private int timeRange=60;
+	private int oscNumbers=6;
+	private long firstOscTS;
+	private boolean oscillating=false;
+	
+	
+	public void setState(String newState)
+	{
+		this.state= newState;
+		checkOscillation();
+	}
+	
+	private void checkOscillation() {
+		final boolean isCurrentlyActive = isActive();
+		if (isCurrentlyActive != lastActiveState) {
+			increaseCounter();
+		} else {
+			resetCounter();
+		}
+		lastActiveState = isCurrentlyActive;
+		//System.out.println("last data:" + newData + " ==== counter:" + counterFault + " ==== OSCILLATING: " + oscillating);
+	}
 
-  /**
-   * Create a deep clone of this AlarmCacheObject.
-   * 
-   * @return a deep clone of this AlarmCacheObject
-   * @throws CloneNotSupportedException should never be thrown
-   */
-  public Object clone() throws CloneNotSupportedException {    
-     AlarmCacheObject alarmCacheObject = (AlarmCacheObject) super.clone();
-     if (this.condition != null) {
-       alarmCacheObject.condition = (AlarmCondition) this.condition.clone();
-     }     
-     if (this.timestamp != null){
-       alarmCacheObject.timestamp = (Timestamp) this.timestamp.clone();
-     }
-     if (this.lastPublication != null) {
-       alarmCacheObject.lastPublication = (AlarmPublication) lastPublication.clone();
-     }
-     return alarmCacheObject;
-  }
+	private void increaseCounter() {
+		counterFault++;
+		if (counterFault == 1) {
+			firstOscTS = System.currentTimeMillis();
+		}
+		oscillating = checkConditions();
+	}
 
-  public final Metadata getMetadata() {
-    if (this.metadata == null) {
-      this.metadata = new Metadata();
-    }
-    return this.metadata;
-  }
+	private void resetCounter() {
+		counterFault = 0;
+		oscillating = false;
+	}
 
-  /**
-   * Getter method.
-   *
-   * @return the unique identifier of the DataTag to which the alarm is attached
-   */
-  @Override
-  public final Long getTagId() {
-    return this.dataTagId;
-  }
+	private boolean checkConditions() {
+		return ((counterFault >= oscNumbers) && ((System.currentTimeMillis() - firstOscTS) <= timeRange * 1000));
+	}
 
-  /**
-   * Checks if the Alarm state is ACTIVE.
-   * @return true if the alarm is currently active.
-   */
-  public boolean isActive() {
-    return this.state != null && this.state.equals(AlarmCondition.ACTIVE);
-  }
-  
-  @Override
-  public void hasBeenPublished(Timestamp publicationTime) {
-    if (publicationTime == null) {
-      throw new NullPointerException("Cannot set publication time to null");
-    }
-    published = true;
-    if (state != null) {
-      lastPublication = new AlarmPublication(this.state, this.info, publicationTime);
-    }    
-  }
-  
-  @Override
-  public void notYetPublished() {
-    published = false;
-  }
+	/**
+	 * EP
+	 **/
 
-  @Override
-  public boolean isPublishedToLaser() {
-    return isPublished();
-  }
-  
-  @Override
-  public String toString() {
-    StringBuffer str = new StringBuffer();
+	private Timestamp timestamp;
 
-    str.append(getId());
-    str.append('\t');
-    str.append(getTagId());
-    str.append('\t');
-    str.append(getTimestamp());
-    str.append('\t');
-    str.append(getFaultFamily());
-    str.append('\t');
-    str.append(getFaultMember());
-    str.append('\t');
-    str.append(getFaultCode());
-    str.append('\t');
-    str.append(getState());
-    if (getInfo() != null) {
-      str.append('\t');
-      str.append(getInfo());
-    }
+	/**
+	 * Was the current alarm value published?
+	 */
+	private boolean published = false;
 
-    return str.toString();
-  }
+	/**
+	 * Optional info property. TODO in TIM1 care was taken to make sure this is
+	 * "" and not null - be careful when sending to LASER as this may be the
+	 * reason
+	 **/
+	private String info;
+
+	/**
+	 * Latest state to be published.
+	 */
+	private AlarmPublication lastPublication;
+
+	/**
+	 * Name of the JMS topic on which the alarm will be distributed to clients.
+	 * TODO remove because single topic now for alarm publication?
+	 */
+	private String topic = "c2mon.client.alarm";
+
+	
+	
+	/**
+	 * Default constructor.
+	 */
+	public AlarmCacheObject() {
+		// Initialise run-time parameters with default values
+		// (overwritten on loading if DB has none null values)
+		this.state = AlarmCondition.TERMINATE;
+		this.alarmChangeState = AlarmChangeState.CHANGE_NONE;
+		this.timestamp = new Timestamp(0);
+		this.info = "";
+	}
+
+	/**
+	 * Constructor setting Alarm id.
+	 * 
+	 * @param id
+	 *            the id of the Alarm
+	 */
+	public AlarmCacheObject(final Long id) {
+		this();
+		this.id = id;
+	}
+
+	/**
+	 * Create a deep clone of this AlarmCacheObject.
+	 * 
+	 * @return a deep clone of this AlarmCacheObject
+	 * @throws CloneNotSupportedException
+	 *             should never be thrown
+	 */
+	public Object clone() throws CloneNotSupportedException {
+		AlarmCacheObject alarmCacheObject = (AlarmCacheObject) super.clone();
+		if (this.condition != null) {
+			alarmCacheObject.condition = (AlarmCondition) this.condition.clone();
+		}
+		if (this.timestamp != null) {
+			alarmCacheObject.timestamp = (Timestamp) this.timestamp.clone();
+		}
+		if (this.lastPublication != null) {
+			alarmCacheObject.lastPublication = (AlarmPublication) lastPublication.clone();
+		}
+		return alarmCacheObject;
+	}
+
+	public final Metadata getMetadata() {
+		if (this.metadata == null) {
+			this.metadata = new Metadata();
+		}
+		return this.metadata;
+	}
+
+	/**
+	 * Getter method.
+	 *
+	 * @return the unique identifier of the DataTag to which the alarm is
+	 *         attached
+	 */
+	@Override
+	public final Long getTagId() {
+		return this.dataTagId;
+	}
+
+	/**
+	 * Checks if the Alarm state is ACTIVE.
+	 * 
+	 * @return true if the alarm is currently active.
+	 */
+	public boolean isActive() {
+		return this.state != null && this.state.equals(AlarmCondition.ACTIVE);
+	}
+
+	@Override
+	public void hasBeenPublished(Timestamp publicationTime) {
+		if (publicationTime == null) {
+			throw new NullPointerException("Cannot set publication time to null");
+		}
+		published = true;
+		if (state != null) {
+			lastPublication = new AlarmPublication(this.state, this.info, publicationTime);
+		}
+	}
+
+	@Override
+	public void notYetPublished() {
+		published = false;
+	}
+
+	@Override
+	public boolean isPublishedToLaser() {
+		return isPublished();
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer str = new StringBuffer();
+
+		str.append(getId());
+		str.append('\t');
+		str.append(getTagId());
+		str.append('\t');
+		str.append(getTimestamp());
+		str.append('\t');
+		str.append(getFaultFamily());
+		str.append('\t');
+		str.append(getFaultMember());
+		str.append('\t');
+		str.append(getFaultCode());
+		str.append('\t');
+		str.append(getState());
+		if (getInfo() != null) {
+			str.append('\t');
+			str.append(getInfo());
+		}
+
+		return str.toString();
+	}
 
 }
