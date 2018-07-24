3b501bc139b740fbab96582d5b515b1dabbb828b
Emiliano Piselli <emiliano.piselli@cern.ch>
2018-07-24 16:52:06 +0200

    issue #145 Uncomment tests performed

--- c2mon-server/c2mon-server-common/src/test/java/cern/c2mon/server/common/alarm/AlarmCacheObjectTest.java
+++ c2mon-server/c2mon-server-common/src/test/java/cern/c2mon/server/common/alarm/AlarmCacheObjectTest.java
@@ -12,26 +12,26 @@ public class AlarmCacheObjectTest {
 		// - assert that the oscillation is detected
 		AlarmCacheObject aco = new AlarmCacheObject();
 
-//		for (int i = 0; i < 7; i++) {
-//			if (i % 2 == 0) {
-//				aco.setState(AlarmCondition.ACTIVE);
-//			} else {
-//				aco.setState(AlarmCondition.TERMINATE);
-//			}
-//		}
-//		assertTrue(aco.isOscillating());
-//
-//		aco = new AlarmCacheObject();
-//
-//		// Second case : no oscillation if less than 5 state changes
-//		for (int i = 0; i < 5; i++) {
-//			if (i % 2 == 0) {
-//				aco.setState(AlarmCondition.ACTIVE);
-//			} else {
-//				aco.setState(AlarmCondition.TERMINATE);
-//			}
-//		}
-//		assertFalse(aco.isOscillating());
+		for (int i = 0; i < 7; i++) {
+			if (i % 2 == 0) {
+				aco.setState(AlarmCondition.ACTIVE);
+			} else {
+				aco.setState(AlarmCondition.TERMINATE);
+			}
+		}
+		assertTrue(aco.isOscillating());
+
+		aco = new AlarmCacheObject();
+
+		// Second case : no oscillation if less than 5 state changes
+		for (int i = 0; i < 5; i++) {
+			if (i % 2 == 0) {
+				aco.setState(AlarmCondition.ACTIVE);
+			} else {
+				aco.setState(AlarmCondition.TERMINATE);
+			}
+		}
+		assertFalse(aco.isOscillating());
 
 		// Third case : reset after some time
 
