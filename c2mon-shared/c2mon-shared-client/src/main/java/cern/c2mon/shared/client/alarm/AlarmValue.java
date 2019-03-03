5be24c5165afe2380dc3445d5a70c01b0748c8bd
Matthias Braeger <Matthias.Braeger@cern.ch>
2019-03-03 23:03:19 +0100

    #145: Put back deleted method getTagDescription to AlarmValue

--- c2mon-shared/c2mon-shared-client/src/main/java/cern/c2mon/shared/client/alarm/AlarmValue.java
+++ c2mon-shared/c2mon-shared-client/src/main/java/cern/c2mon/shared/client/alarm/AlarmValue.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
+ * Copyright (C) 2010-2019 CERN. All rights not expressly granted are reserved.
  * 
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
@@ -22,7 +22,7 @@ import java.util.Map;
 import cern.c2mon.shared.client.request.ClientRequestResult;
 
 /**
- * This 
+ * Alarm value interface
  *
  * @author Matthias Braeger
  */
@@ -72,6 +72,11 @@ public interface AlarmValue extends ClientRequestResult {
   Long getTagId();
 
   Map<String, Object> getMetadata();
+  
+  /** 
+   * @return The description of the Tag 
+   */
+  String getTagDescription();
   
   /**
    * @return true if the alarm is currently active.
