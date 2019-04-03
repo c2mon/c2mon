5b37240a9d86fb6c1ac20a4e72739c962eff1646
bcopy <brice.copy@cern.ch>
2019-04-03 09:29:00 +0200

    fix typos

--- c2mon-server/c2mon-server-cache/src/test/java/cern/c2mon/server/cache/alarm/impl/OscillationUpdaterTest.java
+++ c2mon-server/c2mon-server-cache/src/test/java/cern/c2mon/server/cache/alarm/impl/OscillationUpdaterTest.java
@@ -100,7 +100,6 @@ public class OscillationUpdaterTest {
       try {
         Thread.sleep(100);
       } catch (InterruptedException e) {
-        // TODO Auto-generated catch block
         e.printStackTrace();
       }
       dataTagCacheObject.setValue(i % 2);
