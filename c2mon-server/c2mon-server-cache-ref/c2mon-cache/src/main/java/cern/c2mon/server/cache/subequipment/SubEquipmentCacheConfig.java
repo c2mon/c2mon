398249f6fd5b3c4e2ea8acc464ba593b4571b95f
Alexander Papageorgiou Koufidis <alexander.papageorgiou@cern.ch>
2019-08-27 17:03:25 +0200

    #39 Use Lombok Delegate annotation to replace SubequipmentService implem

--- c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/subequipment/SubEquipmentCacheConfig.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/subequipment/SubEquipmentCacheConfig.java
@@ -1,22 +1,32 @@
 package cern.c2mon.server.cache.subequipment;
 
+import cern.c2mon.cache.api.C2monCache;
+import cern.c2mon.cache.api.factory.AbstractCacheFactory;
+import cern.c2mon.server.cache.CacheName;
+import cern.c2mon.server.cache.alarm.AbstractCacheConfig;
+import cern.c2mon.server.cache.loader.SubEquipmentDAO;
+import cern.c2mon.server.cache.loader.config.CacheLoaderProperties;
+import cern.c2mon.server.common.subequipment.SubEquipment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 /**
  * @author Szymon Halastra
  */
 
 @Configuration
-public class SubEquipmentCacheConfig {
+public class SubEquipmentCacheConfig extends AbstractCacheConfig<SubEquipment> {
 
+  public SubEquipmentCacheConfig(ThreadPoolTaskExecutor cacheLoaderTaskExecutor, CacheLoaderProperties properties, AbstractCacheFactory cachingFactory) {
+    super(cacheLoaderTaskExecutor, properties, cachingFactory);
+  }
+
+//  TODO Fix this signature to properly handle BatchCacheLoaderDAO (see AlarmCacheConfig) and SimpleCacheLoaderDAO
 //  @Bean(name = CacheName.Names.SUBEQUIPMENT)
-//  public C2monCache createCache(AbstractCacheFactory cachingFactory, SubEquipmentDAO subEquipmentDAO) {
-//    C2monCache cache = cachingFactory.createCache(CacheName.SUBEQUIPMENT.getLabel(), Long.class,
-//            SubEquipment.class);
-//
-//    CacheLoader cacheLoader = new SimpleCacheLoader<>(cache, subEquipmentDAO);
-//    cache.setCacheLoader(cacheLoader);
-//
-//    return cache;
+//  @Autowired
+//  public C2monCache<SubEquipment> createCache(SubEquipmentDAO subEquipmentDAO) {
+//    return super.createCache(subEquipmentDAO, CacheName.SUBEQUIPMENT.getLabel(), SubEquipment.class, "SubEquipmentCacheLoader-");
 //  }
 }
--- c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/subequipment/SubEquipmentService.java
+++ c2mon-server/c2mon-server-cache-ref/c2mon-cache/src/main/java/cern/c2mon/server/cache/subequipment/SubEquipmentService.java
@@ -1,9 +1,12 @@
 package cern.c2mon.server.cache.subequipment;
 
-import java.sql.Timestamp;
 import java.util.Map;
 
+import cern.c2mon.server.cache.CoreAbstractEquipmentService;
+import cern.c2mon.server.cache.SupervisedServiceImpl;
+import lombok.experimental.Delegate;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cern.c2mon.cache.api.C2monCache;
@@ -12,8 +15,6 @@ import cern.c2mon.cache.api.service.SupervisedService;
 import cern.c2mon.server.cache.alivetimer.AliveTimerService;
 import cern.c2mon.server.cache.commfault.CommFaultService;
 import cern.c2mon.server.common.subequipment.SubEquipment;
-import cern.c2mon.shared.client.supervision.SupervisionEvent;
-import cern.c2mon.shared.common.supervision.SupervisionConstants;
 
 /**
  * @author Szymon Halastra
@@ -24,115 +25,32 @@ public class SubEquipmentService implements SupervisedService<SubEquipment>, Abs
 
   private C2monCache<SubEquipment> subEquipmentCacheRef;
 
+  private interface SubEquipmentSupervisedService extends SupervisedService<SubEquipment>{}
+
+  @Delegate(types = SubEquipmentSupervisedService.class)
   private SupervisedService<SubEquipment> supervisedService;
 
+  @Delegate(types = AbstractEquipmentService.class)
   private AbstractEquipmentService abstractEquipmentService;
 
   private CommFaultService commFaultService;
 
   private AliveTimerService aliveTimerService;
 
-//  @Autowired
-//  public SubEquipmentService(C2monCache<Long, SubEquipment> subEquipmentCacheRef, CommFaultService commFaultService, AliveTimerService aliveTimerService) {
-//    this.subEquipmentCacheRef = subEquipmentCacheRef;
-//    this.commFaultService = commFaultService;
-//    this.aliveTimerService = aliveTimerService;
-//
-//    this.supervisedService = new SupervisedServiceImpl<>(subEquipmentCacheRef, aliveTimerService);
-//    this.abstractEquipmentService = new CoreAbstractEquipmentService<>(subEquipmentCacheRef, commFaultService);
-//  }
+  public SubEquipmentService() {
+  }
+
+  @Autowired
+  public SubEquipmentService(C2monCache<SubEquipment> subEquipmentCacheRef, CommFaultService commFaultService, AliveTimerService aliveTimerService) {
+    this.subEquipmentCacheRef = subEquipmentCacheRef;
+    this.commFaultService = commFaultService;
+    this.aliveTimerService = aliveTimerService;
+
+    this.supervisedService = new SupervisedServiceImpl<>(subEquipmentCacheRef, aliveTimerService);
+    this.abstractEquipmentService = new CoreAbstractEquipmentService<>(subEquipmentCacheRef, commFaultService);
+  }
 
   public C2monCache getCache() {
     return subEquipmentCacheRef;
-  }
-
-  @Override
-  public SupervisionEvent getSupervisionStatus(Long id) {
-    return supervisedService.getSupervisionStatus(id);
-  }
-
-  @Override
-  public void refreshAndNotifyCurrentSupervisionStatus(Long id) {
-    supervisedService.refreshAndNotifyCurrentSupervisionStatus(id);
-  }
-
-  @Override
-  public void start(Long id, Timestamp timestamp) {
-    supervisedService.start(id, timestamp);
-  }
-
-  @Override
-  public void start(SubEquipment supervised, Timestamp timestamp) {
-    supervisedService.start(supervised, timestamp);
-  }
-
-  @Override
-  public void stop(SubEquipment supervised, Timestamp timestamp) {
-    supervisedService.stop(supervised, timestamp);
-  }
-
-  @Override
-  public void stop(Long id, Timestamp timestamp) {
-    supervisedService.stop(id, timestamp);
-  }
-
-  @Override
-  public void resume(Long id, Timestamp timestamp, String message) {
-    supervisedService.resume(id, timestamp, message);
-  }
-
-  @Override
-  public void suspend(Long id, Timestamp timestamp, String message) {
-    supervisedService.suspend(id, timestamp, message);
-  }
-
-  @Override
-  public boolean isRunning(SubEquipment supervised) {
-    return supervisedService.isRunning(supervised);
-  }
-
-  @Override
-  public boolean isRunning(Long id) {
-    return supervisedService.isRunning(id);
-  }
-
-  @Override
-  public boolean isUncertain(SubEquipment supervised) {
-    return supervisedService.isUncertain(supervised);
-  }
-
-  @Override
-  public void removeAliveTimer(Long id) {
-    supervisedService.removeAliveTimer(id);
-  }
-
-  @Override
-  public void loadAndStartAliveTag(Long supervisedId) {
-    supervisedService.loadAndStartAliveTag(supervisedId);
-  }
-
-  @Override
-  public void removeAliveDirectly(Long aliveId) {
-    supervisedService.removeAliveDirectly(aliveId);
-  }
-
-  @Override
-  public SupervisionConstants.SupervisionEntity getSupervisionEntity() {
-    return SupervisionConstants.SupervisionEntity.SUBEQUIPMENT;
-  }
-
-  @Override
-  public Long getProcessIdForAbstractEquipment(Long abstractEquipmentId) {
-    return abstractEquipmentService.getProcessIdForAbstractEquipment(abstractEquipmentId);
-  }
-
-  @Override
-  public Map<Long, Long> getAbstractEquipmentControlTags() {
-    return abstractEquipmentService.getAbstractEquipmentControlTags();
-  }
-
-  @Override
-  public void removeCommFault(Long abstractEquipmentId) {
-    abstractEquipmentService.removeCommFault(abstractEquipmentId);
   }
 }
