60dd79b41199906ca5d8881f6d6bef44c4beea9b
Matthias Braeger <Matthias.Braeger@cern.ch>
2019-11-04 12:21:43 +0100

    CM-232: Fix tests after refactoring

--- c2mon-client/c2mon-client-core/src/test/java/cern/c2mon/client/core/elasticsearch/ElasticsearchServiceTest.java
+++ c2mon-client/c2mon-client-core/src/test/java/cern/c2mon/client/core/elasticsearch/ElasticsearchServiceTest.java
@@ -54,13 +54,13 @@ public class ElasticsearchServiceTest extends BaseElasticsearchIntegrationTest {
   public void before() throws Exception {
     properties.getElasticsearch().setUrl("http://localhost:" + client.getClient().settings().get("http.port"));
     log.info("Connecting Elasticsearch HTTP client to {}", properties.getElasticsearch().getUrl());
-    service = new ElasticsearchService(properties);
+    service = new ElasticsearchService(properties, "c2mon");
     addDataTags();
   }
 
   @After
   public void cleanup() throws Exception {
-    DataTagCacheObject tag = (DataTagCacheObject) EntityUtils.createDataTag1();
+    DataTagCacheObject tag = EntityUtils.createDataTag1();
 
     TagConfigDocument document = converter.convert(tag)
             .orElseThrow(()->new Exception("Tag conversion failed"));
@@ -72,10 +72,10 @@ public class ElasticsearchServiceTest extends BaseElasticsearchIntegrationTest {
   }
 
   private void addDataTags() throws Exception {
-    DataTagCacheObject tag1 = (DataTagCacheObject) EntityUtils.createDataTag1();
-    DataTagCacheObject tag2 = (DataTagCacheObject) EntityUtils.createDataTag2();
+    DataTagCacheObject tag1 = EntityUtils.createDataTag1();
+    DataTagCacheObject tag2 = EntityUtils.createDataTag2();
 
-    TagConfigDocument document = converter.convert(tag1)
+    TagConfigDocument document = converter.convert(tag1, EntityUtils.createAlarmsForTag(tag1.getId()))
             .orElseThrow(()->new Exception("Tag conversion failed"));
     indexer.indexTagConfig(document);
 
@@ -100,37 +100,61 @@ public class ElasticsearchServiceTest extends BaseElasticsearchIntegrationTest {
   }
 
   @Test
-  public void testSearchByMetadata() throws Exception {
-    Collection<Long> tagsForResponsibleUser = service.findByMetadata("responsiblePerson", "Fred");
+  public void testFindTagsByMetadata() throws Exception {
+    Collection<Long> tagsForResponsibleUser = service.findTagsByMetadata("responsiblePerson", "Fred");
     assertEquals("There should be one tag with responsible user set to requested value", 1, tagsForResponsibleUser.size());
     assertEquals(Long.valueOf(1L), tagsForResponsibleUser.stream().findFirst().get());
 
-    Collection<Long> tagIds = service.findByMetadata("responsiblePerson");
+    Collection<Long> tagIds = service.findTagsByMetadata("responsiblePerson");
     assertEquals("There should be 2 tags with that metadata", 2, tagIds.size());
     assertTrue("Should contain that tag id", tagIds.contains(EntityUtils.createDataTag1().getId()));
     assertTrue("Should contain that tag id", tagIds.contains(EntityUtils.createDataTag2().getId()));
   }
 
   @Test
-  public void testGetDistinctMetadataKeys() throws Exception {
-    ElasticsearchService service = new ElasticsearchService(properties);
-    assertEquals("There should be 4 distinct metadata keys", 4, service.getDistinctMetadataKeys().size());
+  public void testFindTagsByAlarmMetadata() throws Exception {
+    Collection<Long> tagIds = service.findTagsByAlarmMetadata("meta2");
+    assertEquals("There should be 1 tag with an alarm with that metadata", 1, tagIds.size());
+    assertTrue("Should contain that tag id", tagIds.contains(EntityUtils.createDataTag1().getId()));
+
+    tagIds = service.findTagsByAlarmMetadata("meta5");
+    assertEquals("There should be 1 tag with an alarm with that metadata", 1, tagIds.size());
+    assertTrue("Should contain that tag id", tagIds.contains(EntityUtils.createDataTag1().getId()));
+
+    tagIds = service.findTagsByAlarmMetadata("metaDoesNotExist");
+    assertEquals("There should be no tag with that alarm metadata", 0, tagIds.size());
+
+    tagIds = service.findTagsByAlarmMetadata("meta6", "value6");
+    assertEquals("There should be 1 tag with an alarm with that metadata", 1, tagIds.size());
+    assertTrue("Should contain that tag id", tagIds.contains(EntityUtils.createDataTag1().getId()));
+
+    tagIds = service.findTagsByAlarmMetadata("meta1", "1");
+    assertEquals("There should be 1 tag with an alarm with that metadata", 1, tagIds.size());
+    assertTrue("Should contain that tag id", tagIds.contains(EntityUtils.createDataTag1().getId()));
+
+    tagIds = service.findTagsByAlarmMetadata("meta1", "DoesNotExist");
+    assertEquals("There should be no tag with that alarm metadata", 0, tagIds.size());
   }
 
   @Test
-  public void testSearchByNameAndMetadata() throws Exception {
+  public void testGetDistinctTagsMetadataKeys() throws Exception {
+    assertEquals("There should be 4 distinct metadata keys", 4, service.getDistinctTagMetadataKeys().size());
+  }
+
+  @Test
+  public void testFindTagsByNameAndMetadata() throws Exception {
     Collection<Long> tagIds = service.findTagsByNameAndMetadata("cpu.*", "building", "2");
     assertEquals("There should be one tag with given name and metadata", 1, tagIds.size());
     assertEquals("Tag with id 2L should match", Long.valueOf(2L), tagIds.stream().findFirst().get());
   }
 
   @Test
-  public void testSearchByName() throws Exception {
-    Collection<Long> tagsForResponsibleUser = service.findByName(EntityUtils.createDataTag1().getName());
+  public void testFindTagsByName() throws Exception {
+    Collection<Long> tagsForResponsibleUser = service.findTagsByName(EntityUtils.createDataTag1().getName());
     assertNotNull("The tags collection should not be null", tagsForResponsibleUser);
     assertEquals("The search result should be 1", 1, tagsForResponsibleUser.size());
 
-    tagsForResponsibleUser = service.findByName("cpu.*");
+    tagsForResponsibleUser = service.findTagsByName("cpu.*");
     assertNotNull("The tags collection should not be null", tagsForResponsibleUser);
     assertEquals("The search result should be 2", 2, tagsForResponsibleUser.size());
   }
