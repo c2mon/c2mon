3940848a454b3878026f84c483e97dd23d10949d
Alexander Papageorgiou Koufidis <alexander.papageorgiou@cern.ch>
2019-12-05 16:30:59 +0100

    #39 Merge DeviceConfigTransactedImpl and DeviceConfigHandlerImpl

--- c2mon-server/c2mon-server-configuration/src/main/java/cern/c2mon/server/configuration/handler/transacted/DeviceConfigTransactedImpl.java
+++ c2mon-server/c2mon-server-configuration/src/main/java/cern/c2mon/server/configuration/handler/transacted/DeviceConfigTransactedImpl.java
@@ -16,13 +16,11 @@
  *****************************************************************************/
 package cern.c2mon.server.configuration.handler.transacted;
 
-import cern.c2mon.server.cache.DeviceCache;
-import cern.c2mon.server.cache.DeviceClassCache;
-import cern.c2mon.server.cache.DeviceFacade;
-import cern.c2mon.server.cache.exception.CacheElementNotFoundException;
+import cern.c2mon.cache.actions.device.DeviceCacheObjectFactory;
+import cern.c2mon.cache.api.C2monCache;
 import cern.c2mon.server.cache.loading.DeviceDAO;
 import cern.c2mon.server.common.device.Device;
-import cern.c2mon.server.common.device.DeviceCacheObject;
+import cern.c2mon.server.common.device.DeviceClass;
 import cern.c2mon.server.configuration.handler.DeviceConfigHandler;
 import cern.c2mon.server.configuration.impl.ProcessChange;
 import cern.c2mon.shared.client.configuration.ConfigurationElement;
@@ -44,116 +42,70 @@ import java.util.Properties;
 @Slf4j
 public class DeviceConfigTransactedImpl implements DeviceConfigHandler {
 
-  /**
-   * Reference to the Device cache.
-   */
-  private DeviceCache deviceCache;
+  private C2monCache<Device> deviceCache;
 
-  /**
-   * Reference to the Device facade bean.
-   */
-  private DeviceFacade deviceFacade;
-
-  /**
-   * Reference to the Device DAO bean.
-   */
   private DeviceDAO deviceDAO;
 
-  /**
-   * Reference to the DeviceClass cache.
-   */
-  private DeviceClassCache deviceClassCache;
+  private DeviceCacheObjectFactory deviceCacheObjectFactory;
+
+  private C2monCache<DeviceClass> deviceClassCache;
 
   /**
    * Default constructor.
-   *
    * @param deviceCache reference to the Device cache.
-   * @param deviceFacade reference to he Device facade bean.
-   * @param deviceDAO reference to the Device DAO bean.
+   * @param deviceDAO
+   * @param deviceCacheObjectFactory reference to the Device DAO bean.
    * @param deviceClassCache reference to the DeviceClass cache.
    */
   @Autowired
-  public DeviceConfigTransactedImpl(final DeviceCache deviceCache,
-                                    final DeviceFacade deviceFacade,
-                                    final DeviceDAO deviceDAO,
-                                    final DeviceClassCache deviceClassCache) {
+  public DeviceConfigTransactedImpl(final C2monCache<Device> deviceCache,
+                                    final DeviceDAO deviceDAO, final DeviceCacheObjectFactory deviceCacheObjectFactory,
+                                    final C2monCache<DeviceClass> deviceClassCache) {
     this.deviceCache = deviceCache;
-    this.deviceFacade = deviceFacade;
     this.deviceDAO = deviceDAO;
+    this.deviceCacheObjectFactory = deviceCacheObjectFactory;
     this.deviceClassCache = deviceClassCache;
   }
 
   @Override
   @Transactional(value = "cacheTransactionManager")
-  public ProcessChange create(ConfigurationElement element) throws IllegalAccessException {
-    deviceCache.acquireWriteLockOnKey(element.getEntityId());
+  public ProcessChange create(ConfigurationElement element) {
+    log.trace("Creating Device " + element.getEntityId());
 
-    try {
-      log.trace("Creating Device " + element.getEntityId());
-
-      // Check if the device class already exists
-      if (deviceCache.hasKey(element.getEntityId())) {
-        throw new ConfigurationException(ConfigurationException.ENTITY_EXISTS, "Attempting to create a Device with an already existing id: "
-            + element.getEntityId());
-      }
-
-      // Create the cache object
-      Device device = deviceFacade.createCacheObject(element.getEntityId(), element.getElementProperties());
-
-      // Insert the device into the DB
-      try {
-        deviceDAO.insert(device);
-
-      } catch (Exception e) {
-        log.error("Exception caught while inserting a new Device into the DB - rolling back changes", e);
-        throw new UnexpectedRollbackException("Unexpected exception while creating a Device: rolling back the change", e);
-      }
-
-      // Insert the device into the cache
-      try {
-        deviceCache.putQuiet(device);
-
-        // Update the device class so that it knows about the new device
-        deviceClassCache.updateDeviceIds(device.getDeviceClassId());
-
-        return new ProcessChange();
-
-      } catch (Exception e) {
-        deviceCache.remove(device.getId());
-        log.error("Exception caught when attempting to create a Device - rolling back the DB transaction and undoing cache changes.");
-        throw new UnexpectedRollbackException("Unexpected exception while creating a Device: rolling back the change", e);
-      }
-
-    } finally {
-      deviceCache.releaseWriteLockOnKey(element.getEntityId());
+    if (deviceCache.containsKey(element.getEntityId())) {
+      throw new ConfigurationException(ConfigurationException.ENTITY_EXISTS, "Attempting to create a Device with an already existing id: "
+        + element.getEntityId());
     }
+
+    Device device = deviceCacheObjectFactory.createCacheObject(element.getEntityId(), element.getElementProperties());
+
+    // DB insert
+    deviceDAO.insert(device);
+
+    // Cache insert
+    deviceCache.putQuiet(device.getId(), device);
+
+    // Update the device class so that it knows about the new device
+    deviceClassCache.loadFromDb(device.getDeviceClassId());
+
+    return new ProcessChange();
   }
 
   @Override
   @Transactional(value = "cacheTransactionManager", propagation = Propagation.REQUIRES_NEW)
   public ProcessChange update(Long id, Properties properties) {
-    deviceCache.acquireWriteLockOnKey(id);
+    log.trace("Updating Device " + id);
 
-    try {
-      log.trace("Updating Device " + id);
+    Device device = deviceCache.get(id);
 
-      Device device = deviceCache.get(id);
-      deviceDAO.updateConfig(device);
-      deviceFacade.updateConfig(device, properties);
+    deviceCacheObjectFactory.updateConfig(device, properties);
 
-      // No event for DAQ layer
-      return new ProcessChange();
+    deviceDAO.updateConfig(device);
 
-    } catch (CacheElementNotFoundException e) {
-      throw e;
+    deviceCache.put(id, device);
 
-    } catch (Exception e) {
-      log.error("Exception caught while updating a Device - rolling back DB transaction", e);
-      throw new UnexpectedRollbackException("Unexpected exception caught while updating a Device configuration", e);
-
-    } finally {
-      deviceCache.releaseWriteLockOnKey(id);
-    }
+    // No event for DAQ layer
+    return new ProcessChange();
   }
 
   @Override
@@ -161,30 +113,25 @@ public class DeviceConfigTransactedImpl implements DeviceConfigHandler {
   public ProcessChange remove(Long id, ConfigurationElementReport elementReport) {
     log.trace("Removing Device " + id);
 
-    try {
-      deviceCache.acquireWriteLockOnKey(id);
-      DeviceCacheObject device = (DeviceCacheObject) deviceCache.get(id);
-
+    if (deviceCache.containsKey(id)) {
       try {
-        deviceDAO.deleteItem(device.getId());
-        return new ProcessChange();
+        deviceDAO.deleteItem(id);
 
-      } catch (Exception e) {
+        deviceCache.remove(id);
+
+      } catch (RuntimeException e) {
         log.error("Exception caught while removing a Device.", e);
         elementReport.setFailure("Unable to remove Device with id " + id);
         throw new UnexpectedRollbackException("Unable to remove Device " + id, e);
       }
-
-    } catch (CacheElementNotFoundException e) {
+    }
+    else {
       log.warn("Attempting to remove a non-existent Device - no action taken.");
       elementReport.setWarning("Attempting to remove a non-existent Device");
       return new ProcessChange();
-
-    } finally {
-      if (deviceCache.isWriteLockedByCurrentThread(id)) {
-        deviceCache.releaseWriteLockOnKey(id);
-      }
     }
+
+    return new ProcessChange();
   }
 
 }
