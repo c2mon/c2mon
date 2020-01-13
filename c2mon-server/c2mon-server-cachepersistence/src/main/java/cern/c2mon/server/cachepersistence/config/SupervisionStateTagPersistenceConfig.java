b7cc18773a2aa24567a198989bee710a97a2fbc2
Alexandros Papageorgiou <alkoclick@gmail.com>
2020-01-13 15:56:03 +0100

    #39 Refactor SupervisionStateTag interface into concrete class

---
+++ c2mon-server/c2mon-server-cachepersistence/src/main/java/cern/c2mon/server/cachepersistence/config/SupervisionStateTagPersistenceConfig.java
@@ -0,0 +1,22 @@
+package cern.c2mon.server.cachepersistence.config;
+
+import cern.c2mon.cache.api.C2monCache;
+import cern.c2mon.server.common.alive.AliveTag;
+import cern.c2mon.server.common.status.SupervisionStateTag;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+
+/**
+ * @author Alexandros Papageorgiou
+ */
+@Service
+public class SupervisionStateTagPersistenceConfig extends AbstractPersistenceConfig<SupervisionStateTag> {
+
+  // TODO (Alex) Complete this when control tag refactoring is through
+
+  @Inject
+  public SupervisionStateTagPersistenceConfig(final C2monCache<SupervisionStateTag> supervisionStateTagC2monCache) {
+    super(supervisionStateTagC2monCache, null);
+  }
+}
--- c2mon-server/c2mon-server-common/src/main/java/cern/c2mon/server/common/status/SupervisionStateTag.java
+++ c2mon-server/c2mon-server-common/src/main/java/cern/c2mon/server/common/status/SupervisionStateTag.java
@@ -1,12 +1,18 @@
 package cern.c2mon.server.common.status;
 
+import cern.c2mon.server.common.AbstractCacheableImpl;
 import cern.c2mon.server.common.alive.AliveTag;
 import cern.c2mon.server.common.commfault.CommFaultTag;
 import cern.c2mon.server.common.supervision.Supervised;
-import cern.c2mon.shared.common.Cacheable;
 import cern.c2mon.shared.common.supervision.SupervisionConstants;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Timestamp;
+
+import static cern.c2mon.shared.common.supervision.SupervisionConstants.*;
 
 /**
  * Expresses the current situation of a {@link Supervised} object
@@ -14,40 +20,48 @@ import java.sql.Timestamp;
  * @author Alexandros Papageorgiou, Brice Copy
  * @apiNote <a href=https://stackoverflow.com/questions/1162816/naming-conventions-state-versus-status>State vs Status discussion for technical jargon naming</a>
  */
-public interface SupervisionStateTag extends Cacheable {
+@Slf4j
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class SupervisionStateTag extends AbstractCacheableImpl {
 
-  /**
-   * Id of the {@link Supervised} object
-   */
-  long getSupervisedId();
+    /**
+     * Id of the {@link Supervised} object
+     */
+    final long supervisedId;
 
-  /**
-   * Type of the {@link Supervised} object
-   */
-  SupervisionConstants.SupervisionEntity getSupervisedEntity();
+    /**
+     * Type of the {@link Supervised} object
+     */
+    final SupervisionEntity supervisedEntity;
 
-  /**
-   * Supervision status of this object when it was recovered from cache
-   */
-  SupervisionConstants.SupervisionStatus getSupervisionStatus();
+    /**
+     * Supervision status of this object when it was recovered from cache
+     */
+    final SupervisionStatus supervisionStatus;
 
-  /**
-   * Reason/description of the current status, or empty
-   */
-  String getStatusDescription();
+    /**
+     * Reason/description of the current status, or empty
+     */
+    final String statusDescription;
 
-  /**
-   * Time when this supervision status was last confirmed
-   */
-  Timestamp getStatusTime();
+    /**STOPPED
+     * Time when this supervision status was last confirmed
+     */
+    final Timestamp statusTime;
 
-  /**
-   * Id of the associated {@link AliveTag} used for supervision, or null
-   */
-  Long getAliveTagId();
+    /**
+     * Id of the associated {@link AliveTag} used for supervision, or null
+     */
+    final Long aliveTagId;
 
-  /**
-   * Id of the associated {@link CommFaultTag} used for supervision, or null
-   */
-  Long getCommFaultTagId();
+    /**
+     * Id of the associated {@link CommFaultTag} used for supervision, or null
+     */
+    final Long commFaultTagId;
+
+    public SupervisionStateTag() {
+        this(0L, SupervisionEntity.EQUIPMENT, SupervisionStatus.DOWN, "", new Timestamp(0L), null, null);
+    }
 }
