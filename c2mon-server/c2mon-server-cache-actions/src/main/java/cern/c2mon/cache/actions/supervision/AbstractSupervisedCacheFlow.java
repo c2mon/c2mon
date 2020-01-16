e6acc2531aa5256c16e921ddbc19abd915005fd3
Alexander Papageorgiou Koufidis <alexander.papageorgiou@cern.ch>
2020-01-16 17:50:22 +0100

    #39 Remove postInsert supervised events

--- c2mon-server/c2mon-server-cache-actions/src/main/java/cern/c2mon/cache/actions/supervision/AbstractSupervisedCacheFlow.java
+++ c2mon-server/c2mon-server-cache-actions/src/main/java/cern/c2mon/cache/actions/supervision/AbstractSupervisedCacheFlow.java
@@ -2,11 +2,6 @@ package cern.c2mon.cache.actions.supervision;
 
 import cern.c2mon.cache.api.flow.DefaultCacheFlow;
 import cern.c2mon.server.common.supervision.Supervised;
-import cern.c2mon.shared.common.CacheEvent;
-import lombok.NonNull;
-import org.springframework.lang.Nullable;
-
-import java.util.Set;
 
 public class AbstractSupervisedCacheFlow<T extends Supervised> extends DefaultCacheFlow<T> {
 
@@ -14,19 +9,5 @@ public class AbstractSupervisedCacheFlow<T extends Supervised> extends DefaultCa
     super((older, newer) ->
       (older.getStatusTime() == null || newer.getStatusTime() == null)
         || newer.getStatusTime().getTime() >= older.getStatusTime().getTime());
-  }
-
-  @Override
-  public Set<CacheEvent> postInsertEvents(@Nullable T older, @NonNull T newer) {
-    Set<CacheEvent> events = super.postInsertEvents(older, newer);
-    events.add(CacheEvent.SUPERVISION_UPDATE);
-
-    if (older != null) {
-      if (older.isRunning() != newer.isRunning())
-        events.add(CacheEvent.SUPERVISION_CHANGE);
-    } else {
-      events.add(CacheEvent.SUPERVISION_CHANGE);
-    }
-    return events;
   }
 }
