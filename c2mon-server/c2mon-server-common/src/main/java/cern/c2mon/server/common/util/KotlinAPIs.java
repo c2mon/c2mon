68958576dbee7940b335bc768b0a492fd7f3d834
Alexander Papageorgiou Koufidis <alexander.papageorgiou@cern.ch>
2020-01-17 15:21:37 +0100

    #39 Add let to KotlinAPIs

--- c2mon-server/c2mon-server-common/src/main/java/cern/c2mon/server/common/util/KotlinAPIs.java
+++ c2mon-server/c2mon-server-common/src/main/java/cern/c2mon/server/common/util/KotlinAPIs.java
@@ -1,6 +1,7 @@
 package cern.c2mon.server.common.util;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * This class provides some of the APIs of Kotlin for quality of life in development
@@ -15,8 +16,20 @@ public final class KotlinAPIs {
 
   }
 
+  /**
+   * Takes the object as argument, applies the function on it, then returns the original object
+   *
+   * example: list.add( apply(obj, System.out::println) )
+   */
   public static <T> T apply(T object, Consumer<T> applyFunction) {
     applyFunction.accept(object);
     return object;
+  }
+
+  /**
+   * Takes the object as argument, then returns whatever the transformer function returns
+   */
+  public static <OUT, IN> OUT let(IN object, Function<IN,OUT> transformer) {
+    return transformer.apply(object);
   }
 }
