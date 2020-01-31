9dd2440242c4f1f26d96bcb2cd4a1a72d2031ce1
Alexander Papageorgiou Koufidis <alexander.papageorgiou@cern.ch>
2020-01-31 12:16:42 +0100

    #39 Make CachePopRule collect the caches into lists

--- c2mon-server/c2mon-server-test/src/main/java/cern/c2mon/server/test/CachePopulationRule.java
+++ c2mon-server/c2mon-server-test/src/main/java/cern/c2mon/server/test/CachePopulationRule.java
@@ -12,10 +12,12 @@ import cern.c2mon.server.common.process.Process;
 import cern.c2mon.server.common.rule.RuleTag;
 import cern.c2mon.server.common.subequipment.SubEquipment;
 import cern.c2mon.server.common.supervision.SupervisionStateTag;
+import cern.c2mon.shared.common.Cacheable;
 import cern.c2mon.shared.common.command.CommandTag;
 
 import javax.inject.Named;
 import java.sql.SQLException;
+import java.util.List;
 
 import static cern.c2mon.server.common.util.Java9Collections.listOf;
 
@@ -23,23 +25,12 @@ import static cern.c2mon.server.common.util.Java9Collections.listOf;
  * Using this rule in a JUnit test will ensure that all caches are preloaded
  * cleanly with test data at the start of each test.
  *
- * @author Justin Lewis Salmon
+ * @author Alexandros Papageorgiou, Justin Lewis Salmon
  */
 @Named
 public class CachePopulationRule extends DatabasePopulationRule {
 
-  private C2monCache<Alarm> alarmCache;
-  private C2monCache<AliveTag> aliveTimerCache;
-  private C2monCache<CommandTag> commandTagCache;
-  private C2monCache<CommFaultTag> commFaultTagCache;
-  private C2monCache<DataTag> dataTagCache;
-  private C2monCache<Device> deviceCache;
-  private C2monCache<DeviceClass> deviceClassCache;
-  private C2monCache<Equipment> equipmentCache;
-  private C2monCache<Process> processCache;
-  private C2monCache<RuleTag> ruleTagCache;
-  private C2monCache<SubEquipment> subEquipmentCache;
-  private C2monCache<SupervisionStateTag> stateTagCache;
+  private List<C2monCache<? extends Cacheable>> allCaches;
 
   public CachePopulationRule(C2monCache<Alarm> alarmCache, C2monCache<AliveTag> aliveTimerCache,
                              C2monCache<CommandTag> commandTagCache, C2monCache<CommFaultTag> commFaultTagCache,
@@ -47,30 +38,16 @@ public class CachePopulationRule extends DatabasePopulationRule {
                              C2monCache<DeviceClass> deviceClassCache, C2monCache<Equipment> equipmentCache,
                              C2monCache<Process> processCache, C2monCache<RuleTag> ruleTagCache,
                              C2monCache<SubEquipment> subEquipmentCache, C2monCache<SupervisionStateTag> stateTagCache) {
-    this.alarmCache = alarmCache;
-    this.aliveTimerCache = aliveTimerCache;
-    this.commandTagCache = commandTagCache;
-    this.commFaultTagCache = commFaultTagCache;
-    this.dataTagCache = dataTagCache;
-    this.deviceCache = deviceCache;
-    this.deviceClassCache = deviceClassCache;
-    this.equipmentCache = equipmentCache;
-    this.processCache = processCache;
-    this.ruleTagCache = ruleTagCache;
-    this.subEquipmentCache = subEquipmentCache;
-    this.stateTagCache = stateTagCache;
+    allCaches = listOf(
+      alarmCache, aliveTimerCache, commandTagCache, commFaultTagCache, dataTagCache, deviceCache, deviceClassCache,
+      equipmentCache, processCache, ruleTagCache, subEquipmentCache, stateTagCache
+    );
   }
 
 
   @Override
   protected void before() throws SQLException {
     super.before();
-    listOf(
-      deviceCache, deviceClassCache,
-      subEquipmentCache, equipmentCache, processCache,
-      commFaultTagCache, aliveTimerCache, stateTagCache,
-      alarmCache,
-      ruleTagCache, dataTagCache, commandTagCache
-    ).forEach(C2monCache::init);
+    allCaches.forEach(C2monCache::init);
   }
 }
