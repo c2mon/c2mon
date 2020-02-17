62e0fefa10610dd71abfae0147e15f6d552f9c09
Alexander Papageorgiou Koufidis <alexander.papageorgiou@cern.ch>
2020-02-17 16:34:15 +0100

    #39 Drop a lombok logger annot and use declaration instead

--- c2mon-server/c2mon-server-supervision/src/main/java/cern/c2mon/server/supervision/impl/event/AliveTagEvents.java
+++ c2mon-server/c2mon-server-supervision/src/main/java/cern/c2mon/server/supervision/impl/event/AliveTagEvents.java
@@ -3,16 +3,17 @@ package cern.c2mon.server.supervision.impl.event;
 import cern.c2mon.cache.actions.alive.AliveTagService;
 import cern.c2mon.server.common.alive.AliveTag;
 import cern.c2mon.shared.common.supervision.SupervisionEntity;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
-@Slf4j
 @Named
 @Singleton
 public class AliveTagEvents {
+  private static final Logger LOG = LoggerFactory.getLogger(AliveTagEvents.class);
 
   private final AliveTagService aliveTimerService;
 
@@ -27,19 +28,19 @@ public class AliveTagEvents {
    */
   public void onAliveTimerExpiration(final Long aliveTimerId) {
     if (aliveTimerId == null) {
-      log.warn("onAliveTimerExpiration(null) called - ignoring the call.");
+      LOG.warn("onAliveTimerExpiration(null) called - ignoring the call.");
       return;
     }
 
     if (!aliveTimerService.isRegisteredAliveTimer(aliveTimerId)) {
-      log.error("AliveTimer received does not exist in cache - unable to take any action on alive reception.");
+      LOG.error("AliveTimer received does not exist in cache - unable to take any action on alive reception.");
       return;
     }
 
     AliveTag aliveTimer = aliveTimerService.getCache().get(aliveTimerId);
 
-    log.debug("Alive of " + aliveTimer.getSupervisedEntity().toString() + " " + aliveTimer.getSupervisedName()
-      + " (alive tag: " + aliveTimer.getId() + ") has expired.");
+    LOG.debug("Alive of {} {} (alive tag: {}) has expired.",
+      aliveTimer.getSupervisedEntity(),  aliveTimer.getSupervisedName(), aliveTimer.getId());
 
     SupervisionEntity supervisedEntity = aliveTimer.getSupervisedEntity();
 
