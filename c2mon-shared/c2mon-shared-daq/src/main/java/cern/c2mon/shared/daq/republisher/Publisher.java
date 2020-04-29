0e1a0876cc6a28bcacf7b530d9c9f19a2ed15fb5
Matthias Braeger <Matthias.Braeger@cern.ch>
2020-04-29 13:29:29 +0200

    CM-256: Move Publisher interface to c2mon-daq-shared

--- c2mon-shared/c2mon-shared-daq/src/main/java/cern/c2mon/shared/daq/republisher/Publisher.java
+++ c2mon-shared/c2mon-shared-daq/src/main/java/cern/c2mon/shared/daq/republisher/Publisher.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
-package cern.c2mon.server.common.republisher;
+package cern.c2mon.shared.daq.republisher;
 
 import org.springframework.jms.JmsException;
 
