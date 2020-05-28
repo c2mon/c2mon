0c4547d5ecfb6f6485672a76334fd95ea35d7dc8
danielma <daniel.martin.anido@cern.ch>
2020-05-28 14:28:57 +0200

    #39 Update ElasticSearch dependents in c2mon-client-core

--- c2mon-client/c2mon-client-core/src/test/java/cern/c2mon/client/core/elasticsearch/ElasticsearchServiceTest.java
+++ c2mon-client/c2mon-client-core/src/test/java/cern/c2mon/client/core/elasticsearch/ElasticsearchServiceTest.java
@@ -53,7 +53,7 @@ public class ElasticsearchServiceTest {
 
   public ElasticsearchServiceTest() {
     client = new ElasticsearchClientRest(elasticsearchProperties);
-    esTestClient = new ElasticsearchTestClient(client);
+    esTestClient = new ElasticsearchTestClient(elasticsearchProperties);
     indexManager = new IndexManager(client);
     ProcessCache processCache = createNiceMock(ProcessCache.class);
     EquipmentCache equipmentCache = createNiceMock(EquipmentCache.class);
