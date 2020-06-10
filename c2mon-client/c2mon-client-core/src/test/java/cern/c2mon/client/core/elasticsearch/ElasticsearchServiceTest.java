b6f6cd8c26f3757b128022537ccf7133bdb01a7e
danielma <daniel.martin.anido@cern.ch>
2020-06-10 13:11:17 +0200

    #39 Fix ElasticSearch tests for c2mon-client-core (apply same approach as with c2mon-server-elasticsearch)

--- c2mon-client/c2mon-client-core/src/test/java/cern/c2mon/client/core/elasticsearch/ElasticsearchServiceTest.java
+++ c2mon-client/c2mon-client-core/src/test/java/cern/c2mon/client/core/elasticsearch/ElasticsearchServiceTest.java
@@ -1,81 +1,133 @@
+/******************************************************************************
+ * Copyright (C) 2010-2019 CERN. All rights not expressly granted are reserved.
+ *
+ * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
+ * C2MON is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the license.
+ *
+ * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
 package cern.c2mon.client.core.elasticsearch;
 
+import cern.c2mon.cache.actions.CacheActionsModuleRef;
+import cern.c2mon.cache.actions.alarm.AlarmService;
+import cern.c2mon.cache.config.CacheConfigModuleRef;
+import cern.c2mon.cache.config.collections.TagCacheCollection;
+import cern.c2mon.cache.impl.configuration.C2monIgniteConfiguration;
 import cern.c2mon.client.core.config.C2monClientProperties;
-import cern.c2mon.server.cache.EquipmentCache;
-import cern.c2mon.server.cache.ProcessCache;
-import cern.c2mon.server.cache.SubEquipmentCache;
-import cern.c2mon.server.cache.TagFacadeGateway;
+import cern.c2mon.server.cache.dbaccess.config.CacheDbAccessModule;
+import cern.c2mon.server.cache.loading.config.CacheLoadingModuleRef;
+import cern.c2mon.server.common.alarm.TagWithAlarms;
+import cern.c2mon.server.common.config.CommonModule;
 import cern.c2mon.server.common.datatag.DataTagCacheObject;
 import cern.c2mon.server.elasticsearch.IndexManager;
 import cern.c2mon.server.elasticsearch.MappingFactory;
-import cern.c2mon.server.elasticsearch.client.ElasticsearchClientRest;
+import cern.c2mon.server.elasticsearch.config.ElasticsearchModule;
 import cern.c2mon.server.elasticsearch.config.ElasticsearchProperties;
 import cern.c2mon.server.elasticsearch.domain.IndexMetadata;
 import cern.c2mon.server.elasticsearch.tag.config.TagConfigDocumentConverter;
 import cern.c2mon.server.elasticsearch.tag.config.TagConfigDocumentIndexer;
 import cern.c2mon.server.elasticsearch.tag.config.TagConfigDocumentListener;
+import cern.c2mon.server.elasticsearch.util.ContainerizedElasticsearchManager;
 import cern.c2mon.server.elasticsearch.util.ElasticsearchTestClient;
 import cern.c2mon.server.elasticsearch.util.EmbeddedElasticsearchManager;
+import cern.c2mon.server.supervision.config.SupervisionModule;
 import cern.c2mon.shared.client.configuration.ConfigConstants;
-import org.apache.http.annotation.NotThreadSafe;
-import org.easymock.Mock;
-import org.elasticsearch.node.NodeValidationException;
 import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.IOException;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import static java.lang.Double.doubleToLongBits;
+import static java.util.Collections.emptyList;
 import static org.easymock.EasyMock.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-@NotThreadSafe
+/**
+ * When running in GitLab CI, these tests will connect to ElasticSearch's service
+ * as configured in gitlab-ci.yml and c2mon-server-gitlab-ci.properties. For local
+ * testing, an embedded ElasticSearch service is used by default, and can be
+ * turned off by setting {@code c2mon.server.elasticsearch.embedded=false} below,
+ * while ensuring Docker is running locally.
+ */
+@ContextConfiguration(classes = {
+  CommonModule.class,
+  CacheActionsModuleRef.class,
+  CacheConfigModuleRef.class,
+  CacheDbAccessModule.class,
+  CacheLoadingModuleRef.class,
+  SupervisionModule.class,
+  ElasticsearchModule.class,
+  C2monIgniteConfiguration.class
+})
+@RunWith(SpringRunner.class)
+@TestPropertySource(properties = {
+  // "c2mon.server.elasticsearch.embedded=false"
+})
 public class ElasticsearchServiceTest {
 
-  private static ElasticsearchProperties elasticsearchProperties = new ElasticsearchProperties();
+  @Autowired
+  private ElasticsearchProperties elasticsearchProperties;
 
-  private TagConfigDocumentListener tagDocumentListener;
-  private C2monClientProperties properties = new C2monClientProperties();
-  private ElasticsearchClientRest client;
+  @Autowired
   private ElasticsearchTestClient esTestClient;
+
+  @Autowired
   private IndexManager indexManager;
 
-  @Mock
-  private TagFacadeGateway tagFacadeGateway;
+  @Autowired
+  private TagConfigDocumentIndexer indexer;
 
-  public ElasticsearchServiceTest() {
-    client = new ElasticsearchClientRest(elasticsearchProperties);
-    esTestClient = new ElasticsearchTestClient(client);
-    indexManager = new IndexManager(client);
-    ProcessCache processCache = createNiceMock(ProcessCache.class);
-    EquipmentCache equipmentCache = createNiceMock(EquipmentCache.class);
-    SubEquipmentCache subequipmentCache = createNiceMock(SubEquipmentCache.class);
-    TagConfigDocumentConverter converter = new TagConfigDocumentConverter(processCache, equipmentCache, subequipmentCache);
-    tagFacadeGateway = createNiceMock(TagFacadeGateway.class);
-    TagConfigDocumentIndexer indexer = new TagConfigDocumentIndexer(elasticsearchProperties, indexManager, tagFacadeGateway, converter);
-    tagDocumentListener = new TagConfigDocumentListener(elasticsearchProperties, indexer, converter, tagFacadeGateway);
-  }
+  @Autowired
+  private TagConfigDocumentConverter converter;
 
-  @BeforeClass
-  public static void setUpClass() throws IOException, InterruptedException {
-    EmbeddedElasticsearchManager.start(elasticsearchProperties);
+  @Autowired
+  private TagCacheCollection tagCacheFacade;
+
+  private final ElasticsearchService esService = new ElasticsearchService(new C2monClientProperties(), "c2mon");
+
+  private AlarmService alarmService;
+
+  private TagConfigDocumentListener tagDocumentListener;
+
+  @Before
+  public void setUp() {
+    alarmService = createNiceMock(AlarmService.class);
+    tagDocumentListener = new TagConfigDocumentListener(elasticsearchProperties, indexer, converter, tagCacheFacade, alarmService);
+
+    if (elasticsearchProperties.isEmbedded()) {
+      EmbeddedElasticsearchManager.start(elasticsearchProperties);
+    } else {
+      ContainerizedElasticsearchManager.start(elasticsearchProperties);
+    }
   }
 
   @AfterClass
   public static void tearDownClass() {
     EmbeddedElasticsearchManager.stop();
+    ContainerizedElasticsearchManager.stop();
   }
 
   @Before
-  public void setupElasticsearch() throws InterruptedException, NodeValidationException {
+  public void setupElasticsearch() throws InterruptedException {
     try {
       CompletableFuture<Void> nodeReady = CompletableFuture.runAsync(() -> {
         esTestClient.deleteIndex(elasticsearchProperties.getTagConfigIndex());
@@ -95,21 +147,21 @@ public class ElasticsearchServiceTest {
 
   @Before
   public void resetMocks() {
-    reset(tagFacadeGateway);
+    reset(alarmService);
   }
 
   @Test
   public void testSearchByMetadata() throws InterruptedException {
     try {
-      Long testUserTagId = Double.doubleToLongBits(Math.random()) % 10000;
-      String testUser = Long.toHexString(Double.doubleToLongBits(Math.random()));
+      Long testUserTagId = doubleToLongBits(Math.random()) % 10000;
+      String testUser = Long.toHexString(doubleToLongBits(Math.random()));
       String responsible = "responsible";
       DataTagCacheObject tag = new DataTagCacheObject(testUserTagId);
       tag.getMetadata().getMetadata().put(responsible, testUser);
-      expect(tagFacadeGateway.getAlarms(anyObject(DataTagCacheObject.class))).andReturn(Collections.emptyList()).times(2);
-      replay(tagFacadeGateway);
+      expect(alarmService.getTagWithAlarmsAtomically(anyLong())).andReturn(new TagWithAlarms<>(tag, emptyList())).times(2);
+      replay(alarmService);
       tagDocumentListener.onConfigurationEvent(tag, ConfigConstants.Action.CREATE);
-      Long tag1234Id = Double.doubleToLongBits(Math.random()) % 10000;
+      Long tag1234Id = doubleToLongBits(Math.random()) % 10000;
       String value1234 = "1234";
       tag = new DataTagCacheObject(tag1234Id);
       String key1234 = "1234";
@@ -119,15 +171,13 @@ public class ElasticsearchServiceTest {
       esTestClient.refreshIndices();
       Thread.sleep(10000);
 
-      ElasticsearchService service = new ElasticsearchService(properties, "c2mon");
+      assertEquals("There should be 2 tags, one for responsible and one for 1234", 2, esService.getDistinctTagMetadataKeys().size());
 
-      assertEquals("There should be 2 tags, one for responsible and one for 1234", 2, service.getDistinctTagMetadataKeys().size());
-
-      Collection<Long> tagsForResponsibleUser = service.findTagsByMetadata(responsible, testUser);
+      Collection<Long> tagsForResponsibleUser = esService.findTagsByMetadata(responsible, testUser);
       assertEquals("There should be one tag with responsible user set to requested value", 1, tagsForResponsibleUser.size());
       assertEquals(testUserTagId, tagsForResponsibleUser.stream().findFirst().get());
 
-      Collection<Long> tags1234 = service.findTagsByMetadata(key1234, value1234);
+      Collection<Long> tags1234 = esService.findTagsByMetadata(key1234, value1234);
       assertEquals("There should be one tag with 1234 parameter set to requested value", 1, tags1234.size());
       assertEquals(tag1234Id, tags1234.stream().findFirst().get());
     } catch (Exception e) {
@@ -139,23 +189,23 @@ public class ElasticsearchServiceTest {
   @Test
   public void testSearchByNameAndMetadata() throws InterruptedException {
     try {
-      Long testUserTagId = Double.doubleToLongBits(Math.random()) % 10000;
-      String testUser = Long.toHexString(Double.doubleToLongBits(Math.random()));
+      Long testUserTagId = doubleToLongBits(Math.random()) % 10000;
+      String testUser = Long.toHexString(doubleToLongBits(Math.random()));
       String metadataKey = "metadataKey";
       DataTagCacheObject tag = new DataTagCacheObject(testUserTagId);
       String tagname = "tagname";
       tag.setName(tagname);
       tag.getMetadata().getMetadata().put(metadataKey, testUser);
-      expect(tagFacadeGateway.getAlarms(anyObject(DataTagCacheObject.class))).andReturn(Collections.emptyList()).times(3);
-      replay(tagFacadeGateway);
+      expect(alarmService.getTagWithAlarmsAtomically(anyLong())).andReturn(new TagWithAlarms<>(tag, emptyList())).times(3);
+      replay(alarmService);
       tagDocumentListener.onConfigurationEvent(tag, ConfigConstants.Action.CREATE);
 
-      tag = new DataTagCacheObject(Double.doubleToLongBits(Math.random()) % 10000);
+      tag = new DataTagCacheObject(doubleToLongBits(Math.random()) % 10000);
       tag.setName(tagname);
       tag.getMetadata().getMetadata().put(metadataKey, "some other metadata value");
       tagDocumentListener.onConfigurationEvent(tag, ConfigConstants.Action.CREATE);
 
-      tag = new DataTagCacheObject(Double.doubleToLongBits(Math.random()) % 10000);
+      tag = new DataTagCacheObject(doubleToLongBits(Math.random()) % 10000);
       tag.setName("other_tagname");
       tag.getMetadata().getMetadata().put(metadataKey, testUser);
       tagDocumentListener.onConfigurationEvent(tag, ConfigConstants.Action.CREATE);
@@ -163,9 +213,7 @@ public class ElasticsearchServiceTest {
       esTestClient.refreshIndices();
       Thread.sleep(10000);
 
-      ElasticsearchService service = new ElasticsearchService(properties, "c2mon");
-
-      Collection<Long> tagsForResponsibleUser = service.findTagsByNameAndMetadata(tagname, metadataKey, testUser);
+      Collection<Long> tagsForResponsibleUser = esService.findTagsByNameAndMetadata(tagname, metadataKey, testUser);
       assertEquals("There should be one tag with given name and metadata", 1, tagsForResponsibleUser.size());
       assertEquals(testUserTagId, tagsForResponsibleUser.stream().findFirst().get());
     } catch (Exception e) {
@@ -173,12 +221,11 @@ public class ElasticsearchServiceTest {
       throw e;
     }
   }
-  
+
   @Test
-  public void testSearchByName() throws InterruptedException {
+  public void testSearchByName() {
     try {
-      ElasticsearchService service = new ElasticsearchService(properties, "c2mon");
-      Collection<Long> tagsForResponsibleUser = service.findTagsByName("TEST");
+      Collection<Long> tagsForResponsibleUser = esService.findTagsByName("TEST");
       assertNotNull("The tags collection should not be null", tagsForResponsibleUser);
       assertEquals("There tags collection should be empty", 0, tagsForResponsibleUser.size());
     } catch (Exception e) {
