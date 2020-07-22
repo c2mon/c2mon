96f42763fdab3c847bdcb0a3c5c4f07e90c7d2ed
danielma <daniel.martin.anido@cern.ch>
2020-07-22 17:23:04 +0200

    #39 Remove fields from AbstractSupervisedCacheObject that were used only in tests

--- c2mon-server/c2mon-server-common/src/main/java/cern/c2mon/server/common/equipment/AbstractSupervisedCacheObject.java
+++ c2mon-server/c2mon-server-common/src/main/java/cern/c2mon/server/common/equipment/AbstractSupervisedCacheObject.java
@@ -18,11 +18,11 @@ package cern.c2mon.server.common.equipment;
 
 import cern.c2mon.server.common.AbstractCacheableImpl;
 import cern.c2mon.server.common.supervision.Supervised;
-import cern.c2mon.shared.common.supervision.SupervisionStatus;
-import lombok.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-
-import java.sql.Timestamp;
 
 /**
  * Common part of all cache objects that need supervising by the server. Supervision involves an alive timer, with
@@ -37,102 +37,77 @@ import java.sql.Timestamp;
 @Slf4j
 public abstract class AbstractSupervisedCacheObject extends AbstractCacheableImpl implements Supervised {
 
-    private static final long serialVersionUID = -7826198425602484249L;
+  private static final long serialVersionUID = -7826198425602484249L;
 
-    /**
-     * Default alive interval for all equipment.
-     */
-    private static final int ALIVE_DEFAULT_INTERVAL = 60000;
+  /**
+   * Default alive interval for all equipment.
+   */
+  private static final int ALIVE_DEFAULT_INTERVAL = 60000;
 
-    /**
-     * Interval in milliseconds at which the alive tag is expected to change.
-     */
-    private Integer aliveInterval = ALIVE_DEFAULT_INTERVAL;
+  /**
+   * Interval in milliseconds at which the alive tag is expected to change.
+   */
+  private Integer aliveInterval = ALIVE_DEFAULT_INTERVAL;
 
-    /**
-     * Unique name of the equipment.
-     */
-    private String name;
+  /**
+   * Unique name of the equipment.
+   */
+  private String name;
 
-    /**
-     * The current status of this supervision equipment.
-     */
-    @NonNull
-    private SupervisionStatus supervisionStatus = SupervisionStatus.DOWN;
+  /**
+   * Identifier of the equipment's state tag.
+   */
+  private Long stateTagId;
 
-    /**
-     * Description/reason for the current status.
-     */
-    @NonNull
-    private String statusDescription;
+  /**
+   * Identifier of the equipment's alive tag (if any).
+   */
+  private Long aliveTagId;
 
-    /**
-     * The time of the last update of the supervision status.
-     */
-    @NonNull
-    private Timestamp statusTime;
+  /**
+   * Constructor.
+   *
+   * @param id2
+   * @param stateTagId2
+   */
+  protected AbstractSupervisedCacheObject(Long id2, Long stateTagId2) {
+    super(id2);
+    this.stateTagId = stateTagId2;
+  }
 
-    /**
-     * Identifier of the equipment's state tag.
-     */
-    private Long stateTagId;
+  /**
+   * Constructor
+   *
+   * @param id
+   * @param name
+   * @param stateTagId
+   * @param aliveTagId
+   * @param aliveInterval
+   */
+  public AbstractSupervisedCacheObject(Long id, String name, Long stateTagId, Long aliveTagId, int aliveInterval) {
+    this(id, name, stateTagId);
+    this.aliveTagId = aliveTagId;
+    this.aliveInterval = aliveInterval;
+  }
 
-    /**
-     * Identifier of the equipment's alive tag (if any).
-     */
-    private Long aliveTagId;
-
-    /**
-     * Constructor.
-     *
-     * @param id2
-     * @param stateTagId2
-     */
-    protected AbstractSupervisedCacheObject(Long id2, Long stateTagId2) {
-        super(id2);
-        this.stateTagId = stateTagId2;
-    }
-
-    /**
-     * Constructor
-     *
-     * @param id
-     * @param name
-     * @param stateTagId
-     * @param aliveTagId
-     * @param aliveInterval
-     */
-    public AbstractSupervisedCacheObject(Long id, String name, Long stateTagId, Long aliveTagId, int aliveInterval) {
-        this(id, name, stateTagId);
-        this.aliveTagId = aliveTagId;
-        this.aliveInterval = aliveInterval;
-    }
-
-
-    public AbstractSupervisedCacheObject(Long id, String name, Long stateTagId) {
-        super(id);
-        this.name = name;
-        this.stateTagId = stateTagId;
-    }
+  public AbstractSupervisedCacheObject(Long id, String name, Long stateTagId) {
+    super(id);
+    this.name = name;
+    this.stateTagId = stateTagId;
+  }
 
   public AbstractSupervisedCacheObject(long id) {
     super(id);
   }
 
   /**
-     * Clone implementation. All runtime information is frozen in clone, that no longer resides in cache (such as
-     * SupervisionStatus).
-     *
-     * @return clone of cache object
-     */
-    @Override
-    public AbstractSupervisedCacheObject clone() {
-        AbstractSupervisedCacheObject cacheObject = (AbstractSupervisedCacheObject) super.clone();
-
-        if (this.statusTime != null) {
-          cacheObject.statusTime = (Timestamp) this.statusTime.clone();
-        }
-
-        return cacheObject;
-    }
+   * Clone implementation. All runtime information is frozen in clone, that no longer resides in cache (such as
+   * SupervisionStatus).
+   *
+   * @return clone of cache object
+   */
+  @Override
+  public AbstractSupervisedCacheObject clone() {
+    return (AbstractSupervisedCacheObject) super.clone();
+  }
 }
