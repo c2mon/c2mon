4cc8bad7ea4863b41958d062e375318d287875cd
Elisabeth Stockinger <elisabeth.stockinger@cern.ch>
2020-12-07 15:50:06 +0100

    Add concept of DeviceClass to Client API

---
+++ c2mon-client/c2mon-client-core/src/main/java/cern/c2mon/client/core/configuration/DeviceClassConfigurationManager.java
@@ -0,0 +1,89 @@
+/******************************************************************************
+ * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
+ *
+ * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
+ * C2MON is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the license.
+ *
+ * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+package cern.c2mon.client.core.configuration;
+
+import cern.c2mon.shared.client.configuration.ConfigurationReport;
+import cern.c2mon.shared.client.configuration.api.device.DeviceClass;
+import cern.c2mon.shared.client.configuration.api.process.Process;
+
+/**
+ * The ProcessConfigurationManager allows to apply create, update and delete
+ * configurations for Devices.
+ */
+public interface DeviceClassConfigurationManager {
+
+  /**
+   * Creates a new 'DeviceClass' on the server for the given name.
+   * <p>
+   * The DeviceClass is created with default parameters.
+   *
+   * @param deviceClassName The name of the device to be created.
+   * @return A {@link ConfigurationReport} containing all details of the
+   * DeviceClass configuration, including if it was successful or not.
+   * @see DeviceClassConfigurationManager#createDeviceClass(DeviceClass)
+   */
+  ConfigurationReport createDeviceClass(String deviceClassName);
+
+  /**
+   * Creates a new 'DeviceClass' with the given parameters in the
+   * {@link DeviceClass} object.
+   * <p>
+   * Next to the specified parameters the DeviceClass is created with default
+   * parameters.
+   * <p>
+   * Note: You have to use {@link Process#create(String)} to instantiate the
+   * parameter of this method.
+   *
+   * @param deviceClass The {@link DeviceClass} configuration for the 'create'.
+   * @return A {@link ConfigurationReport} containing all details of the
+   * Process configuration, including if it was successful or not.
+   * @see DeviceClassConfigurationManager#createDeviceClass(String)
+   */
+  ConfigurationReport createDeviceClass(DeviceClass deviceClass);
+
+  /**
+   * Updates a existing 'DeviceClass' with the given parameters in the
+   * {@link DeviceClass} object.
+   * <p>
+   * Note: You have to use {@link DeviceClass#update(Long)} or
+   * {@link DeviceClass#update(String)} to instantiate the parameter of this
+   * method.
+   *
+   * @param deviceClass The {@link DeviceClass} configuration for the 'update'.
+   * @return A {@link ConfigurationReport} containing all details of the
+   * DeviceClass configuration, including if it was successful or not.
+   */
+  ConfigurationReport updateDeviceClass(DeviceClass deviceClass);
+
+  /**
+   * Removes a existing 'DeviceClass' with the given id.
+   *
+   * @param id The id of the DeviceClass which needs to be removed.
+   * @return A {@link ConfigurationReport} containing all details of the
+   * DeviceClass configuration, including if it was successful or not.
+   */
+  ConfigurationReport removeDeviceClassById(Long id);
+
+  /**
+   * Removes a existing 'DeviceClass' with the given name.
+   *
+   * @param name The name of the DeviceClass which needs to be removed.
+   * @return A {@link ConfigurationReport} containing all details of the
+   * DeviceClass configuration, including if it was successful or not.
+   */
+  ConfigurationReport removeDeviceClass(String name);
+}
---
+++ c2mon-client/c2mon-client-core/src/main/java/cern/c2mon/client/core/configuration/impl/DeviceClassConfigurationManagerImpl.java
@@ -0,0 +1,105 @@
+/******************************************************************************
+ * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
+ *
+ * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
+ * C2MON is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the license.
+ *
+ * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+package cern.c2mon.client.core.configuration.impl;
+
+import cern.c2mon.client.core.configuration.ConfigurationRequestSender;
+import cern.c2mon.client.core.configuration.DeviceClassConfigurationManager;
+import cern.c2mon.client.core.configuration.ProcessConfigurationManager;
+import cern.c2mon.shared.client.configuration.ConfigurationReport;
+import cern.c2mon.shared.client.configuration.api.Configuration;
+import cern.c2mon.shared.client.configuration.api.device.DeviceClass;
+import cern.c2mon.shared.client.configuration.api.process.Process;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static cern.c2mon.client.core.configuration.util.ConfigurationUtil.validateIsCreate;
+import static cern.c2mon.client.core.configuration.util.ConfigurationUtil.validateIsUpdate;
+
+@Service("deviceClassConfigurationManager")
+public class DeviceClassConfigurationManagerImpl implements DeviceClassConfigurationManager {
+
+  private ConfigurationRequestSender configurationRequestSender;
+
+  @Autowired
+  DeviceClassConfigurationManagerImpl(ConfigurationRequestSender configurationRequestSender) {
+    this.configurationRequestSender = configurationRequestSender;
+  }
+
+  public ConfigurationReport createDeviceClass(String deviceClassName) {
+
+    return createDeviceClass(DeviceClass.create(deviceClassName).build());
+  }
+
+  public ConfigurationReport createDeviceClass(DeviceClass deviceClass) {
+
+    List<DeviceClass> deviceClasses = new ArrayList<>();
+    deviceClasses.add(deviceClass);
+
+    validateIsCreate(deviceClasses);
+
+    Configuration config = new Configuration();
+    config.setEntities(deviceClasses);
+
+    return configurationRequestSender.applyConfiguration(config, null);
+  }
+
+  @Override
+  public ConfigurationReport updateDeviceClass(DeviceClass deviceClass) {
+
+    List<DeviceClass> deviceClasses = new ArrayList<>();
+    deviceClasses.add(deviceClass);
+
+    validateIsUpdate(deviceClasses);
+
+    Configuration config = new Configuration();
+    config.setEntities(deviceClasses);
+
+    return configurationRequestSender.applyConfiguration(config, null);
+  }
+
+  @Override
+  public ConfigurationReport removeDeviceClassById(Long id) {
+
+    DeviceClass deleteDeviceClass = new DeviceClass();
+    deleteDeviceClass.setId(id);
+    deleteDeviceClass.setDeleted(true);
+
+    Configuration config = new Configuration();
+    config.setEntities(Collections.singletonList(deleteDeviceClass));
+
+    return configurationRequestSender.applyConfiguration(config, null);
+  }
+
+  @Override
+  public ConfigurationReport removeDeviceClass(String name) {
+
+    DeviceClass deleteDeviceClass = new DeviceClass();
+    deleteDeviceClass.setName(name);
+    deleteDeviceClass.setDeleted(true);
+
+    Configuration config = new Configuration();
+    config.setEntities(Collections.singletonList(deleteDeviceClass));
+
+    return configurationRequestSender.applyConfiguration(config, null);
+  }
+
+
+}
