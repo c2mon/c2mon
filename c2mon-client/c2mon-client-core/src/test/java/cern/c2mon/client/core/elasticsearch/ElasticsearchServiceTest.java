5e25eee2f86c21d8102ac4a6d72313ba89672a06
tmolivei <tiago.marques.oliveira@cern.ch>
2021-04-16 17:21:32 +0200

    Update elasticsearch dependency to 7.12.0 Update http components dependency that was not compatible Update test

--- c2mon-client/c2mon-client-core/src/test/java/cern/c2mon/client/core/elasticsearch/ElasticsearchServiceTest.java
+++ c2mon-client/c2mon-client-core/src/test/java/cern/c2mon/client/core/elasticsearch/ElasticsearchServiceTest.java
@@ -29,7 +29,7 @@ public class ElasticsearchServiceTest {
 
     private static final DockerImageName ELASTICSEARCH_IMAGE = DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch");
 
-    private static final String ELASTICSEARCH_TAG = "7.0.0";
+    private static final String ELASTICSEARCH_TAG = "7.12.0";
 
     @ClassRule
     public static ElasticsearchContainer elasticsearchContainer =
